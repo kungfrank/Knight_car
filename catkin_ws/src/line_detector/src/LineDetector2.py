b50995276feae35dd8f64ec5118442a2d014fce5
Hang <zhaohang0124@gmail.com>
2015-12-19 01:39:52 -0500

    change color space

--- catkin_ws/src/line_detector/src/LineDetector2.py
+++ catkin_ws/src/line_detector/src/LineDetector2.py
@@ -4,28 +4,31 @@ import sys
 
 class LineDetector(object):
 	def __init__(self):
-		self.color_white1 = np.array([150, 150, 150])
-		self.color_white2 = np.array([255, 255, 255])
-		self.color_yellow1 = np.array([0, 150, 150])
-		self.color_yellow2 = np.array([150, 255, 255]) 
-		self.color_red1 = np.array([0, 0, 150])
-		self.color_red2 = np.array([50, 50, 255]) 
+		self.yuv_white1 = np.array([150, 100, 100])
+		self.yuv_white2 = np.array([255, 160, 160])
+		self.yuv_yellow1 = np.array([100, 150, 0])
+		self.yuv_yellow2 = np.array([200, 200, 50]) 
+		self.yuv_red1 = np.array([0, 150, 110])
+		self.yuv_red2 = np.array([100, 255, 140]) 
 
 	def __colorFilter(self, bgr, color):
+		# transform into YUV color space
+		yuv = cv2.cvtColor(bgr, cv2.COLOR_BGR2YUV)
 		if color == 'white':
-			color1 = self.color_white1
-			color2 = self.color_white2		
+			yuv_color1 = self.yuv_white1
+			yuv_color2 = self.yuv_white2		
 		elif color == 'yellow':
-			color1 = self.color_yellow1
-			color2 = self.color_yellow2		
+			yuv_color1 = self.yuv_yellow1
+			yuv_color2 = self.yuv_yellow2		
 		elif color == 'red':
-			color1 = self.color_red1
-			color2 = self.color_red2		
+			yuv_color1 = self.yuv_red1
+			yuv_color2 = self.yuv_red2		
 		else:
 			raise Exception('Error: Undefined color strings...')
 		
-		# threshold lanes by color
-		lane = cv2.inRange(bgr, color1, color2)
+		# threshold lanes by color in YUV space
+		lane = cv2.inRange(yuv, yuv_color1, yuv_color2)
+
 		# binary image processing
 		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3))
 		lane = cv2.erode(lane, kernel)
@@ -38,7 +41,7 @@ class LineDetector(object):
 		return edges
 
 	def __HoughLine(self, edge, bgr):
-		lines = cv2.HoughLinesP(edge, 1, np.pi/180, 100, np.empty(1), minLineLength=100, maxLineGap=20)
+		lines = cv2.HoughLinesP(edge, 1, np.pi/180, 100, np.empty(1), minLineLength=30, maxLineGap=20)
 		if lines is not None:
 			lines = lines[0]
 		return lines
@@ -54,45 +57,9 @@ class LineDetector(object):
 			for x1,y1,x2,y2 in lines:
 				cv2.line(bgr, (x1,y1), (x2,y2), paint, 3)
 
-"""
-def lineDetector(bgr):
-	# Return data structure: line[0] for white, line[1] for yellow
-	lines = [None] * 2
-
-	# Color filtering: white and yellow
-	lane_w = cv2.inRange(bgr, np.array([150, 150, 150]), np.array([255, 255, 255]))
-	lane_y = cv2.inRange(bgr, np.array([0, 150, 150]), np.array([150, 255, 255]))
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3))
-	lane_w = cv2.erode(lane_w, kernel)
-	lane_y = cv2.erode(lane_y, kernel)
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5, 5))
-	lane_w = cv2.dilate(lane_w, kernel)
-	lane_y = cv2.dilate(lane_y, kernel)
-
-	# Canny edge detection
-	gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, ksize=(7,7), sigmaX=5.0)
-	edges = cv2.Canny(gray, 10, 30, apertureSize = 3)
-
-	# Hough Line Transform
-	lane_w = cv2.bitwise_and(lane_w, edges)
-	lines[0] = cv2.HoughLinesP(lane_w, 1, np.pi/180, 100, np.empty(1), minLineLength=100, maxLineGap=20)
-	if lines[0] is not None:
-		lines[0] = lines[0][0]
-		# draw red lines around white lanes
-		for x1,y1,x2,y2 in lines[0]:
-			cv2.line(bgr, (x1,y1), (x2,y2), (0,0,255), 3)
-		
-	lane_y = cv2.bitwise_and(lane_y, edges)
-	lines[1] = cv2.HoughLinesP(lane_y, 1, np.pi/180, 100, np.empty(1), minLineLength=100, maxLineGap=20)
-	if lines[1] is not None:
-		lines[1] = lines[1][0]
-		# draw blue lines around yellow lanes
-		for x1,y1,x2,y2 in lines[1]:
-			cv2.line(bgr, (x1,y1), (x2,y2), (255,0,0), 3)
-	
-	return lines
-"""
+	def getLane(self, bgr, color):
+		lane = self.__colorFilter(bgr, color)
+		return lane
 
 def _main():
 	# read video from file or camera
@@ -126,8 +93,10 @@ def _main():
 		detector.drawLines(bgr, lines_red, (0,255,0))
 
 		cv2.imshow('Line Detector', bgr)
-
 		cv2.waitKey(30)
+
+		# lane = detector.getLane(bgr, 'yellow')
+		# cv2.imshow('Yellow lane', lane)
 
 if __name__ == '__main__':
 	_main()	

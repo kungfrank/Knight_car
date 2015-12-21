4f7956ad50f66fb69ee8735e8ad1de56ce518470
Pontiac <zhaohang0124@gmail.com>
2015-12-21 22:33:19 +0000

    SegmentList Publisher

--- catkin_ws/src/line_detector/src/line_detector_node.py
+++ catkin_ws/src/line_detector/src/line_detector_node.py
@@ -3,98 +3,78 @@ import rospy
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from duckietown_msgs.msg import Segment, SegmentList, Pixel, Point3
+from duckietown_msgs.msg import Segment, SegmentList, Pixel
 from LineDetector import *
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 import time
 
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
-
 class LineDetectorNode(object):
     def __init__(self):
         self.bridge = CvBridge()
-	self.sub_image = rospy.Subscriber("~image", Image, self.cbImage)
-	# self.pub_lines = rospy.Publisher("segments", Segments)
-        # self.marker_pub = rospy.Publisher("~visualization_marker",Marker)
-	self.image_pub = rospy.Publisher("~image_with_lines",Image, queue_size=1);
-	self.detector = LineDetector()
-        # self.white_line_list=Marker()
-        # self.yellow_line_list=Marker()
-        # self.red_line_list=Marker()
-        # markerSetup();
 
+	self.sub_image = rospy.Subscriber("~image", Image, self.cbImage)
+	self.pub_lines = rospy.Publisher("segments", Segments)
+	self.pub_image = rospy.Publisher("~image_with_lines",Image, queue_size=1);
+
+	self.detector = LineDetector()
+
+	self.segmentList = SegmentList()
+	self.segment = Segment()
 
     def cbImage(self,image_msg):
         image_cv = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
-	#rospy.loginfo("Type of image: %s" %str(image_cv.shape))
 	
+	# Line detection
 	lines_white = self.detector.detectLines(image_cv, 'white')
 	lines_yellow = self.detector.detectLines(image_cv, 'yellow')
 	lines_red = self.detector.detectLines(image_cv, 'red')
 
+	# Draw detected lines
 	image_with_lines_cv = image_cv
 	self.detector.drawLines(image_with_lines_cv, lines_white, (0,0,0))
 	self.detector.drawLines(image_with_lines_cv, lines_yellow, (255,0,0))
 	self.detector.drawLines(image_with_lines_cv, lines_red, (0,255,0))
-
-	#rospy.loginfo("Type of image: %s" %str(image_with_lines_cv.shape))			
-	image_msg = self.bridge.cv2_to_imgmsg(image_with_lines_cv, "bgr8")
-	self.image_pub.publish(image_msg)
 	
-	if len(lines_white)>0:	
+	# TODO: Pixel frame to body frame covnersion
+
+	# Publish the segments
+	if len(lines_white)>0:
+		# publishSegmentList(lines_white, self.segmentList.WHITE)	
 		rospy.loginfo("[LineDetectorNode] number of white lines = %s" %(len(lines_white)))
 	if len(lines_yellow)>0:
 		rospy.loginfo("[LineDetectorNode] number of yellow lines = %s" %(len(lines_yellow)))
 	if len(lines_red)>0:
 		rospy.loginfo("[LineDetectorNode] number of red lines = %s" %(len(lines_red)))
-        # publishSegmentList()
+	
+	# Publish the frame with lines
+	image_msg = self.bridge.cv2_to_imgmsg(image_with_lines_cv, "bgr8")
+	self.pub_image.publish(image_msg)
 
     def onShutdown(self):
         rospy.loginfo("[LineDetectorNode] Shutdown.")
-"""  
-def markerSetup(self):
-	white_line_list = 
-        white_line_list.id=0
-        yellow_line_list.id=1
-        red_line_list.id=2
-        white_line_list.type = Marker.LINE_LIST
-        yellow_line_list.type = Marker.LINE_LIST
-        red_line_list.type = Marker.LINE_LIST
-        white_line_list.scale.x=0.2
-        yellow_line_list.scale.x=0.2
-        red_line_list.scale.x=0.2
-        white_line_list.color.r=1.0
-        white_line_list.color.g=1.0
-        white_line_list.color.b=1.0
-        white_line_list.color.a=1.0
-        yellow_line_list.color.g=1.0
-        yellow_line_list.color.r=1.0
-        yellow_line_list.color.a=1.0
-        red_line_list.color.r=1.0
-        red_line_list.color.a=1.0
+    
+    def publishSegmentList(self, lines, color):
+	self.segmentList = []
+	self.segmentList.color = color
+        pixel1=Pixel()
+        pixel2=Pixel()
+	point1=Point()
+	point2=Point()
 
-    def publishSegmentList(self,lines_white,lines_yellow,lines_red):
-        for x1,y1,x2,y2 in lines_white:
-            point1=Point()
-            point2=Point()
-            point1.x=x1
-            point1.y=y1
-            point2.x=x2
-            point2.y=y2
-            white_line_list.push_back(point1)
-            white_line_list.push_back(point2)
-            ..
-            
+        for u1,v1,u2,v2 in lines:
+            pixel1.u=u1
+            pixel1.v=v1
+            pixel2.u=u2
+            pixel2.v=v2
+            segment.pixel_segs[0]=pixel1
+	    segment.pixel_segs[1]=pixel2
+	    # TODO: assign segment.point_segs
+	
+	    self.segmentList.append(segment)
 
-        for x1  lines_yello
-		..
-        
-        self.marker_pub.Publish(lines_white)
-        self.marker_pub.Publish(lines_yellow)
-        ...
-"""   
+        self.pub_lines.publish(segmentList)
+          
 
 if __name__ == '__main__': 
     rospy.init_node('line_detector',anonymous=False)
@@ -102,25 +82,3 @@ if __name__ == '__main__':
     rospy.on_shutdown(line_detector_node.onShutdown)
     rospy.spin()
 
-    # resolution = (640, 480)
-    # fps = 30
-
-    # # initialize the camera
-    # camera = PiCamera()
-    # camera.resolution = resolution
-    # camera.framerate = fps
-    # rawCapture = PiRGBArray(camera, size=resolution)
- 
-    # # capture frames from the camera
-    # for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    #   bgr = frame.array
-        
-    #   # Line detector
-    #   lines = laneDetector.lineDetector(bgr)
- 
-    #   # show the frame
-    #   cv2.imshow("Frame", bgr)
-    #   cv2.waitKey(1)
- 
-    #   # clear the stream in preparation for the next frame
-    #   rawCapture.truncate(0)

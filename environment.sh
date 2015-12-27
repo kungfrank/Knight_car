522bcb7b5a0fec2af5bbd32def019122d5f0ee75
mercedes <censi@mit.edu>
2015-12-27 01:05:06 +0000

    other environment variables

--- environment.sh
+++ environment.sh
@@ -2,5 +2,11 @@ echo "Activating ROS"
 source /opt/ros/indigo/setup.bash
 echo "Setting up PYTHONPATH"
 export PYTHONPATH=/home/ubuntu/duckietown/catkin_ws/src:$PYTHONPATH
-echo "Activating development"
+echo "Activating development workspace"
 source ~/duckietown/catkin_ws/devel/setup.bash
+echo "setting ROS_MASTER_URI"
+export ROS_MASTER_URI=http://`hostname`.local:11311
+echo ROS_MASTER_URI=$ROS_MASTER_URI
+
+export ROS_HOSTNAME=`hostname`.local
+echo ROS_HOSTNAME=$ROS_HOSTNAME

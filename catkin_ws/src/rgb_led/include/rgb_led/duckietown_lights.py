2badd8adee97b8b8a14e416d3292050d3aea574c
mercedes <censi@mit.edu>
2016-01-02 01:21:22 +0000

    blinking LEDs

---
+++ catkin_ws/src/rgb_led/include/rgb_led/duckietown_lights.py
@@ -0,0 +1,19 @@
+
+TOP = 2
+BACK_LEFT = 0
+BACK_RIGHT = 1
+FRONT_LEFT = 3
+FRONT_RIGHT = 4
+
+s = 0.15
+
+
+
+
+RED = [0.24, 0, 0]
+GREEN = [0, s, 0]
+WHITE = [s, s, s]
+OFF = [0, 0, 0]
+
+s = 0.5
+WHITE = [s, s, s]

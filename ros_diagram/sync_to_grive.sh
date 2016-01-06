c01d095d488cde770dee0121fb69d6dbf8c32518
Shih-Yuan Liu <syliu@mit.edu>
2016-01-06 00:37:28 -0500

    Minor tweak on sync_to_grive.sh

--- ros_diagram/sync_to_grive.sh
+++ ros_diagram/sync_to_grive.sh
@@ -7,13 +7,13 @@ export SUB_FOLDER=Duckietown/Duckietown-Public/Duckietown_design
 echo "GRIVE_ROOT set to $GRIVE_ROOT"
 
 echo "Generating pdf file..."
-dot -O Duckietown_ROS_Diagram.dot -Tpdf
+dot Duckietown_ROS_Diagram.dot -Tpdf -o Duckietown_ROS_Diagram.pdf
 
-echo "Copying Duckietown_ROS_Diagram.* to $GRIVE_ROOT/$SUB_FOLDER"
-cp Duckietown_ROS_Diagram.* $GRIVE_ROOT/$SUB_FOLDER/
+echo "Copying Duckietown_ROS_Diagram.dot.pdf to $GRIVE_ROOT/$SUB_FOLDER"
+cp Duckietown_ROS_Diagram.pdf $GRIVE_ROOT/$SUB_FOLDER/
 
 echo "Removing Duckietown_ROS_Diagram.dot.pdf"
-rm Duckietown_ROS_Diagram.dot.*
+rm Duckietown_ROS_Diagram.dot.pdf
 
 if [ "$(uname)" == "Linux" ]; then
     echo "Calling grive to sync to google drive if on Linux."

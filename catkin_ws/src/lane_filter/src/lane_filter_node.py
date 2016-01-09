8158000a0aad02344dd068559ac27c8ca53994f8
liampaull <lpaull@mit.edu>
2016-01-09 16:33:20 -0500

    debugging the lane filter node

--- catkin_ws/src/lane_filter/src/lane_filter_node.py
+++ catkin_ws/src/lane_filter/src/lane_filter_node.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from LaneFilter import *
 from duckietown_msgs.msg import SegmentList, Segment, Pixel, LanePose
 from scipy.stats import multivariate_normal
-from math import floor, atan2
+from math import floor, atan2, pi
 
 # Lane Filter Node
 # Author: Liam Paull
@@ -15,17 +14,18 @@ from math import floor, atan2
 
 class LaneFilterNode(object):
     def __init__(self):
+        self.node_name = "Lane Filter"
         self.sub = rospy.Subscriber("~segment_list", SegmentList, self.processSegments)
         # self.sub = rospy.Subscriber("~velocity",
         self.pub_lane_pose = rospy.Publisher("~lane_pose", LanePose)
         self.mean_0 = [self.setupParam("~mean_d_0",0) , self.setupParam("~mean_phi_0",0)]
-        self.cov_0  = [ [self.setupParam("~sigma_d_0",0),0] , [0, self.setupParam("~sigma_phi_0",0)] ] 
+        self.cov_0  = [ [self.setupParam("~sigma_d_0",0.1) , 0] , [0, self.setupParam("~sigma_phi_0",0.01)] ] 
         self.delta_d     = self.setupParam("~delta_d",0.02) # in meters
         self.delta_phi   = self.setupParam("~delta_phi",0.02) # in radians
         self.d_max       = self.setupParam("~d_max",0.5)
         self.d_min       = self.setupParam("~d_min",-0.7)
-        self.phi_min     = self.setupParam("~phi_min",-PI/2)
-        self.phi_max     = self.setupParam("~phi_max",PI/2)
+        self.phi_min     = self.setupParam("~phi_min",-pi/2)
+        self.phi_max     = self.setupParam("~phi_max",pi/2)
         self.cov_v       = self.setupParam("~cov_v",0.5) # linear velocity "input"
         self.cov_omega   = self.setupParam("~cov_omega",0.01) # angular velocity "input"
         self.linewidth_white = self.setupParam("~linewidth_white",0.04)
@@ -33,9 +33,9 @@ class LaneFilterNode(object):
         self.lanewidth        = self.setupParam("~lanewidth",0.4)
         self.max_entropy = self.setupParam("~max_entropy", 0.04) # nats
 
-        self.d,self.phi = np.mgrid[d_min:d_max:delta_d,phi_min:phi_max:delta_phi]
-        self.beliefRV=np.array()
-        initializeBelief()
+        self.d,self.phi = np.mgrid[self.d_min:self.d_max:self.delta_d,self.phi_min:self.phi_max:self.delta_phi]
+        self.beliefRV=np.empty(self.d.shape)
+        self.initializeBelief()
         
 
 
@@ -51,31 +51,36 @@ class LaneFilterNode(object):
         # initialize measurement likelihood
         measurement_likelihood = np.zeros(d.shape)
         for segment in segment_list_msg.segments:
-            if segment.color != segment.WHITE && segment.color != segment.YELLOW:
+            if segment.color != segment.WHITE and segment.color != segment.YELLOW:
                 continue
-            d_i,phi_i = generateVote(segment)
+            d_i,phi_i = self.generateVote(segment)
             if d_i > self.d_max or d_i < self.d_min or phi_i < self.phi_min or phi_i>self.phi_max:
                 continue
             i = floor((d_i - self.d_min)/self.delta_d)
             j = floor((d_j - self.phi_min)/self.delta_phi)
             measurement_likelihood[i,j] += 1
         measurement_likelihood = measurement_likelihood/np.linalg.norm(measurement_likelihood)
-        updateBelief(measurement_likelihood)
+        self.updateBelief(measurement_likelihood)
+        # TODO entropy test:
+        # TODO publish
+
 
     def initializeBelief(self):
-        pos = np.empty(d.shape + (2,))
-        pos[:,:,0]=d
-        pos[:,:,1]=phi
-        RV = multivariate_normal(mean_0,cov_0)
-        self.beliefRV=RV(pos)
+        pos = np.empty(self.d.shape + (2,))
+        pos[:,:,0]=self.d
+        pos[:,:,1]=self.phi
+        self.cov_0
+        RV = multivariate_normal(self.mean_0,self.cov_0)
+        self.beliefRV=RV.pdf(pos)
 
     def propagateBelief(self):
         # starting with option 1 (don't read linear and angular velocity)
         # even simpler.. do nothing
+        return
 
-    def updateBelief(self,ndarray):
-        beliefRV=multiply(beliefRV,measurement_likelihood)
-        beliefRV=beliefRV/np.linalg.norm(beliefRV)
+    def updateBelief(self,measurement_likelihood):
+        self.beliefRV=multiply(self.beliefRV,measurement_likelihood)
+        self.beliefRV=beliefRV/np.linalg.norm(self.beliefRV)
 
     def generateVote(self,segment):
         p1 = segment.points[0]

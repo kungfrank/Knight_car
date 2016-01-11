c131343c597983f3b5251d1a16b75a147fac7318
cchoi <cchoi@csail.mit.edu>
2016-01-11 09:25:15 -0500

    add vehicle name param

--- catkin_ws/src/ground_projection/scripts/ex_get_ground_coordinate.py
+++ catkin_ws/src/ground_projection/scripts/ex_get_ground_coordinate.py
@@ -7,23 +7,29 @@ from geometry_msgs.msg import Point
 import numpy as np
 import IPython
 
-def call_service_get_ground_coordinate(req):
-  rospy.wait_for_service("/ground_projection/get_ground_coordinate")
+def call_service_get_ground_coordinate(req, veh):
+  rospy.wait_for_service(veh + "/ground_projection/get_ground_coordinate")
 
   try:
-    get_ground_coord = rospy.ServiceProxy('/ground_projection/get_ground_coordinate', GetGroundCoord)
+    get_ground_coord = rospy.ServiceProxy(veh + '/ground_projection/get_ground_coordinate', GetGroundCoord)
     resp = get_ground_coord(req)
     return resp.gp
   except rospy.ServiceException, e:
     print "Service call failed: %s" % e
 
 if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print "usage: " + sys.argv[0] + " vehicle name"
+    sys.exit()
+
+  veh = "/" + sys.argv[1]
+
   rospy.init_node("ex_get_ground_coordinate")
 
   uv = Pixel()
   uv.u = 320
   uv.v = 300
 
-  gp = call_service_get_ground_coordinate(uv)
+  gp = call_service_get_ground_coordinate(uv, veh)
 
   print "ground coordinate: (%f, %f, %f)" % (gp.x, gp.y, gp.z)

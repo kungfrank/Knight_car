f457d5080884a2aaaa9419688c9ad09daabb3351
Shih-Yuan Liu <syliu@mit.edu>
2016-01-12 10:26:33 -0500

    Add duckiebot_install.sh

---
+++ setup/duckiebot_install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get install ros-indigo-{tf-conversions,cv-bridge,image-transport,camera-info-manager,theora-image-transport,joy,image-proc}
+sudo apt-get install libyaml-cpp-dev
+

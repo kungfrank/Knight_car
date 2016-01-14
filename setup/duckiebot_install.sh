36588a62d4a567261bc1fbedae5f23d79af077de
Shih-Yuan Liu <syliu@mit.edu>
2016-01-14 18:09:04 -0500

    Add duckietown_install.sh.

--- setup/duckiebot_install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install ros-indigo-{tf-conversions,cv-bridge,image-transport,camera-info-manager,theora-image-transport,joy,image-proc}
-sudo apt-get install libyaml-cpp-dev
-

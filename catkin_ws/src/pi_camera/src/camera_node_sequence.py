b472f9812e7dd36d110a2e87c015f5880435bd5b
MegaMan <syliu@mit.edu>
2016-01-20 04:43:00 +0000

    Add decompress parameter to camera node for easier calibration operation.

--- catkin_ws/src/pi_camera/src/camera_node_sequence.py
+++ catkin_ws/src/pi_camera/src/camera_node_sequence.py
@@ -4,8 +4,7 @@ import cv2
 import io
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image, CompressedImage
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import time
@@ -20,14 +19,19 @@ class CameraNode(object):
         self.framerate = self.setupParam("~framerate",60.0)
         self.res_w = self.setupParam("~res_w",320)
         self.res_h = self.setupParam("~res_h",200)
+        self.decompress = self.setupParam("~decompress",False)
         # self.uncompress = self.setupParam("~uncompress",False)
         
         self.has_published = False
-        self.pub_img= rospy.Publisher("~image/compressed",CompressedImage,queue_size=1)
+        
+        if self.decompress:
+            self.pub_img= rospy.Publisher("~image/raw",Image,queue_size=1)
+            self.bridge = CvBridge()
+        else:
+            self.pub_img= rospy.Publisher("~image/compressed",CompressedImage,queue_size=1)
 
         # Setup PiCamera
         self.stream = io.BytesIO()
-        self.bridge = CvBridge()
         self.camera = PiCamera()
         self.camera.framerate = self.framerate
         self.camera.resolution = (self.res_w,self.res_h)
@@ -53,14 +57,25 @@ class CameraNode(object):
 
             yield stream
             # Construct image_msg
-            image_msg = CompressedImage()
-            image_msg.header.stamp = rospy.Time.now()
-            image_msg.format = "jpeg"
             # Grab image from stream
             stream.seek(0)
-            image_msg.data = stream.getvalue()
-            # Publish 
+            stream_data = stream.getvalue()
+            stamp = rospy.Time.now()
+            if not self.decompress:
+                # Generate compressed image
+                image_msg = CompressedImage()
+                image_msg.format = "jpeg"
+                image_msg.data = stream_data
+            else:
+                # Generate raw image
+                image_msg = Image()
+                data = np.fromstring(stream_data, dtype=np.uint8)  #320 by 240 90Hz
+                image = cv2.imdecode(data, cv2.CV_LOAD_IMAGE_COLOR) # drop to about 60Hz
+                image_msg = self.bridge.cv2_to_imgmsg(image) # drop to about 30hz..
+
+            image_msg.header.stamp = stamp
             publisher.publish(image_msg)
+            
             # Clear stream
             stream.seek(0)
             stream.truncate()
@@ -68,7 +83,8 @@ class CameraNode(object):
             if not self.has_published:
                 rospy.loginfo("[%s] Published the first image." %(self.node_name))
                 self.has_published = True
-
+                
+            rospy.sleep(rospy.Duration.from_sec(0.001))
 
     def setupParam(self,param_name,default_value):
         value = rospy.get_param(param_name,default_value)
@@ -76,7 +92,7 @@ class CameraNode(object):
         rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
         return value
 
-    def onShutdown():
+    def onShutdown(self):
         rospy.loginfo("[%s] Closing camera." %(self.node_name))
         # self.camera.stop_recording(splitter_port=0)
         # rospy.sleep(rospy.Duration.from_sec(2.0))

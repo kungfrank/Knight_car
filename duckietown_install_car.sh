64fc8c97bc82e6b852470ce38c8d32facd897acf
Shih-Yuan Liu <syliu@mit.edu>
2016-02-02 22:41:35 -0500

    Update install scripts. Add duckietop_setup.sh.

--- duckietown_install_car.sh
+++ duckietown_install_car.sh
@@ -19,11 +19,10 @@ sudo ldconfig
 cd $START_PATH
 
 # Install some packages that were missed in v1.1
-sudo apt-get install ros-indigo-{tf-conversions,cv-bridge,image-transport,camera-info-manager,theora-image-transport,joy,image-proc} -y --force-yes
-sudo apt-get install ros-indigo-compressed-image-transport -y --force-yes
+sudo apt-get install ros-indigo-{tf-conversions,cv-bridge,image-transport,camera-info-manager,theora-image-transport,joy,image-proc} -y
+sudo apt-get install ros-indigo-compressed-image-transport -y
 sudo apt-get install libyaml-cpp-dev -y
 
 # packages for the IMU
 sudo apt-get install ros-indigo-phidgets-drivers
-#sudo apt-get install ros-indigo-imu-tools
 sudo apt-get install ros-indigo-imu-complementary-filter ros-indigo-imu-filter-madgwick
--- duckietown_install_laptop.sh
+++ duckietown_install_laptop.sh
@@ -1,5 +1,19 @@
-
+#!/usr/bin/env bash
+set -e
 
 # packages for the IMU
-sudo apt-get install ros-indigo-phidgets-drivers
-sudo apt-get install ros-indigo-imu-tools
+sudo apt-get install ros-indigo-phidgets-drivers ros-indigo-imu-tools -y
+
+#Install open-ssh and avahi
+sudo apt-get install openssh-server avahi-daemon avahi-discover avahi-utils -y
+
+#Install some GUI tools for git
+sudo apt-get install gitg kdiff3 -y
+
+#Install byobu
+sudo apt-get install byobu -y
+
+#Install sublime text editor
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer -y

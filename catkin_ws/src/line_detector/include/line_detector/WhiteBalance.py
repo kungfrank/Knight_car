f6df1c4b8b4bda7ebe99b34f394ebfbc7cbf0613
Hang <zhaohang0124@gmail.com>
2016-02-10 18:47:19 -0500

    update white balancing

--- catkin_ws/src/line_detector/include/line_detector/WhiteBalance.py
+++ catkin_ws/src/line_detector/include/line_detector/WhiteBalance.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 import sys
 import time
+from matplotlib import pyplot as plt
 
 class WhiteBalance(object):
     def __init__(self):
         self.x = 0.5
-        self.y = 0.7
-        self.w = 0.16
-        self.h = 0.16
+        self.y = 0.5
+        self.w = 0.08
+        self.h = 0.08
         self.norm_bgr = np.ones((1,1,3))         
 
     def __calculateRegion(self, shape):
@@ -19,15 +20,26 @@ class WhiteBalance(object):
         return x1, y1, x2, y2
 
     def setRefImg(self, bgr):
-        x1, y1, x2, y2 = self.__calculateRegion(bgr.shape)
-
         # region that we assume to be gray
+        x1, y1, x2, y2 = self.__calculateRegion(bgr.shape)
         region = np.copy(bgr[y1:y2, x1:x2, :])
-        self.norm_bgr = np.mean(region, axis=(0,1), keepdims=True)/np.mean(region)
+
+        # Gray world assumption algorithm
+        # mean or max
+        self.norm_bgr = np.mean(region)/np.mean(region, axis=(0,1), keepdims=True)
+        #self.norm_bgr = max(np.mean(region, axis=(0,1)))/np.mean(region, axis=(0,1), keepdims=True)
         print self.norm_bgr
 
     def correctImg(self, bgr):
-        bgr = (1.*bgr/self.norm_bgr).astype('uint8')
+        bgr *= self.norm_bgr
+    
+    def plotHist(self, bgr):
+        color = ('b','g','r')
+        for i,col in enumerate(color):
+            histr = cv2.calcHist([bgr],[i],None,[256],[0,256])
+            plt.plot(histr,color = col)
+            plt.xlim([0,256])
+        plt.show()
 
     def drawRegion(self, bgr):
         x1, y1, x2, y2 = self.__calculateRegion(bgr.shape)
@@ -38,10 +50,17 @@ def main():
 
     if len(sys.argv)==2:
         bgr = cv2.imread(sys.argv[1])
-        bgr = cv2.resize(bgr, (320, 240)) 
+        bgr = cv2.resize(bgr, (640, 480)) 
+        
+        # Set reference image to estimate the parameters
         wb.setRefImg(bgr)
 
+        # Histogram before balancing
+        wb.plotHist(bgr)
+        
+        # Apply white balancing
         wb.correctImg(bgr)
+        wb.plotHist(bgr)
 
         wb.drawRegion(bgr)
         cv2.imshow('image', bgr)

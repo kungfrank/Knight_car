8c50f2fc08e69643d40253345ad57189c203bf90
mnovitzky <novitzky@mit.edu>
2016-02-22 00:34:23 +0000

    first pass at Finite State Machine

---
+++ catkin_ws/src/fsm/src/fsm_node.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+import rospy
+#from fsm.util import HelloGoodbye #Imports module. Not limited to modules in this pkg. 
+from duckietown_msgs.msg import FSMState
+from std_msgs.msg import String #Imports msg
+from std_msgs.msg import Bool #Imports msg
+#from duckietown_msgs.msg import messages to command the wheels
+#from duckietown_msgs.msg import WheelsCmdStamped
+
+class FSMNode(object):
+    def __init__(self):
+        self.actual = FSMState()
+        self.actual.state = self.actual.LANE_FOLLOWING
+        self.in_lane = False
+        self.at_stop_line = False
+        self.intersection_go = False
+        self.intersection_done = False
+
+        # Save the name of the node
+        self.node_name = rospy.get_name()
+        
+        rospy.loginfo("[%s] Initialzing." %(self.node_name))
+
+        # Setup publishers
+        self.pub_topic_mode = rospy.Publisher("~mode",String, queue_size=1, latch=True)
+        self.pub_topic_intersection_done = rospy.Publisher("~intersection_done",Bool, queue_size=1)
+        self.pub_topic_intersection_go = rospy.Publisher("~intersection_go",Bool, queue_size=1)
+        # Setup subscribers
+        self.sub_topic_in_lane = rospy.Subscriber("~in_lane", Bool, self.cbInLane, queue_size=1)
+#        self.sub_topic_at_stop_line = rospy.Subscriber("~at_stop_line", Bool, self.cbAtStopLine, queue_size=1)
+#        self.sub_topic_intersection_go = rospy.Subscriber("~intersection_go", Bool, self.cbIntersectionGo, queue_size=1)
+#        self.sub_topic_intersection_done = rospy.Subscriber("~intersection_done", Bool, self.cbIntersectionDone, queue_size=1)
+
+        # Read parameters
+        self.pub_timestep = self.setupParameter("~pub_timestep",1.0)
+        # Create a timer that calls the cbTimer function every 1.0 second
+        #self.timer = rospy.Timer(rospy.Duration.from_sec(self.pub_timestep),self.cbTimer)
+
+        rospy.loginfo("[%s] Initialzed." %(self.node_name))
+
+        self.rate = rospy.Rate(30) # 10hz
+
+    def cbInLane(self, in_lane_msg):
+        print in_lane_msg
+        self.in_lane = in_lane_msg.data
+        self.updateState()
+
+    def updateState(self):
+        if(self.actual.state == self.actual.LANE_FOLLOWING):
+            if(self.at_stop_line == True):
+#update the state
+                self.actual.state = self.actual.COORDINATION
+        elif(self.actual.state == self.actual.COORDINATION):
+            if(self.intersection_go == True):
+                self.actual.state = self.actual.INTERSECTION_CONTROL
+                self.intersection_go = False
+        else(self.actual.state == self.actual.INTERSECTION_CONTROL):
+            if(self.in_lane == True and self.intersection_done == True):
+                self.actual.state = self.actual.LANE_FOLLOWING
+                self.intersection_done = False
+
+        self.pub_topic_mode.publish(self.actual.state)f
+
+
+
+    def cbMode(self, mode_msg):
+        print mode_msg
+        if(mode_msg.data == "intersection_control"):
+            self.turnRight()
+
+
+    def turnRight(self):
+        #move forward
+        forward_for_time_leave = 2.0
+        turn_for_time = 0.7
+        forward_for_time_enter = 2.0
+        
+        starting_time = rospy.Time.now()
+        while((rospy.Time.now() - starting_time) < rospy.Duration(forward_for_time_leave)):
+            wheels_cmd_msg = WheelsCmdStamped()
+            wheels_cmd_msg.header.stamp = rospy.Time.now()
+            wheels_cmd_msg.vel_left = 0.4
+            wheels_cmd_msg.vel_right = 0.4
+            self.pub_wheels_cmd.publish(wheels_cmd_msg)    
+            rospy.loginfo("Moving?.")
+            self.rate.sleep()
+        #turn right
+        starting_time = rospy.Time.now()
+        while((rospy.Time.now() - starting_time) < rospy.Duration(turn_for_time)):
+            wheels_cmd_msg = WheelsCmdStamped()
+            wheels_cmd_msg.header.stamp = rospy.Time.now()
+            wheels_cmd_msg.vel_left = 0.25
+            wheels_cmd_msg.vel_right = -0.25
+            self.pub_wheels_cmd.publish(wheels_cmd_msg)    
+            rospy.loginfo("Moving?.")
+            self.rate.sleep()
+   
+            #coordination with lane controller means part way through announce finished turn
+            self.pub_wheels_done.publish(True)
+
+        #move forward
+        starting_time = rospy.Time.now()
+        while((rospy.Time.now() - starting_time) < rospy.Duration(forward_for_time_enter)):
+            wheels_cmd_msg = WheelsCmdStamped()
+            wheels_cmd_msg.header.stamp = rospy.Time.now()
+            wheels_cmd_msg.vel_left = 0.4
+            wheels_cmd_msg.vel_right = 0.4
+            self.pub_wheels_cmd.publish(wheels_cmd_msg)    
+            rospy.loginfo("Moving?.")
+            self.rate.sleep()
+   
+    def setupParameter(self,param_name,default_value):
+        value = rospy.get_param(param_name,default_value)
+        rospy.set_param(param_name,value) #Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+        return value
+
+    def cbTopic(self,msg):
+        rospy.loginfo("[%s] %s" %(self.node_name,msg.data))
+
+#    def cbTimer(self,event):
+#        singer = HelloGoodbye()
+        # Simulate hearing something
+#        msg = String()
+#        msg.data = "duckietown"
+#singer.sing("duckietown")
+#        self.pub_topic_a.publish(msg)
+#        wheels_cmd_msg = WheelsCmdStamped()
+#        wheels_cmd_msg.vel_left = 0.1
+#        wheels_cmd_msg.vel_right = 0.1
+#        self.pub_wheels_cmd.publish(wheels_cmd_msg)
+
+    def on_shutdown(self):
+        rospy.loginfo("[%s] Shutting down." %(self.node_name))
+
+if __name__ == '__main__':
+    # Initialize the node with rospy
+    rospy.init_node('fsm_node', anonymous=False)
+
+    # Create the NodeName object
+    node = FSMNode()
+
+    # Setup proper shutdown behavior 
+    rospy.on_shutdown(node.on_shutdown)
+    # Keep it spinning to keep the node alive
+    rospy.spin()

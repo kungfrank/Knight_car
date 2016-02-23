cd93feb86e2c880773d77c876e80cb862b8a9a86
MegaMan <syliu@mit.edu>
2016-02-23 02:58:14 +0000

    clean up camera_node_sequence.py

--- catkin_ws/src/pi_camera/src/camera_node_sequence.py
+++ catkin_ws/src/pi_camera/src/camera_node_sequence.py
@@ -24,42 +24,18 @@ class CameraNode(object):
         self.framerate = self.setupParam("~framerate",30.0)
         self.res_w = self.setupParam("~res_w",640)
         self.res_h = self.setupParam("~res_h",480)
-        self.decompress = self.setupParam("~decompress",False)
-        self.publish_info = self.setupParam("~publish_info",True)
-        self.cali_file_name = self.setupParam("~cali_file_name","default")
-        self.config         = self.setupParam("~config","baseline")
-        self.frame_id = rospy.get_namespace() + "camera_optical_frame"
 
+        # For intrinsic calibration
         rospack = rospkg.RosPack()
+        self.config = self.setupParam("~config","baseline")
         self.cali_file_folder = rospack.get_path('duckietown') + "/config/" + self.config + "/calibration/camera_intrinsic/"
-        self.cali_file = self.cali_file_folder +  self.cali_file_name + ".yaml" 
-        self.camera_info_msg = None
+    
+        self.frame_id = rospy.get_namespace().strip('/') + "/camera_optical_frame"
 
-        if not os.path.isfile(self.cali_file):
-            rospy.logwarn("[%s] Can't find calibration file: %s.\nUsing default calibration instead." %(self.node_name,self.cali_file))
-            self.cali_file = rospack.get_path('duckietown') + "/config/" + self.config + "/calibration/camera_intrinsic/default.yaml" 
-
-        if not os.path.isfile(self.cali_file):
-            rospy.signal_shutdown("Found no calibration file ... aborting")
-
-        rospy.loginfo("[%s] Using calibration file: %s" %(self.node_name,self.cali_file))
-        self.camera_info_msg = self.loadCameraInfo(self.cali_file)        
-        self.camera_info_msg.header.frame_id = self.frame_id
-        
-        if self.publish_info:
-            self.pub_camera_info = rospy.Publisher("~camera_info",CameraInfo,queue_size=1)
-        
         self.has_published = False
-        
-        if self.decompress:
-            self.pub_img= rospy.Publisher("~image/raw",Image,queue_size=1)
-            self.bridge = CvBridge()
-        else:
-            self.pub_img= rospy.Publisher("~image/compressed",CompressedImage,queue_size=1)
+        self.pub_img= rospy.Publisher("~image/compressed",CompressedImage,queue_size=1)
 
-
-
-        # Create service
+        # Create service (for camera_calibration)
         self.srv_set_camera_info = rospy.Service("~set_camera_info",SetCameraInfo,self.cbSrvSetCameraInfo)
 
         # Setup PiCamera
@@ -89,34 +65,29 @@ class CameraNode(object):
 
             yield stream
             # Construct image_msg
+
             # Grab image from stream
             stamp = rospy.Time.now()
             stream.seek(0)
             stream_data = stream.getvalue()
-            if not self.decompress:
-                # Generate compressed image
-                image_msg = CompressedImage()
-                image_msg.format = "jpeg"
-                image_msg.data = stream_data
-            else:
-                # Generate raw image
-                image_msg = Image()
-                data = np.fromstring(stream_data, dtype=np.uint8)  #320 by 240 90Hz
-                image = cv2.imdecode(data, cv2.CV_LOAD_IMAGE_COLOR) # drop to about 60Hz
-                image_msg = self.bridge.cv2_to_imgmsg(image) # drop to about 30hz..
+            # Generate compressed image
+            image_msg = CompressedImage()
+            image_msg.format = "jpeg"
+            image_msg.data = stream_data
+
+            # Generate raw image
+            # image_msg = Image()
+            # data = np.fromstring(stream_data, dtype=np.uint8)  #320 by 240 90Hz
+            # image = cv2.imdecode(data, cv2.CV_LOAD_IMAGE_COLOR) # drop to about 60Hz
+            # image_msg = self.bridge.cv2_to_imgmsg(image) # drop to about 30hz..
 
             image_msg.header.stamp = stamp
             image_msg.header.frame_id = self.frame_id
             publisher.publish(image_msg)
-            
-            
+                        
             # Clear stream
             stream.seek(0)
             stream.truncate()
-
-            if self.publish_info:
-                self.camera_info_msg.header.stamp = stamp
-                self.pub_camera_info.publish(self.camera_info_msg)
             
             if not self.has_published:
                 rospy.loginfo("[%s] Published the first image." %(self.node_name))
@@ -152,6 +123,7 @@ class CameraNode(object):
         return response
 
     def saveCameraInfo(self, camera_info_msg, filename):
+        # Convert camera_info_msg and save to a yaml file
         rospy.loginfo("[saveCameraInfo] filename: %s" %(filename))
         file = open(filename, 'w')
 
@@ -173,63 +145,10 @@ class CameraNode(object):
         except IOError:
             return False
 
-
-    def loadCameraInfo(self, filename):
-        stream = file(filename, 'r')
-        calib_data = yaml.load(stream)
-        cam_info = CameraInfo()
-        cam_info.width = calib_data['image_width']
-        cam_info.height = calib_data['image_height']
-        cam_info.K = calib_data['camera_matrix']['data']
-        cam_info.D = calib_data['distortion_coefficients']['data']
-        cam_info.R = calib_data['rectification_matrix']['data']
-        cam_info.P = calib_data['projection_matrix']['data']
-        cam_info.distortion_model = calib_data['distortion_model']
-        return cam_info
-
-# def output(publisher):
-#     stream = io.BytesIO()
-#     # image_msg = Image()
-#     image_msg = CompressedImage()
-#     image_msg.format = "jpeg"
-
-#     bridge = CvBridge()
-#     # while not rospy.is_shutdown():
-#     while True:
-#         if rospy.is_shutdown():
-#             break
-#         yield stream
-#         stream.seek(0)
-#         stream_value = stream.getvalue()
-#         # data = np.fromstring(stream_value, dtype=np.uint8)  #320 by 240 90Hz
-#         # image = cv2.imdecode(data, 1) # drop to about 60Hz
-#         # image_msg = bridge.cv2_to_imgmsg(image) # drop to about 30hz...
-#         image_msg.data = stream_value
-#         publisher.publish(image_msg)
-#         # Clean up stream
-#         stream.seek(0)
-#         stream.truncate()
-#     return
-
-
-
-
-
 if __name__ == '__main__': 
     rospy.init_node('camera',anonymous=False,disable_signals=True)
     camera_node = CameraNode()
     signal.signal(signal.SIGINT, camera_node.signal_handler)
     rospy.on_shutdown(camera_node.onShutdown)
     camera_node.startCapturing()
-    # pub_image = rospy.Publisher("~image/compressed",CompressedImage,queue_size=1)
-    # pub_image = rospy.Publisher("~image",Image,queue_size=1)
-    # resolution = (320,240)
-    # resolution = (640,480)
-    # # initialize the camera
-    # with PiCamera() as camera:
-    #     camera.resolution = resolution
-    #     camera.framerate = 90
-    #     gen = output(publisher=pub_image)
-    #     # camera.capture_sequence(gen,'bgr',use_video_port=True)
-    #     camera.capture_sequence(gen,'jpeg',use_video_port=True)
     rospy.spin()

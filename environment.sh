8201b08d90a9ce9ffcccd3e2d8b3740cc5e18a70
Valerio Varricchio <valerio@mit.edu>
2016-02-25 03:58:51 -0500

    no hardcoded path from environment.sh

--- environment.sh
+++ environment.sh
@@ -2,10 +2,9 @@
 echo "Activating ROS"
 source /opt/ros/indigo/setup.bash
 echo "Setting up PYTHONPATH"
-export PYTHONPATH=/home/ubuntu/duckietown/catkin_ws/src:$PYTHONPATH
+export PYTHONPATH=$DUCKIETOWN_ROOT/catkin_ws/src:$PYTHONPATH
 echo "Activating development"
-source ~/duckietown/catkin_ws/devel/setup.bash
+source $DUCKIETOWN_ROOT/catkin_ws/devel/setup.bash
 echo "Setup ROS_HOSTNAME"
 export ROS_HOSTNAME=$HOSTNAME.local
-export DUCKIETOWN_ROOT=$HOME/duckietown
 exec "$@" #Passes arguments. Need this for ROS remote launching to work.

4cb1f9824a3a30eea79e2956e763c69f2e170860
Valerio Varricchio <valerio@mit.edu>
2016-02-25 18:40:45 -0500

    fixing the hardcoded ROOT envvar in environment.sh

--- environment.sh
+++ environment.sh
@@ -4,8 +4,7 @@ source /opt/ros/indigo/setup.bash
 echo "Setting up PYTHONPATH"
 export PYTHONPATH=/home/ubuntu/duckietown/catkin_ws/src:$PYTHONPATH
 echo "Activating development"
-source ~/duckietown/catkin_ws/devel/setup.bash
+source $DUCKIETOWN_ROOT/catkin_ws/devel/setup.bash
 echo "Setup ROS_HOSTNAME"
 export ROS_HOSTNAME=$HOSTNAME.local
-export DUCKIETOWN_ROOT=$HOME/duckietown
 exec "$@" #Passes arguments. Need this for ROS remote launching to work.

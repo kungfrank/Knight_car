9bd63450604092617c0c7ef8dbfbc9c25383e50e
Andrea Tacchetti <atacchet@mit.edu>
2016-02-26 11:17:02 -0500

    moved files around -- completed setp 4.0

--- catkin_ws/src/spring2016/atacchet/virtual_mirror_atacchet/setup.py
+++ catkin_ws/src/spring2016/atacchet/virtual_mirror_atacchet/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['virtual_mirror_atacchet'],
-    package_dir={'': 'src'},
+    package_dir={'': 'include'},
 )
 
 setup(**setup_args)

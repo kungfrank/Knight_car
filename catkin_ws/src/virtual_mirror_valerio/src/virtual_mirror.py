aefda8080008e61c82787de88e06f4d12a074500
Valerio Varricchio <valerio@mit.edu>
2016-02-26 03:26:24 -0500

    [RCDP5] Processing data from log
    Conflicts:
    	catkin_ws/src/virtual_mirror_valerio/src/virtual_mirror.py

--- catkin_ws/src/virtual_mirror_valerio/src/virtual_mirror.py
+++ catkin_ws/src/virtual_mirror_valerio/src/virtual_mirror.py
@@ -10,6 +10,7 @@ import cv2
 # Define callback function
 def cam_callback(msg):
 	np_arr = np.fromstring(msg.data, np.uint8)
+	
 	# Decode 
 	image_rgb = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
 

3d3275bfccb8ed463ffbb80e2a6e953931ed1a07
Robert Katzschmann <rkk@csail.mit.edu>
2016-02-25 21:59:12 -0500

    updated the fix_wifi.sh script to fix a bug in it

--- fix_wifi.sh
+++ fix_wifi.sh
@@ -4,22 +4,21 @@
 
 set -e
 
-if grep -q "trusty-proposed" /etc/apt/sources.list
+if grep -q "$(lsb_release -sc)-proposed" /etc/apt/sources.list
 then
     echo "you already got trusty-proposed repos enabled!"
 else
     echo "adding trusty-proposed!"
-    echo 'deb http://ports.ubuntu.com trusty-proposed restricted main multiverse universe' >> /etc/apt/sources.list
+    sudo sh -c 'echo "deb http://ports.ubuntu.com $(lsb_release -sc)-proposed restricted main multiverse universe" > /etc/apt/sources.list'
 fi
 
 if [ -f /etc/apt/preferences.d/proposed-updates ];
 then
     echo "you already have a preference file in place!"
 else
-    echo 'Package: *\nPin: release a=trusty-proposed\nPin-Priority: 400\n' >>  /etc/apt/preferences.d/proposed-updates
+    sudo sh -c $'echo "Package: *\nPin: release a=trusty-proposed\nPin-Priority: 400\n" >  /etc/apt/preferences.d/proposed-updates'
 fi
 
-#dpkg -s ifupdown | grep Version
 sudo apt-get update
 sudo apt-get install ifupdown/trusty-proposed
 

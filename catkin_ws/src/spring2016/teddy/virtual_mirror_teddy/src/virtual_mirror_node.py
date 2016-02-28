7afe460e1754f489fd072f0a6ad79954b8501a5b
Teddy Ort <teddy@mit.edu>
2016-02-28 06:40:55 -0500

    Adds virtual mirror node for M02-RCDP4

---
+++ catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image, CompressedImage
+import numpy as np
+from cv_bridge import CvBridge
+import cv2
+
+# Virtual Mirror Node
+# Author: Teddy Ort
+# Inputs: Image
+# Outputs: Image - A horizontal reflection of the input image
+# This node was created as part of the M02-RCDP module
+
+class VirtualMirrorNode(object):
+    def __init__(self):
+        self.node_name = 'Virtual Mirror'
+        self.sub = rospy.Subscriber("~image_in", CompressedImage, self.processImage)
+        self.pub = rospy.Publisher("~image_out", Image, queue_size=1)
+        self.bridge = CvBridge()
+        rospy.loginfo("%s has started", self.node_name)
+
+    def processImage(self, image_msg):
+        #rospy.loginfo('Image Received')
+        image_cv = cv2.imdecode(np.fromstring(image_msg.data, np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+        image_cv=image_cv[:,::-1]
+        image_msg_out = self.bridge.cv2_to_imgmsg(image_cv, "bgr8")
+        image_msg_out.header.stamp = image_msg.header.stamp
+        self.pub.publish(image_msg_out)
+
+if __name__ == '__main__':
+    rospy.init_node('virtual_mirror', anonymous=False)
+    virtual_mirror_node = VirtualMirrorNode()
+    rospy.spin()
+

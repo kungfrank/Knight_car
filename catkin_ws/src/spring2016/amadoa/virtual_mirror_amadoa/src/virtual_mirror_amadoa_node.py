9574bb2ce5cff35f957a8a078c83dc7320eca4e0
Amado Antonini <amadoa@mit.edu>
2016-03-04 11:37:51 -0500

    updadted virtual_mirror_amadoa_node.py to use cv2.flip

--- catkin_ws/src/spring2016/amadoa/virtual_mirror_amadoa/src/virtual_mirror_amadoa_node.py
+++ catkin_ws/src/spring2016/amadoa/virtual_mirror_amadoa/src/virtual_mirror_amadoa_node.py
@@ -6,19 +6,21 @@ from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 from numpy import array
 
+def setupParam(param_name,default_value):
+	value = rospy.get_param(param_name,default_value)
+	rospy.set_param(param_name,value) #Write to parameter server for transparancy
+	rospy.loginfo("[virtual_mirror_amadoa_node] %s = %s " %(param_name,value))
+	return value
+
+flip_direction = setupParam("~flip_direction", "horz") # "horz" or "vert"
 
 def imageCallback(msg):
-	# rospy.loginfo("imagaCallback called")
-
 	#### direct conversion to CV2 ####
 	np_arr = np.fromstring(msg.data, np.uint8)
 	image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-	# print('image_np_dimensions: %s x %s x %s' % (len(image_np), len(image_np[0]), len(image_np[0][0])))
 
-	# print image_np[0] # result [[35 120 108] [35 120 108] [35 120 108] ...
-
-	image_np_out = array([row[::-1] for row in image_np])
-
+	flipCode = 1 if flip_direction == "horz" else 0
+	image_np_out = cv2.flip(image_np, flipCode)
 
 
 	#### Create CompressedIamge ####
@@ -30,6 +32,7 @@ def imageCallback(msg):
 
 	# Publish new image
 	publisher.publish(out)
+
 
 def mirror():
 	global publisher

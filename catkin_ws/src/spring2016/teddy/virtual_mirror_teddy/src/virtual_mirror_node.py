f348b57296ea8e9fd4fd9e56a8add9e3b457d866
Teddy Ort <teddy@mit.edu>
2016-03-08 17:31:29 -0500

    Adds the setupParam function to the virtual_mirror_node

--- catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
+++ catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
@@ -8,7 +8,7 @@ import cv2
 # Virtual Mirror Node
 # Author: Teddy Ort
 # Inputs: Image
-# Outputs: Image - A horizontal reflection of the input image
+# Outputs: Image - A reflection of the input image
 # This node was created as part of the M02-RCDP module
 
 class VirtualMirrorNode(object):
@@ -26,6 +26,12 @@ class VirtualMirrorNode(object):
         image_msg_out = self.bridge.cv2_to_imgmsg(image_cv, "bgr8")
         image_msg_out.header.stamp = image_msg.header.stamp
         self.pub.publish(image_msg_out)
+
+        def setupParam(self,param_name,default_value):
+            value = rospy.get_param(param_name,default_value)
+            rospy.set_param(param_name,value) #Write to parameter server for transparancy
+            rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+            return value
 
 if __name__ == '__main__':
     rospy.init_node('virtual_mirror', anonymous=False)

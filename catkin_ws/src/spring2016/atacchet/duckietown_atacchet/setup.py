6343f0353debbed4ca7a7b94ba07c7de5bbb90ee
Andrea Tacchetti <atacchet@mit.edu>
2016-03-08 14:17:37 -0500

    msg works param is broken

--- catkin_ws/src/spring2016/atacchet/duckietown_atacchet/setup.py
+++ catkin_ws/src/spring2016/atacchet/duckietown_atacchet/setup.py
@@ -5,7 +5,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-		packages=['duckietown_msgs_atacchet'],
-		package_dir={'': 'include'},
+    packages=['duckietown_atacchet'],
+    package_dir={'': 'include'},
 )
+
 setup(**setup_args)
+

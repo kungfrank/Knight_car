ee1fd17eaf34734e660e1a83defe885e39695720
Teddy Ort <teddy@mit.edu>
2016-03-09 01:12:29 -0500

    Adds duckietown_msg_tedd package with a msg file for FlipDirection

--- catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
+++ catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image, CompressedImage
+from duckietown_msg_teddy.msg import FlipDirection
 import numpy as np
 from cv_bridge import CvBridge
 import cv2
@@ -17,7 +18,10 @@ class VirtualMirrorNode(object):
         self.flip_direction     = self.setupParam("~flip_direction", 'vert')
         self.sub = rospy.Subscriber("~image_in", CompressedImage, self.processImage)
         self.pub = rospy.Publisher("~image_out", Image, queue_size=1)
+        self.param_pub = rospy.Publisher("~flip_direction", FlipDirection, queue_size=1)
+        self.param_msg = FlipDirection()
         self.bridge = CvBridge()
+        self.param_timer = rospy.Timer(rospy.Duration.from_sec(1.0),self.cbParamTimer)
         rospy.loginfo("%s has started", self.node_name)
 
     def processImage(self, image_msg):
@@ -28,6 +32,11 @@ class VirtualMirrorNode(object):
         image_msg_out = self.bridge.cv2_to_imgmsg(image_cv, "bgr8")
         image_msg_out.header.stamp = image_msg.header.stamp
         self.pub.publish(image_msg_out)
+
+    def cbParamTimer(self, event):
+        self.flip_direction = rospy.get_param("~flip_direction", 1.0)
+        self.param_msg.flip_direction = FlipDirection.HORZ if self.flip_direction == 'horz' else FlipDirection.VERT
+        self.param_pub.publish(self.param_msg)
 
     def setupParam(self,param_name,default_value):
         value = rospy.get_param(param_name,default_value)

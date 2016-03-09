ca2281b4f29be68d5ab38c848a38ee16c1236342
Teddy Ort <teddy@mit.edu>
2016-03-09 00:27:24 -0500

    Adds a parameter to choose direction of flip in virtual_mirror

--- catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
+++ catkin_ws/src/spring2016/teddy/virtual_mirror_teddy/src/virtual_mirror_node.py
@@ -14,6 +14,7 @@ import cv2
 class VirtualMirrorNode(object):
     def __init__(self):
         self.node_name = 'Virtual Mirror'
+        self.flip_direction     = self.setupParam("~flip_direction", 'vert')
         self.sub = rospy.Subscriber("~image_in", CompressedImage, self.processImage)
         self.pub = rospy.Publisher("~image_out", Image, queue_size=1)
         self.bridge = CvBridge()
@@ -22,16 +23,17 @@ class VirtualMirrorNode(object):
     def processImage(self, image_msg):
         #rospy.loginfo('Image Received')
         image_cv = cv2.imdecode(np.fromstring(image_msg.data, np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
-        image_cv=image_cv[:,::-1]
+        flipCode = 0 if self.flip_direction == 'vert' else 1
+        cv2.flip(image_cv, flipCode, image_cv)
         image_msg_out = self.bridge.cv2_to_imgmsg(image_cv, "bgr8")
         image_msg_out.header.stamp = image_msg.header.stamp
         self.pub.publish(image_msg_out)
 
-        def setupParam(self,param_name,default_value):
-            value = rospy.get_param(param_name,default_value)
-            rospy.set_param(param_name,value) #Write to parameter server for transparancy
-            rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
-            return value
+    def setupParam(self,param_name,default_value):
+        value = rospy.get_param(param_name,default_value)
+        rospy.set_param(param_name,value) #Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+        return value
 
 if __name__ == '__main__':
     rospy.init_node('virtual_mirror', anonymous=False)

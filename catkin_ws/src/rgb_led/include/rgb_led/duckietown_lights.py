2dad067ade9a8383d6f7de8945a1a1afce8503d5
Andrea Censi (@emma) <censi@mit.edu>
2016-03-10 23:34:24 +0000

    done

--- catkin_ws/src/rgb_led/include/rgb_led/duckietown_lights.py
+++ catkin_ws/src/rgb_led/include/rgb_led/duckietown_lights.py
@@ -8,20 +8,21 @@ FRONT_RIGHT = 4
 
 # Do not change the constants: add your own colors or sequence
 
-RED = [0.24, 0, 0]
 
-s = 0.5
+s = 1.0
+RED = [s, 0, 0]
 GREEN = [0, s, 0]
 WHITE = [s, s, s]
 BLUE = [0, 0, s]
+YELLOW = [0, s, s]
 
+WHITE = [s, s, s]
 GREEN2 = [0, 0.3, 0]
 
 
 OFF = [0, 0, 0]
 
 s = 0.5
-WHITE = [s, s, s]
 
 s = 0.8
 WHITE2 = [s, s, s]
@@ -34,6 +35,12 @@ lightseq1 = [
 lightseq2 = [
 	(0.25, {TOP: GREEN2, BACK_LEFT:RED, BACK_RIGHT:RED, FRONT_LEFT:WHITE2,FRONT_RIGHT:WHITE2}),	
 	(0.25, {TOP: OFF, BACK_LEFT:OFF, BACK_RIGHT:OFF, FRONT_LEFT:WHITE2,FRONT_RIGHT:WHITE2}),
+]
+
+
+lightseq3 = [
+	(0.25, {TOP: GREEN, BACK_LEFT:GREEN2, BACK_RIGHT:[0,1,1], FRONT_LEFT:YELLOW,FRONT_RIGHT:WHITE}),	
+	(0.25, {TOP: RED, BACK_LEFT:GREEN2, BACK_RIGHT:RED, FRONT_LEFT:RED,FRONT_RIGHT:RED}),
 ]
 
 def cycle_LEDs(sequence):
---
+++ catkin_ws/src/rgb_led/script/blinking3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from rgb_led import *
+
+
+cycle_LEDs(lightseq3)

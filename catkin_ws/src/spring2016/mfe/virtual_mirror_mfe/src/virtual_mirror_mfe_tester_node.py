785b263eb677d19604f08f807f4e615e24916824
Michael Everett <mfe@mit.edu>
2016-03-10 19:12:36 -0500

    Finished up assignment with virtual mirror launch files

--- catkin_ws/src/spring2016/mfe/virtual_mirror_mfe/src/virtual_mirror_mfe_tester_node.py
+++ catkin_ws/src/spring2016/mfe/virtual_mirror_mfe/src/virtual_mirror_mfe_tester_node.py
@@ -3,11 +3,22 @@ import rospy
 import numpy as np
 from duckietown_msgs.msg import SegmentList, Segment
 
-class LaneFilterTesterNode(object):
+class VirtualMirrorTesterNode(object):
     def __init__(self):
         self.pub_compressed_image = rospy.Publisher("~compressed_image", CompressedImage, queue_size=1)
+	# Get raw image from decoder node
 
+	# Set flip direction to horizontal
+	
+	# Flip the image according to flip direction using virtual_mirror_mfe_node
 
+	# Compress the flipped image from virtual_mirror_mfe_node
+
+	# Confirm that it's the same as the test file
+
+	# change flip direction to vertical
+
+	# repeat above steps
 
 
 

219d32655245df9b4352b3c67a5d6878f4510e86
Amado Antonini <amadoa@mit.edu>
2016-03-11 17:42:52 -0500

    installed rpg_svo

---
+++ cmake_ws/fast/build/CMakeFiles/fast_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fast_test.dir/test/test.cpp.o"
+  "fast_test.pdb"
+  "fast_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fast_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

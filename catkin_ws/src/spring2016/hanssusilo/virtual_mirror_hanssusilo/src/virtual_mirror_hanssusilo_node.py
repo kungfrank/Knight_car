7d081a430462967a16ca11bb5ab7de7af9e8897c
hanssusilo <hsusilo@mit.edu>
2016-03-14 12:52:19 -0400

    finished lab 04

--- catkin_ws/src/spring2016/hanssusilo/virtual_mirror_hanssusilo/src/virtual_mirror_hanssusilo_node.py
+++ catkin_ws/src/spring2016/hanssusilo/virtual_mirror_hanssusilo/src/virtual_mirror_hanssusilo_node.py
@@ -3,13 +3,20 @@
 import rospy
 import cv2
 from sensor_msgs.msg import CompressedImage, Image
-from virtual_mirror_hanssusilo.msg import FlipDir
+from duckietown_msg_hanssusilo.msg import FlipDir
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
 # Define callback function
 def callback(msg):
-	imgcv = cv2.imdecode(np.fromstring(msg.data, np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+	flip = rospy.get_param("/penguin/virtual_mirror_hanssusilo_node/flip_direction")
+	if flip == 'vert':
+		flipi = FlipDir.vert
+	elif flip == "horz":
+		flipi = FlipDir.horz
+	#print flipi
+	imgcv = bridge.imgmsg_to_cv2(msg, "bgr8")
+	#cv2.imdecode(np.fromstring(msg.data, np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
 	flippedcv = cv2.flip(imgcv,flipi)
 	imgout = bridge.cv2_to_imgmsg(flippedcv, "bgr8")
 	imgout.header.stamp = msg.header.stamp
@@ -26,17 +33,17 @@ def paramTimerCB(event):
 	flip_dir_publisher.publish(msg)
 
 # Initialize the node with rospy
-rospy.init_node('virtual_mirror_node')
-# Create publisher
-publisher = rospy.Publisher("~img_topic_out",Image,queue_size=1)
-# Create publisher
-flip_dir_publisher = rospy.Publisher("flip_dir",FlipDir,queue_size=1)
+rospy.init_node('virtual_mirror_hanssusilo_node')
 # Create subscriber
-subscriber = rospy.Subscriber("~img_topic_in", Image, callback)
+subscriber = rospy.Subscriber("img_orig", Image, callback)
+# Create publisher
+publisher = rospy.Publisher("img_flipped",Image,queue_size=1)
+# Create publisher
+flip_dir_publisher = rospy.Publisher("~flip_dir",FlipDir,queue_size=1)
 # Create bridge
 bridge = CvBridge()
 # initialize parameter 
-flip = rospy.set_param("/penguin/virtual_mirror_hanssusilo_node/flip_direction","vert")
+flip = rospy.get_param("/penguin/virtual_mirror_hanssusilo_node/flip_direction")
 # timer to publish param
 if flip == 'vert':
 	flipi = FlipDir.vert

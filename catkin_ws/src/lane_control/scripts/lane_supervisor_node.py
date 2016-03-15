2f830b82ba2219bf0f8d5d0c523c58e76cd1b063
Liam Paull Test <lpaull@mit.edu>
2016-03-15 15:50:20 +0000

    lane supervisor v0

--- catkin_ws/src/lane_control/scripts/lane_supervisor_node.py
+++ catkin_ws/src/lane_control/scripts/lane_supervisor_node.py
@@ -2,7 +2,9 @@
 import rospy
 import numpy as np
 import math
+from std_msgs.msg import Bool
 from duckietown_msgs.msg import CarControl, WheelsCmdStamped, LanePose
+
 
 class lane_supervisor(object):
     def __init__(self):
@@ -15,10 +17,12 @@ class lane_supervisor(object):
         # Params:
         self.max_cross_track_error=self.setupParameter("~max_cross_track_error",0.1)
         self.max_heading_error=self.setupParameter("~max_heading_error",math.pi/4)
+        self.max_speed=self.setupParameter("~max_speed",1.0)
+        self.max_steer=self.setupParameter("~max_steer",0.2)
 
         # Publicaiton
-        # self.pub_lane_cmd = rospy.Publisher("~lane_control",CarControl,queue_size=1)
         self.pub_wheels_cmd = rospy.Publisher("~wheels_control",WheelsCmdStamped,queue_size=1)
+        self.pub_safe       = rospy.Publisher("~safe",Bool,queue_size=1)
 
         # Subscriptions
         self.sub_lane_pose    = rospy.Subscriber("~lane_pose", LanePose, self.cbLanePose, queue_size=1)
@@ -37,41 +41,46 @@ class lane_supervisor(object):
         cross_track_err = math.fabs(lane_pose_msg.d) 
         heading_err = math.fabs(lane_pose_msg.phi)
         if cross_track_err > self.max_cross_track_error or heading_err > self.max_heading_error:
-            safe = False
+            self.safe = False
         else:
-            safe = True
-        
+            self.safe = True
+        self.pub_safe.publish(self.safe)
+
     def cbLaneControl(self,lane_control_msg):
         self.lane_control = lane_control_msg
 
     def cbJoyControl(self,joy_control_msg):
         self.joy_control = joy_control_msg
-        wheels_cmd_msg = mergeJoyAndLaneControl()
+        wheels_cmd_msg = self.mergeJoyAndLaneControl()
         self.pub_wheels_cmd.publish(wheels_cmd_msg)
 
     def mergeJoyAndLaneControl(self):
         wheels_cmd_msg = WheelsCmdStamped()
         if self.safe:
-            wheels_cmd_msg = self.joy_control
+            car_control_joy = self.wheelsCmdToCarControl(self.joy_control)
+            car_control_joy.speed = min(car_control_joy.speed,self.max_speed)
+            car_control_joy.steering = min(car_control_joy.steering, self.max_steer)
+            wheels_cmd_msg = self.carControlToWheelsCmd(car_control_joy)
         else:
             car_control_joy = self.wheelsCmdToCarControl(self.joy_control)
             car_control_lane = self.wheelsCmdToCarControl(self.lane_control)
             car_control_merged = CarControl()
-            car_control_merged.speed = car_control_joy.speed # take the speed from the joystick 
-            car_control_merged.heading = car_control_lane.heading # take the heading from the lane controller
+            car_control_merged.speed = min(car_control_joy.speed,self.max_speed) # take the speed from the joystick 
+            car_control_merged.steering = min(car_control_lane.steering,self.max_steer) # take the heading from the lane controller
             wheels_cmd_msg = self.carControlToWheelsCmd(car_control_merged)
+            print wheels_cmd_msg
         return wheels_cmd_msg
 
     def wheelsCmdToCarControl(self,wheels_cmd):
         car_control = CarControl()
         car_control.speed = 0.5*(wheels_cmd.vel_right + wheels_cmd.vel_left)
-        car_control.heading = 0.5(wheels_cmd.vel_right - wheels_cmd.vel_left)
+        car_control.steering = 0.5*(wheels_cmd.vel_right - wheels_cmd.vel_left)
         return car_control
 
     def carControlToWheelsCmd(self,car_control):
         wheels_cmd = WheelsCmdStamped()
-        wheels_cmd.vel_right = car_control.speed + car_control.heading
-        wheels_cmd.vel_left  = car_control.speed - car_control.heading
+        wheels_cmd.vel_right = car_control.speed + car_control.steering
+        wheels_cmd.vel_left  = car_control.speed - car_control.steering
         return wheels_cmd
 
 if __name__ == "__main__":

4009df19ab4e6533f28167e0d9df378460ae7bbe
qlai <qlai@mit.edu>
2016-03-16 11:55:30 -0400

    fixed bugs in kmeans

--- catkin_ws/src/spring2016/f1/kmeans.py
+++ catkin_ws/src/spring2016/f1/kmeans.py
@@ -16,7 +16,7 @@ np.random.seed(5)
 
 IMG_PATH = '''IMG PATH'''
 
-cv_img = cv2.imread("strip.jpg")
+cv_img = cv2.imread("test3.jpg")
 # print cv_img.shape
 
 # class GetKMeansModel(object):
@@ -39,10 +39,10 @@ def getimgdatapts(cv2img):
 def runKMeans():
 	testdata = getimgdatapts(cv_img)
 	kmc = KMeans(n_clusters = NUM_COLORS, max_iter = 100)
-	kmc.fit(testdata)
+	kmc.fit_predict(testdata)
 	trained_centers = kmc.cluster_centers_
-	# print trained_centers
-	# print CENTERS
+	print trained_centers
+	print CENTERS
 	return trained_centers
 
 
@@ -56,7 +56,7 @@ def identifyColors(trained, true):
 		for j, truecolor in enumerate(true):
 			matching[i][j] = np.linalg.norm(color - truecolor)
 
-	# print matching
+	print matching
 	colormap= {}
 	for i, color in enumerate(matching):
 		colormap[i] = np.argmin(color)
@@ -64,40 +64,33 @@ def identifyColors(trained, true):
 	return colormap
 
 def getparameters(mapping, trained, true):
-	redX = []
-	redY = []
-	greenX = []
-	greenY = []
-	blueX = []
-	blueY = []
+	redX = np.zeros((3, 1))
+	redY = np.zeros((3, 1))
+	greenX = np.zeros((3, 1))
+	greenY = np.zeros((3, 1))
+	blueX = np.zeros((3, 1))
+	blueY = np.zeros((3, 1))
 	# print type(redY), redX
 	for i, color in enumerate(true):
 		mapped = mapping[i]
 		# print i, color
 		for j, index in enumerate(color):
-			# print index
+			# print index, j
 			if j == 0:
-				redX.append(index)
+				redY[i] = index
 			if j == 1:
-				greenX.append(index)
+				greenY[i] = index
 			if j == 2:
-				blueX.append(index)
+				blueY[i] = index
 		for j2, index2 in enumerate(trained[mapped]):
 			# print j2, index2
 			if j2 == 0:
-				redY.append(index2)
+				redX[i] = index2
 			if j2 == 1:
-				greenY.append(index2)
+				greenX[i] = index2
 			if j2 == 2:
-				blueY.append(index2)
+				blueX[i] = index2
 	# print redX
-	np.asarray(redX).T
-	np.asarray(redY).T
-	np.asarray(greenX).T
-	np.asarray(greenY).T
-	np.asarray(blueX).T
-	np.asarray(blueY).T
-	print type(redY), redX
 	RED = linear_model.LinearRegression()
 	BLUE = linear_model.LinearRegression()
 	GREEN = linear_model.LinearRegression()
@@ -107,6 +100,7 @@ def getparameters(mapping, trained, true):
 	print RED.coef_, RED.intercept_
 	print BLUE.coef_, BLUE.intercept_
 	print GREEN.coef_, GREEN.intercept_
+	return (RED.coef_, RED.intercept_), (BLUE.coef_, BLUE.intercept_), (GREEN.coef_, GREEN.intercept_)
 
 
 

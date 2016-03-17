1fe8a948df860de32e659ae8bae17ffdf5eea714
Andrea Censi <censi@mit.edu>
2016-03-17 00:40:03 +0000

    change explanation

--- environment.sh
+++ environment.sh
@@ -18,6 +18,7 @@ echo "Activating development."
 source $DUCKIETOWN_ROOT/catkin_ws/devel/setup.bash
 
 # TODO: check that the time is >= 2015
-# TODO: run a python script that checks all arguments
+
+# TODO: run a python script that checks all libraries are installed
 
 exec "$@" #Passes arguments. Need this for ROS remote launching to work.

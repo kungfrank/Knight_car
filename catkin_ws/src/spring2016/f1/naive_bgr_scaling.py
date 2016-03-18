9ac4a60555d978049bae22ce3ae7aa48b2fd192a
Tristan Swedish <tswedish@mit.edu>
2016-03-18 10:46:21 -0400

    update naive scaling to work better if imported

--- catkin_ws/src/spring2016/f1/naive_bgr_scaling.py
+++ catkin_ws/src/spring2016/f1/naive_bgr_scaling.py
@@ -1,34 +1,76 @@
+from __future__ import print_function
+
 import numpy as np
 import cv2
-
-import sys
+import kmeans
 
 # Load an color image in grayscale
-c1 = 420
-r1 = 230
-cref = [50.0,50.0,50.0]
+#cref = [50.0,50.0,50.0]
+def refPatchScale(image_fname, cref):
+	img_orig = cv2.imread(image_fname)
+	h = img_orig.shape[0]
+	w = img_orig.shape[1]
 
-img_orig = cv2.imread(sys.argv[1])
-img = img_orig[c1:c1+25,r1:r1+25,:]
+	c1 = 250
+	r1 = 320
+	img_patch = img_orig[c1:c1+50,r1:r1+100,:]
+	hp = img_patch.shape[0]
+	wp = img_patch.shape[1]
 
-#m_mean = cref
+	# just for testing
+	cv2.imwrite("img_patch.jpg",img_patch)
 
-# can do in one line with np?
-m_mean = np.zeros(3)
-m_mean[0] = cv2.mean(img[:,:,0])[0]
-m_mean[1] = cv2.mean(img[:,:,1])[0]
-m_mean[2] = cv2.mean(img[:,:,2])[0]
+	m_mean = np.zeros(3)
+	m_mean = np.mean(np.reshape(img_patch,[wp*hp,3]),0)
 
-print(m_mean)
+	print(m_mean)
 
-mscale = np.array(cref) / np.array(m_mean)
+	mscale = np.array(cref) / np.array(m_mean)
 
-print(mscale)
+	print(mscale)
 
-# reshape to do cleaner multiply
-img_scale = img_orig
-img_scale[:,:,0] = img_orig[:,:,0]*mscale[0]
-img_scale[:,:,1] = img_orig[:,:,1]*mscale[1]
-img_scale[:,:,2] = img_orig[:,:,2]*mscale[2]
+	# reshape to do fast multiply
+	img_scale = np.reshape(img_orig,[h*w,3])
+	img_scale = np.reshape(img_scale*mscale,[h,w,3])
 
-cv2.imwrite(sys.argv[2],img_scale)
+	return img_scale
+
+def scaleandshift(img,scale,shift):
+	h = img.shape[0]
+	w = img.shape[1]
+
+	img_scale = np.reshape(img,[h*w,3])
+	img_scale = np.reshape(img_scale*np.array(scale),[h,w,3])
+
+	img_shift = np.reshape(img_scale,[h*w,3])
+	img_shift = np.reshape(img_shift+np.array(shift),[h,w,3])
+
+	return img_shift
+
+def refPatchShift(image_fname,cref):
+	img_orig = cv2.imread(image_fname)
+	h = img_orig.shape[0]
+	w = img_orig.shape[1]
+
+	c1 = 300
+	r1 = 300
+	img_patch = img_orig[c1:c1+100,r1:r1+100,:]
+	hp = img_patch.shape[0]
+	wp = img_patch.shape[1]
+
+	m_mean = np.zeros(3)
+	m_mean = np.mean(np.reshape(img_patch,[wp*hp,3]),0)
+
+	print(m_mean)
+
+	mshift = np.array(cref) - np.array(m_mean)
+
+	print(mshift)
+
+	img_shift = np.reshape(img_orig,[h*w,3])
+	img_shift = np.reshape(img_shift+mshift,[h,w,3])
+
+	return img_shift
+
+
+# cv2.imwrite("fname_out.jpg",img_scale_n_shift)

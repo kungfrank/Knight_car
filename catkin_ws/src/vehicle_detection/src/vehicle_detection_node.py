f676cdb01cb812084b4658f91a17c692e9c53598
Andrea Tacchetti <atacchet@mit.edu>
2016-03-21 18:01:52 -0400

    added stopit to kill findChessboardCorners if it takes too long

--- catkin_ws/src/vehicle_detection/src/vehicle_detection_node.py
+++ catkin_ws/src/vehicle_detection/src/vehicle_detection_node.py
@@ -10,6 +10,7 @@ import cv2
 import io
 import numpy as np
 import rospy
+import stopit
 import threading
 
 class VehicleDetectionNode(object):
@@ -49,37 +50,36 @@ class VehicleDetectionNode(object):
 			if not self.camera_initialized:
 				self.lock.unlock()
 				return
-
 			image_cv = cv2.imdecode(np.fromstring(image_msg.data, np.uint8), 
 					cv2.CV_LOAD_IMAGE_COLOR)
 			rospy.loginfo("Image Shape: [%d x %d]." % 
 					(image_cv.shape[0], image_cv.shape[1]))
-
-			rectified_image = cv2.copyMakeBorder(image_cv, 0, 0, 0, 0, 
-					cv2.BORDER_REPLICATE)
-			self.pcm.rectifyImage(image_cv, rectified_image)
-			start = rospy.Time.now()
-			(detection, corners) = cv2.findChessboardCorners(rectified_image, 
-					(5, 7))
-			rospy.loginfo("find chessboard corners took: %.3f", 
-					(rospy.Time.now() - start).to_sec())
-			cv2.drawChessboardCorners(rectified_image, (5, 7), corners, detection)
-			image_msg_out = self.bridge.cv2_to_imgmsg(rectified_image, "bgr8")
+			with stopit.ThreadingTimeout(0.2) as to_ctx_mgr:
+				start = rospy.Time.now()
+				(detection, corners) = cv2.findChessboardCorners(image_cv, 
+						(5, 7))
+				rospy.loginfo("find chessboard corners took: %.3f", 
+						(rospy.Time.now() - start).to_sec())
+			if not to_ctx_mgr:
+				rospy.loginfo("find chessboard corners took too long")
+				self.lock.unlock()
+				return
+			cv2.drawChessboardCorners(image_cv, (5, 7), corners, detection)
+			image_msg_out = self.bridge.cv2_to_imgmsg(image_cv, "bgr8")
 			self.pub_chessboard_image.publish(image_msg_out)
-
 			if not detection:
 				self.lock.unlock()
 				rospy.loginfo("Corners not found")
 				return
-			
 			# publish debug chessboard image
 			rospy.loginfo("Corners FOUND")
-
 			corners_msg_out = VehicleCorners()
 			for i in np.arange(corners.shape[0]):
 				p = Point32()
-				p.x = corners[i][0][0]
-				p.y = corners[i][0][1]
+				rect_p = self.pcm.rectifyPoint((corners[i][0][0], 
+						corners[i][0][1]))
+				p.x = rect_p[0]
+				p.y = rect_p[1]
 				corners_msg_out.corners.append(deepcopy(p))
 			self.pub_corners.publish(corners_msg_out)
 			self.lock.unlock()

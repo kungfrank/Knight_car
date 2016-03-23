2f3b8ea34e0ddd945563c85da66d3845f5c1fed9
Andrea Tacchetti <atacchet@mit.edu>
2016-03-23 16:43:57 -0400

    removed camera calibration from vehicle detection -- corners msg has boolean for detection

--- catkin_ws/src/vehicle_detection/src/vehicle_detection_node.py
+++ catkin_ws/src/vehicle_detection/src/vehicle_detection_node.py
@@ -2,7 +2,6 @@
 from copy import deepcopy
 from cv_bridge import CvBridge, CvBridgeError
 from duckietown_msgs.msg import VehicleCorners
-from image_geometry import PinholeCameraModel
 from geometry_msgs.msg import Point32
 from mutex import mutex
 from sensor_msgs.msg import CameraInfo, CompressedImage, Image
@@ -23,16 +22,9 @@ class VehicleDetectionNode(object):
 				VehicleCorners, queue_size=1)
 		self.pub_chessboard_image = rospy.Publisher("~chessboard_image", 
 				Image, queue_size=1)
-		self.sub_camera_info = rospy.Subscriber("~camera_info", CameraInfo,
-				self.cbCameraInfo, queue_size=1)
-		self.pcm = PinholeCameraModel()
-		self.camera_initialized = False
 		self.lock = mutex()
 		rospy.loginfo("Initialization of [%s] completed" % (self.node_name))
 	
-	def cbCameraInfo(self, info_msg):
-		self.lock.lock(self.processCameraInfo, info_msg)
-		
 	def cbImage(self, image_msg):
 		# Start a daemon thread to process the image
 		thread = threading.Thread(target=self.processImage,args=(image_msg,))
@@ -40,16 +32,9 @@ class VehicleDetectionNode(object):
 		thread.start()
 		# Returns rightaway
 	
-	def processCameraInfo(self, info_msg):
-		self.camera_initialized = True
-		self.pcm.fromCameraInfo(info_msg)
-		self.lock.unlock()
-		
 	def processImage(self, image_msg):
 		if self.lock.testandset():
-			if not self.camera_initialized:
-				self.lock.unlock()
-				return
+			corners_msg_out = VehicleCorners()
 			image_cv = cv2.imdecode(np.fromstring(image_msg.data, np.uint8), 
 					cv2.CV_LOAD_IMAGE_COLOR)
 			rospy.loginfo("Image Shape: [%d x %d]." % 
@@ -62,24 +47,26 @@ class VehicleDetectionNode(object):
 						(rospy.Time.now() - start).to_sec())
 			if not to_ctx_mgr:
 				rospy.loginfo("find chessboard corners took too long")
+				corners_msg_out.detection = False
+				self.pub_corners.publish(corners_msg_out)
 				self.lock.unlock()
 				return
 			cv2.drawChessboardCorners(image_cv, (5, 7), corners, detection)
 			image_msg_out = self.bridge.cv2_to_imgmsg(image_cv, "bgr8")
 			self.pub_chessboard_image.publish(image_msg_out)
 			if not detection:
+				corners_msg_out.detection = False
+				self.pub_corners.publish(corners_msg_out)
 				self.lock.unlock()
 				rospy.loginfo("Corners not found")
 				return
 			# publish debug chessboard image
 			rospy.loginfo("Corners FOUND")
-			corners_msg_out = VehicleCorners()
+			corners_msg_out.detection = True
 			for i in np.arange(corners.shape[0]):
 				p = Point32()
-				rect_p = self.pcm.rectifyPoint((corners[i][0][0], 
-						corners[i][0][1]))
-				p.x = rect_p[0]
-				p.y = rect_p[1]
+				p.x = corners[i][0][0]
+				p.y = corners[i][0][1]
 				corners_msg_out.corners.append(deepcopy(p))
 			self.pub_corners.publish(corners_msg_out)
 			self.lock.unlock()

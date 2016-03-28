6899f75adddd49afdafd10750e33f5e8d3dff761
Robert Katzschmann <rkk@csail.mit.edu>
2016-03-28 16:29:55 -0400

    synced wheels driver files, launch file and joystick launch file for this wheelsdriver

---
+++ catkin_ws/src/dagu_car/src/wheels_driver_sync_node.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import rospy
+from duckietown_msgs.msg import WheelsCmdStamped
+from dagu_car.dagu_wheels_driver import DaguWheelsDriver
+from sensor_msgs.msg import CameraInfo
+
+class WheelsDriverAdvancedNode(object):
+    def __init__(self):
+        self.node_name = rospy.get_name()
+        rospy.loginfo("[%s] Initializing " %(self.node_name))
+
+        #internal state of left and right velocity
+        self.vel_left = 0.
+        self.vel_right = 0.
+
+        # Setup publishers
+        self.driver = DaguWheelsDriver()
+        #add publisher for wheels command wih execution time
+        self.pub_topic = rospy.Publisher("~wheels_cmd_executed",WheelsCmdStamped, self.cbWheelsCmd, queue_size=1)
+
+        # Setup subscribers
+        self.sub_topic = rospy.Subscriber("~wheels_cmd", WheelsCmdStamped, self.cbWheelsCmd, queue_size=1)
+        # subscribe to camera info
+        self.sub_camerainfo =  rospy.Subscriber("~camera_info", CameraInfo, self.cbCamInfo, queue_size=1)
+
+
+    def setupParam(self,param_name,default_value):
+        value = rospy.get_param(param_name,default_value)
+        rospy.set_param(param_name,value) #Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+        return value
+    
+    def cbCamInfo(self,msg):
+     #synchronization between video frames and set wheelspeed
+     self.driver.setWheelsSpeed(left=self.vel_left,right=self.vel_right)
+     self.lastFrameTimeStamp = msg.header.stamp
+
+    def cbWheelsCmd(self,msg):
+     #to allow synchronization just store commanded values for now     
+     self.vel_left = msg.vel_left
+     self.vel_right = msg.vel_right	 
+     #republish original message now with updated time stamp
+     msg_whl_cmd_ex = msg
+     msg_whl_cmd_ex.header.stamp = rospy.Time.now()
+     self.pub_topic.publish(msg_whl_cmd_ex)       
+
+    
+    def on_shutdown(self):
+        self.driver.setWheelsSpeed(left=0.0,right=0.0)
+        rospy.loginfo("[%s] Shutting down."%(rospy.get_name()))
+
+if __name__ == '__main__':
+    # Initialize the node with rospy
+    rospy.init_node('wheels_driver_node', anonymous=False)
+    # Create the DaguCar object
+    node = WheelsDriverAdvancedNode()
+    # Setup proper shutdown behavior 
+    rospy.on_shutdown(node.on_shutdown)
+    # Keep it spinning to keep the node alive
+    rospy.spin()

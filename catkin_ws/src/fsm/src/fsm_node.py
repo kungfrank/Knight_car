5feab096db3d548400296f5fadaccaf44c036649
Shih-Yuan Liu <syliu@mit.edu>
2016-03-29 12:53:38 -0400

    fsm_node now handles active nodes too.

--- catkin_ws/src/fsm/src/fsm_node.py
+++ catkin_ws/src/fsm/src/fsm_node.py
@@ -8,8 +8,7 @@ class FSMNode(object):
         self.node_name = rospy.get_name()
 
         # Build transition dictionray
-        self.transition_dict = rospy.get_param("~transitions")
-        rospy.loginfo("[%s] Defined states: %s" %(self.node_name,self.transition_dict.keys()))
+        self.states_dict = rospy.get_param("~states")
 
         # Setup initial state
         self.state_msg = FSMState()
@@ -17,10 +16,15 @@ class FSMNode(object):
         self.state_msg.header.stamp = rospy.Time.now()
         # Setup publisher and publish initial state
         self.pub_state = rospy.Publisher("~state",FSMState,queue_size=1,latch=True)
-        self.publishState()
     
         # Provide service
         self.srv_state = rospy.Service("~set_state",SetFSMState,self.cbSrvSetState)
+
+        # Construct publishers
+        self.pub_dict = dict()
+        nodes = rospy.get_param("~nodes")
+        for node_name, topic_name in nodes.items():
+            self.pub_dict[node_name] = rospy.Publisher(topic_name, BoolStamped, queue_size=1, latch=True)
 
         # Construct subscribers
         param_events_dict = rospy.get_param("~events")
@@ -29,34 +33,74 @@ class FSMNode(object):
         for event_name, topic_name in param_events_dict.items():
             self.sub_list.append(rospy.Subscriber("%s"%(topic_name), BoolStamped, self.cbEvent, callback_args=event_name))
 
+        # Publish initial state
+        self.publish()
+
+    def _getNextState(self, state_name, event_name):
+        state_dict = self.states_dict.get(state_name)
+        if state_dict is None:
+            rospy.logwarn("[%s] %s not defined. Treat as terminal. "%(self.node_name,state_name))
+            return None
+        else:
+            if "transitions" in state_dict.keys():
+                next_state = state_dict["transitions"].get(event_name)
+            else:
+                next_state = None
+            return next_state
+                
+
+    def _getActiveNodes(self,state_name):
+        state_dict = self.states_dict.get(state_name)
+        if state_dict is None:
+            rospy.logwarn("[%s] %s not defined. Treat as terminal. "%(self.node_name,state_name))
+            return None
+        else:
+            active_nodes = state_dict.get("active_nodes")
+            if active_nodes is None:
+                rospy.logwarn("[%s] No active nodes defined for %s. Deactive all nodes."%(self.node_name,state_name))
+                active_nodes = []
+            return active_nodes
+
+    def publish(self):
+        self.publishBools()
+        self.publishState()
+
     def cbSrvSetState(self,req):
         self.state_msg.header.stamp = rospy.Time.now()
         self.state_msg.state = req.state
-        self.publishState()
+        self.publish()
         return SetFSMStateResponse()
 
     def publishState(self):
         self.pub_state.publish(self.state_msg)
         rospy.loginfo("[%s] FSMState: %s" %(self.node_name, self.state_msg.state))
 
+    def publishBools(self):
+        msg = BoolStamped()
+        msg.header.stamp = self.state_msg.header.stamp
+        active_nodes = self._getActiveNodes(self.state_msg.state)
+        if active_nodes is None:
+            active_nodes = []
+
+        for node_name, node_pub in self.pub_dict.items():
+            msg.data = False
+            node_state = "OFF"
+            if node_name in active_nodes:
+                msg.data = True
+                node_state = "ON"
+            node_pub.publish(msg)
+            rospy.loginfo("[%s] Node %s set to %s." %(self.node_name, node_name, node_state))
+
     def cbEvent(self,msg,event_name):
         if (msg.data):
             # Update timestamp
             rospy.loginfo("[%s] Event: %s" %(self.node_name,event_name))
             self.state_msg.header.stamp = msg.header.stamp
-            # Load transitions of the current state
-            trans_dict = self.transition_dict.get(self.state_msg.state)
-            # rospy.loginfo("[%s] Transiniton %s" %(self.node_name,trans_dict))
-            if trans_dict is None:
-                # current state is not defined  
-                rospy.logwarn("[%s] %s is terminal state."%(self.node_name,self.state_msg.state))
-                # rospy.signal_shutdown("Undefined state.")
-            else:
-                next_state = trans_dict.get(event_name)
-                if next_state is not None:
-                    # Has transition for the event
-                    self.state_msg.state = next_state
-                    self.publishState()
+            next_state = self._getNextState(self.state_msg.state,event_name)
+            if next_state is not None:
+                # Has a defined transition
+                self.state_msg.state = next_state
+                self.publish()
 
     def on_shutdown(self):
         rospy.loginfo("[%s] Shutting down." %(self.node_name))

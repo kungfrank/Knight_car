0d0d47b7b273b4e796394ed5e74415e45969c26d
Shih-Yuan Liu <syliu@mit.edu>
2016-03-30 18:26:59 -0400

    Configurable wheels_cmd_switch_node with default.yaml.

--- catkin_ws/src/dagu_car/src/wheels_cmd_switch_node.py
+++ catkin_ws/src/dagu_car/src/wheels_cmd_switch_node.py
@@ -1,62 +1,33 @@
 #!/usr/bin/env python
 import rospy
-#from duckietown_msgs.msg import WheelsCmdStamped, ControlMode
 from duckietown_msgs.msg import WheelsCmdStamped, FSMState
-
 class WheelsCmdSwitchNode(object):
     def __init__(self):
         self.node_name = rospy.get_name()
         rospy.loginfo("[%s] Initializing " %(self.node_name))
+        # Read parameters
+        self.mappings = rospy.get_param("~mappings")
+        source_topic_dict = rospy.get_param("~source_topics")
+        self.current_src_name = None
+
+        # Construct publisher
+        self.pub_cmd = rospy.Publisher("~wheels_cmd",WheelsCmdStamped,queue_size=1)
         
-        self.mode_msg = FSMState()
-        self.mode_msg.state = FSMState.LANE_FOLLOWING
-        self.cmd_dict = {}
-        self.cmd_dict[FSMState.LANE_FOLLOWING] = None
-        self.cmd_dict[FSMState.INTERSECTION_CONTROL] = None
-        self.cmd_dict[FSMState.COORDINATION] = None
+        # Construct subscribers
+        self.sub_fsm_state = rospy.Subscriber(rospy.get_param("~mode_topic"),FSMState,self.cbFSMState)
 
-        self.mode_name_dict = {}
-        self.mode_name_dict[FSMState.LANE_FOLLOWING] = "LANE_FOLLOWING"
-        self.mode_name_dict[FSMState.INTERSECTION_CONTROL] = "INTERSECTION_CONTROL"
-        self.mode_name_dict[FSMState.COORDINATION] = "COORDINATION_CONTROL"
+        self.sub_dict = dict()
+        for src_name, topic_name in source_topic_dict.items():
+            self.sub_dict[src_name] = rospy.Subscriber(topic_name,WheelsCmdStamped,self.cbWheelsCmd,callback_args=src_name)
 
-        # Setup publishers
-        self.pub_wheels_cmd = rospy.Publisher("~wheels_cmd", WheelsCmdStamped, queue_size=1)
+    def cbFSMState(self,fsm_state_msg):
+        self.current_src_name = self.mappings.get(fsm_state_msg.state)
+        if self.current_src_name is None:
+            rospy.logwarn("[%s] FSMState %s not handled. No msg pass through the switch." %(self.node_name,fsm_state_msg.state))
 
-        # Setup subscribers
-        self.sub_mode = rospy.Subscriber("~mode", FSMState, self.cbMode, queue_size=1)
-        self.sub_lane = rospy.Subscriber("~wheels_cmd_lane", WheelsCmdStamped, self.cbWheelsCmd, queue_size=1, callback_args=FSMState.LANE_FOLLOWING)
-        self.sub_interestion = rospy.Subscriber("~wheels_cmd_intersection", WheelsCmdStamped, self.cbWheelsCmd, queue_size=1, callback_args=FSMState.INTERSECTION_CONTROL)
-        self.sub_coordination = rospy.Subscriber("~wheels_cmd_coordination", WheelsCmdStamped, self.cbWheelsCmd, queue_size=1, callback_args=FSMState.COORDINATION)
-
-    def pubWheelsCmd(self):
-        cmd_msg = self.cmd_dict[self.mode_msg.state]
-        if cmd_msg is None:
-            cmd_msg = WheelsCmdStamped()
-            cmd_msg.vel_left=0.0
-            cmd_msg.vel_right=0.0
-            cmd_msg.header.stamp = rospy.Time.now()
-        self.pub_wheels_cmd.publish(cmd_msg)
-
-    def cbMode(self,mode_msg):
-        if not self.mode_msg.state == mode_msg.state:
-            rospy.loginfo("[%s] Switching to %s" %(self.node_name,self.mode_name_dict[mode_msg.state]))
-        self.mode_msg = mode_msg
-        # Always publish a cmd when changing mode.
-        self.pubWheelsCmd()
-
-    def cbWheelsCmd(self,cmd_msg,cb_args):
-        # Save the cmd_msg 
-        self.cmd_dict[cb_args] = cmd_msg
-        # Publish if the received cmd channel matches the current mode
-        if cb_args == self.mode_msg.state:
-            self.pubWheelsCmd()
-
-    # def setupParam(self,param_name,default_value):
-    #     value = rospy.get_param(param_name,default_value)
-    #     rospy.set_param(param_name,value) #Write to parameter server for transparancy
-    #     rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
-    #     return value
+    def cbWheelsCmd(self,msg,src_name):
+        if src_name == self.current_src_name:
+            self.pub_cmd.publish(msg)
 
     def on_shutdown(self):
         rospy.loginfo("[%s] Shutting down." %(self.node_name))

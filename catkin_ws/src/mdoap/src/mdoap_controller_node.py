12346c1001b89d2f51e76b9e64a43e0b34b534db
Catherine Liu <catliu@mit.edu>
2016-03-30 18:42:49 -0400

    starting to work on mdoap

--- catkin_ws/src/mdoap/src/mdoap_controller_node.py
+++ catkin_ws/src/mdoap/src/mdoap_controller_node.py
@@ -3,14 +3,20 @@ import rospy
 from duckietown_msgs.msg import WheelsCmdStamped, BoolStamped, ObstacleProjectedDetectionList, ObstacleProjectedDetection
 
 class MDOAPControllerNode:
-	def __init__(self):
-		self.name = 'mdoap_controller_node'
+    def __init__(self):
+        self.name = 'mdoap_controller_node'
         rospy.loginfo('[%s] started', self.name)
         self.sub_close = rospy.Subscriber("~too_close", BoolStamped, self.cbBool, queue_size=1)
         self.sub_detections = rospy.Subscriber("~detection_list_proj", ObstacleProjectedDetectionList, self.cbBool, queue_size=1)
         self.pub_wheels_cmd = rospy.Publisher("~control",WheelsCmdStamped,queue_size=1)
 
     def cbBool(self, bool_msg):
+        if bool_msg.data:
+            pass
+            #Hijack the param for seting offset of the lane
+        else:
+            pass
+            #Reset offset of lane to 0
         stop = WheelsCmdStamped()
         stop.header = bool_msg.header
         stop.vel_left = 0.0
@@ -18,6 +24,6 @@ class MDOAPControllerNode:
         self.pub_wheels_cmd.publish(stop)
 
 if __name__=="__main__":
-	rospy.init_node('mdoap_controller_node')
-	node = MDOAPControllerNode()
-	rospy.spin()
+    rospy.init_node('mdoap_controller_node')
+    node = MDOAPControllerNode()
+    rospy.spin()
--- catkin_ws/src/mdoap/src/static_object_detector_tester_node.py
+++ catkin_ws/src/mdoap/src/static_object_detector_tester_node.py
@@ -14,14 +14,14 @@ class StaticObjectDetectorTesterNode(object):
         self.pub_fake_images = rospy.Publisher("static_object_detector_node/image_compressed", CompressedImage, queue_size=1)
         rospy.sleep(1)
 
-        #take in directory of images as input, use 01_orig.png, 02, 03, 04
+        #take in directory of images as input, use 01.png, 02.png, etc
         self.im_dir = self.setupParam("~im_dir", "")
         self.num_im = self.setupParam("~num_im", 0)
 
         self.bridge = CvBridge()
         self.sub_detections = rospy.Subscriber("static_object_detector_node/detection_list", ObstacleImageDetectionList, self.checkDetection)
         #start the first image
-        self.num_sent = 1
+        self.num_sent = 0
         rospy.sleep(1)
         self.pubImage()
 
@@ -34,6 +34,7 @@ class StaticObjectDetectorTesterNode(object):
     def checkDetection(self, detections):
         #TODO(CL) Do some duckie/cone matching by hand and find image coordinates
         if True:
+            rospy.loginfo(detections)
             rospy.loginfo("[%s] Detection %s passed" %(self.node_name,self.num_sent))
         else:
             rospy.loginfo("[%s] Detection %s failed" %(self.node_name,self.num_sent))
@@ -43,13 +44,13 @@ class StaticObjectDetectorTesterNode(object):
         self.pubImage()
 
     def pubImage(self):
-        if self.num_sent<=self.num_im:
-            filename = '0'+str(self.num_sent)+'_orig.png'
+        if self.num_sent<self.num_im:
+            filename = '0'+str(self.num_sent)+'.jpg'
             image = cv2.imread(self.im_dir+filename)
             msg = CompressedImage()
             msg.header.stamp = rospy.Time.now()
-            msg.format = 'png'
-            msg.data = np.array(cv2.imencode('.png', image)[1]).tostring()
+            msg.format = 'jpg'
+            msg.data = np.array(cv2.imencode('.jpg', image)[1]).tostring()
             self.pub_fake_images.publish(msg)
             rospy.loginfo("[%s] Image %s published" %(self.node_name,filename))
             self.num_sent+=1
@@ -58,7 +59,7 @@ class StaticObjectDetectorTesterNode(object):
         rospy.loginfo("[%s] Shutdown." %(self.node_name))
 
 if __name__ == '__main__':
-    rospy.init_node('static_object_detector_node',anonymous=False)
-    static_object_detector_node = StaticObjectDetectorTesterNode()
-    rospy.on_shutdown(static_object_detector_node.onShutdown)
+    rospy.init_node('static_object_detector_tester_node',anonymous=False)
+    static_object_detector_tester_node = StaticObjectDetectorTesterNode()
+    rospy.on_shutdown(static_object_detector_tester_node.onShutdown)
     rospy.spin()

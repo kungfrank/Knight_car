fd2fd4248907379adea52c94604ae0ca6ae38cda
Wyatt Ubellacker <wubella@mit.edu>
2016-03-30 12:08:50 -0400

    Bug fix, adding lane pose calculation

--- catkin_ws/src/f4-devel/visual_odometry/src/odometry_training_pairs_node.py
+++ catkin_ws/src/f4-devel/visual_odometry/src/odometry_training_pairs_node.py
@@ -14,25 +14,30 @@ class OdometryTrainingPairsNode(object):
         self.in_lane = False
         self.current_wheels_cmd = WheelsCmdStamped()
         self.old_stop_line_msg = StopLineReading()
+        self.old_lane_pose_msg = LanePose()
 
         ## publishers and subscribers
         self.sub_stop_lin_reading = rospy.Subscriber("~stop_line_reading", StopLineReading, self.stopLineCB)
         self.sub_lane_pose = rospy.Subscriber("~lane_pose", LanePose, self.lanePoseCB)
         self.sub_wheels_cmd_executed = rospy.Subscriber("~wheels_cmd_executed", WheelsCmdStamped, self.wheelsCmdCB)
-        self.pub_theta_dot_sample = rospy.Publisher("~theta_dot_sample", Vsample, queue_size=1)
+        self.pub_v_sample = rospy.Publisher("~v_sample", Vsample, queue_size=1)
         rospy.loginfo('[odometry_training_pairs_node] Initiated')
 
     def stopLineCB(self, stop_line_msg):
         if self.in_lane and self.current_wheels_cmd and stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.secs <= 1.0 and self.old_stop_line_msg:
-            theta_dot_sample_msg = Vsample()
-            theta_dot_sample_msg.d_L = self.current_wheels_cmd.vel_left
-            theta_dot_sample_msg.d_R = self.current_wheels_cmd.vel_right
-            theta_dot_sample_msg.dt = stop_line_msg.header.stamp.secs + stop_line_msg.header.stamp.nsecs/1e9 - self.old_stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.nsecs/1e9
-            theta_dot_sample_msg.theta_angle_pose_delta = 0
-            theta_dot_sample_msg.x_axis_pose_delta = 0
-            theta_dot_sample_msg.y_axis_pose_delta = self.old_stop_line_msg.stop_line_point.y - stop_line_msg.stop_line_point.y
-            self.pub_theta_dot_sample.publish(theta_dot_sample_msg)
-            self.old_stop_line_msg = stop_line_msg
+            v_sample_msg = Vsample()
+            v_sample_msg.d_L = self.current_wheels_cmd.vel_left
+            v_sample_msg.d_R = self.current_wheels_cmd.vel_right
+            #I think 
+            v_sample_msg.dt = stop_line_msg.header.stamp.secs + stop_line_msg.header.stamp.nsecs/1e9 - self.old_stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.nsecs/1e9
+            v_sample_msg.theta_angle_pose_delta = 0
+            #x is the direction the robot is aiming, negative since it is measuring stop line x-coor old - new = -(new-old)
+            v_sample_msg.x_axis_pose_delta = self.old_stop_line_msg.stop_line_point.x - stop_line_msg.stop_line_point.x
+            #not sure what the y axis from the stop_line detection actually means
+            v_sample_msg.y_axis_pose_delta = 0
+            self.pub_v_sample.publish(v_sample_msg)
+        #should reset the old msg regardless of whether the deltas were calculated or not
+        self.old_stop_line_msg = stop_line_msg
 
     def lanePoseCB(self, lane_pose_msg):
         self.in_lane = lane_pose_msg.in_lane
@@ -42,6 +47,18 @@ class OdometryTrainingPairsNode(object):
 
     def processLanePose(self, lane_pose_msg):
         self.lane_pose = lane_pose_msg
+        if self.in_lane and self.current_wheels_cmd and lane_pose_msg.header.stamp.secs - self.old_lane_pose_msg.header.stamp.secs <= 1.0 and self.old_lane_pose_msg:
+            v_sample_msg = Vsample()
+            v_sample_msg.d_L = self.current_wheels_cmd.vel_left
+            v_sample_msg.d_R = self.current_wheels_cmd.vel_right
+            v_sample_msg.dt = stop_line_msg.header.stamp.secs + stop_line_msg.header.stamp.nsecs/1e9 - self.old_stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.nsecs/1e9
+            #new - old since measure robot angle
+            v_sample_msg.theta_angle_pose_delta = lane_pose_msg.phi - self.old_lane_pose_msg.phi
+            #is actually (delta d)*cos(phi), but is probably pretty noisy since we get error from both d and phi
+            v_sample_msg.x_axis_pose_delta = (lane_pose_msg.d - self.old_lane_pose_msg.d)*cos(lane_pose_msg.phi)
+            v_sample_msg.y_axis_pose_delta = 0
+            self.pub_v_sample.publish(v_sample_msg)
+        self.old_lane_pose_msg = lane_pose_msg
 
     def onShutdown(self):
         rospy.loginfo("[odometry_training_pairs_node] Shutdown.")

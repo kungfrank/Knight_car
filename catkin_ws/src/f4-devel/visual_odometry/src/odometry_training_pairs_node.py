8dbb15aba48ca16e93d362be8700f3715a45ff9f
Amado Antonini <amadoa@mit.edu>
2016-04-01 11:17:00 -0400

    cleans up comments a bit and updates params to use param file

--- catkin_ws/src/f4-devel/visual_odometry/src/odometry_training_pairs_node.py
+++ catkin_ws/src/f4-devel/visual_odometry/src/odometry_training_pairs_node.py
@@ -11,21 +11,19 @@ class OdometryTrainingPairsNode(object):
     def __init__(self):
         self.node_name = "Odometry Training Pairs"
 
-        ## Parameters TODO make param file and read from there
-        self.stop_line_max_age = 1.0 # [sec] time don't use stop_line_reading msg pairs more than this far apart
-        self.lane_pose_max_age = 1.0 # [sec] time don't use lane_pose msg pairs more than this far apart
-        self.wheels_cmd_max_age = 2.0 # [sec] throw away wheels_cmd's older than this
-
+        ## Parameters
+        self.stop_line_max_age = self.setupParameter("~stop_line_max_age",1.0) # [sec] don't use stop_line_reading msg pairs more than this far apart
+        self.lane_pose_max_age = self.setupParameter("~lane_pose_max_age", 1.0) # [sec] don't use lane_pose msg pairs more than this far apart
+        self.wheels_cmd_max_age = self.setupParameter("~wheels_cmd_max_age", 2.0) # [sec] throw away wheels_cmd's older than this
 
         ## state vars
         self.in_lane = False
         self.current_wheels_cmd = WheelsCmdStamped()
         self.old_stop_line_msg = StopLineReading()
         self.old_lane_pose_msg = LanePose()
-        # create buffer of wheel commands
-        self.cmd_buffer = deque()
+        self.cmd_buffer = deque() # buffer of wheel commands
 
-        self.v_sample_msg = Vsample()
+        self.v_sample_msg = Vsample() # global variable because different parts of this are set in different callbacks
 
         ## publishers and subscribers
         self.sub_stop_lin_reading = rospy.Subscriber("~stop_line_reading", StopLineReading, self.stopLineCB)
@@ -35,36 +33,38 @@ class OdometryTrainingPairsNode(object):
         self.pub_theta_dot_sample = rospy.Publisher("~theta_dot_sample", ThetaDotSample, queue_size=1)
         rospy.loginfo('[odometry_training_pairs_node] Initiated')
 
+    def setupParameter(self,param_name,default_value):
+        value = rospy.get_param(param_name,default_value)
+        rospy.set_param(param_name,value) #Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+        return value
+
     def stopLineCB(self, stop_line_msg):
         # only execute if in lane and if the old message exists and is not too old
         if self.in_lane and hasattr(self.old_stop_line_msg, 'header') and (stop_line_msg.header.stamp - self.old_stop_line_msg.header.stamp).to_sec() <= self.stop_line_max_age:
             (d_L, d_R) = self.findMatchingDuties(stop_line_msg.header.stamp)
             self.v_sample_msg.d_L = d_L
             self.v_sample_msg.d_R = d_R
-            # self.v_sample_msg.dt = stop_line_msg.header.stamp.secs + stop_line_msg.header.stamp.nsecs/1e9 - self.old_stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.nsecs/1e9
             self.v_sample_msg.dt = (stop_line_msg.header.stamp - self.old_stop_line_msg.header.stamp).to_sec()
-            #x is the direction the robot is aiming, negative since it is measuring stop line x-coor old - new = -(new-old)
+            # x is the direction the robot is aiming, negative since it is measuring stop line x-coor old - new = -(new-old)
             self.v_sample_msg.x_axis_pose_delta = (self.old_stop_line_msg.stop_line_point.x - stop_line_msg.stop_line_point.x)/math.cos(self.lane_pose.phi)
-            #not sure what the y axis from the stop_line detection actually means
-            #self.v_sample_msg.y_axis_pose_delta = 0
             self.pub_v_sample.publish(self.v_sample_msg)
-        #should reset the old msg regardless of whether the deltas were calculated or not
+        # reset the old msg regardless of whether the deltas were calculated or not
         self.old_stop_line_msg = stop_line_msg
 
     def wheelsCmdCB(self, wheels_cmd_msg):
         # append new cmd to the right of the deque
         self.cmd_buffer.append(wheels_cmd_msg)
-        # throw away cmd_msgs older than 2 seconds to keep the buffer small
+        # throw away cmd_msgs older than max_age seconds to keep the buffer small
         while (wheels_cmd_msg.header.stamp - self.cmd_buffer[0].header.stamp).to_sec() > self.wheels_cmd_max_age:
             self.cmd_buffer.popleft()
 
     def findMatchingDuties(self, new_stamp):
-        # if no cmds have been received, return (0, 0)
+        # if no cmds have been received, return (0, 0). This shouldn't happen unless there's no wheels_cmd publiser
         if not self.cmd_buffer:
             return (0, 0)
         cmd = None
         # delete cmds in the buffer older than new_stamp
-        # while len(self.cmd_buffer) and (self.cmd_buffer[0].header.stamp.secs + self.cmd_buffer[0].header.stamp.nsecs/1e9) < (new_stamp.secs + new_stamp.nsecs/1e9):
         while len(self.cmd_buffer) and (new_stamp - self.cmd_buffer[0].header.stamp).to_sec() > 0:
             cmd = self.cmd_buffer.popleft()
         if cmd:
@@ -78,7 +78,7 @@ class OdometryTrainingPairsNode(object):
     def lanePoseCB(self, lane_pose_msg):
         self.lane_pose = lane_pose_msg
         self.in_lane = lane_pose_msg.in_lane
-        # only execute if in lane and if the old message exists and is not too old
+        # only execute if in_lane and if the old message exists and is not too old
         if self.in_lane and hasattr(self.old_lane_pose_msg, 'header') and (lane_pose_msg.header.stamp - self.old_lane_pose_msg.header.stamp).to_sec() <= self.lane_pose_max_age:
             theta_dot_sample_msg = ThetaDotSample()
             (d_L, d_R) = self.findMatchingDuties(lane_pose_msg.header.stamp)

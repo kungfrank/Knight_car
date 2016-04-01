a32349dcd502dc191184b8027d6e6dc5cfceda20
Amado Antonini <amadoa@mit.edu>
2016-04-01 09:29:26 -0400

    creates params and changes dt calculation to use .to_sec()

--- catkin_ws/src/f4-devel/visual_odometry/src/odometry_training_pairs_node.py
+++ catkin_ws/src/f4-devel/visual_odometry/src/odometry_training_pairs_node.py
@@ -11,6 +11,12 @@ class OdometryTrainingPairsNode(object):
     def __init__(self):
         self.node_name = "Odometry Training Pairs"
 
+        ## Parameters TODO make param file and read from there
+        self.stop_line_max_age = 1.0 # [sec] time don't use stop_line_reading msg pairs more than this far apart
+        self.lane_pose_max_age = 1.0 # [sec] time don't use lane_pose msg pairs more than this far apart
+        self.wheels_cmd_max_age = 2.0 # [sec] throw away wheels_cmd's older than this
+
+
         ## state vars
         self.in_lane = False
         self.current_wheels_cmd = WheelsCmdStamped()
@@ -31,11 +37,12 @@ class OdometryTrainingPairsNode(object):
 
     def stopLineCB(self, stop_line_msg):
         # only execute if in lane and if the old message exists and is not too old
-        if self.in_lane and hasattr(self.old_stop_line_msg, 'header') and stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.secs <= 1.0:
+        if self.in_lane and hasattr(self.old_stop_line_msg, 'header') and (stop_line_msg.header.stamp - self.old_stop_line_msg.header.stamp).to_sec() <= self.stop_line_max_age:
             (d_L, d_R) = self.findMatchingDuties(stop_line_msg.header.stamp)
             self.v_sample_msg.d_L = d_L
             self.v_sample_msg.d_R = d_R
-            self.v_sample_msg.dt = stop_line_msg.header.stamp.secs + stop_line_msg.header.stamp.nsecs/1e9 - self.old_stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.nsecs/1e9
+            # self.v_sample_msg.dt = stop_line_msg.header.stamp.secs + stop_line_msg.header.stamp.nsecs/1e9 - self.old_stop_line_msg.header.stamp.secs - self.old_stop_line_msg.header.stamp.nsecs/1e9
+            self.v_sample_msg.dt = (stop_line_msg.header.stamp - self.old_stop_line_msg.header.stamp).to_sec()
             #x is the direction the robot is aiming, negative since it is measuring stop line x-coor old - new = -(new-old)
             self.v_sample_msg.x_axis_pose_delta = (self.old_stop_line_msg.stop_line_point.x - stop_line_msg.stop_line_point.x)/math.cos(self.lane_pose.phi)
             #not sure what the y axis from the stop_line detection actually means
@@ -48,7 +55,7 @@ class OdometryTrainingPairsNode(object):
         # append new cmd to the right of the deque
         self.cmd_buffer.append(wheels_cmd_msg)
         # throw away cmd_msgs older than 2 seconds to keep the buffer small
-        while wheels_cmd_msg.header.stamp.secs - self.cmd_buffer[0].header.stamp.secs > 1:
+        while (wheels_cmd_msg.header.stamp - self.cmd_buffer[0].header.stamp).to_sec() > self.wheels_cmd_max_age:
             self.cmd_buffer.popleft()
 
     def findMatchingDuties(self, new_stamp):
@@ -57,7 +64,8 @@ class OdometryTrainingPairsNode(object):
             return (0, 0)
         cmd = None
         # delete cmds in the buffer older than new_stamp
-        while len(self.cmd_buffer) and (self.cmd_buffer[0].header.stamp.secs + self.cmd_buffer[0].header.stamp.nsecs/1e9) < (new_stamp.secs + new_stamp.nsecs/1e9):
+        # while len(self.cmd_buffer) and (self.cmd_buffer[0].header.stamp.secs + self.cmd_buffer[0].header.stamp.nsecs/1e9) < (new_stamp.secs + new_stamp.nsecs/1e9):
+        while len(self.cmd_buffer) and (new_stamp - self.cmd_buffer[0].header.stamp).to_sec() > 0:
             cmd = self.cmd_buffer.popleft()
         if cmd:
             # append last popped cmd back into buffer on the left to make sure we always have at least one cmd
@@ -71,12 +79,13 @@ class OdometryTrainingPairsNode(object):
         self.lane_pose = lane_pose_msg
         self.in_lane = lane_pose_msg.in_lane
         # only execute if in lane and if the old message exists and is not too old
-        if self.in_lane and hasattr(self.old_lane_pose_msg, 'header') and lane_pose_msg.header.stamp.secs - self.old_lane_pose_msg.header.stamp.secs <= 1.0:
+        if self.in_lane and hasattr(self.old_lane_pose_msg, 'header') and (lane_pose_msg.header.stamp - self.old_lane_pose_msg.header.stamp).to_sec() <= self.lane_pose_max_age:
             theta_dot_sample_msg = ThetaDotSample()
             (d_L, d_R) = self.findMatchingDuties(lane_pose_msg.header.stamp)
             theta_dot_sample_msg.d_L = d_L
             theta_dot_sample_msg.d_R = d_R
-            theta_dot_sample_msg.dt = lane_pose_msg.header.stamp.secs + lane_pose_msg.header.stamp.nsecs/1e9 - self.old_lane_pose_msg.header.stamp.secs - self.old_lane_pose_msg.header.stamp.nsecs/1e9
+            # theta_dot_sample_msg.dt = (lane_pose_msg.header.stamp.secs + lane_pose_msg.header.stamp.nsecs/1e9) - (self.old_lane_pose_msg.header.stamp.secs + self.old_lane_pose_msg.header.stamp.nsecs/1e9)
+            theta_dot_sample_msg.dt = (lane_pose_msg.header.stamp - self.old_lane_pose_msg.header.stamp).to_sec()
             #new - old since measure robot angle
             theta_dot_sample_msg.theta_angle_pose_delta = lane_pose_msg.phi - self.old_lane_pose_msg.phi
             #is actually (delta d)*cos(phi), but is probably pretty noisy since we get error from both d and phi

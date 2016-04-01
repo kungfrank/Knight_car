7f913bcc0f87b94f1212c6a78b02297f6e2909e2
Bethany LaPenta <lapentab@mit.edu>
2016-04-01 15:32:45 +0000

    Add working led_emitter node

--- catkin_ws/src/led_emitter/src/led_emitter_node_test.py
+++ catkin_ws/src/led_emitter/src/led_emitter_node_test.py
@@ -3,21 +3,21 @@ import rospy
 from rgb_led import *
 import sys
 import time
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 import random
 
 class LEDEmitterTest(object):
     def __init__(self):
         self.node_name = rospy.get_name()
-        self.pub_state = rospy.Publisher("~change_state",String,queue_size=1)
-        self.sub_state = rospy.Subscriber("~led_state",String, self.changeState)
+        self.pub_state = rospy.Publisher("~change_to_state",Float32,queue_size=10)
+        self.sub_state = rospy.Subscriber("~current_led_state",Float32, self.changeState)
         self.pub_timer = rospy.Timer(rospy.Duration.from_sec(5.0),self.cycleTimer)
-        self.state_list = ['blinking1', 'blinking2', 'blinking3', 'mar12special'] # From duckietown_lights.py
+        self.state_list = [1, 2, .5, .8] # From duckietown_lights.py
 
     def cycleTimer(self,event):
         state = random.choice(self.state_list)
         self.pub_state.publish(state)
-        rospy.loginfo("Testing state " + state)
+        rospy.loginfo("Testing state " + str(state))
 
 
     def changeState(self,msg):

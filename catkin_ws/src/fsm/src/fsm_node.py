1547419c236c5c58b6317468b019e507eb176d7d
Andrea Censi <censi@mit.edu>
2016-04-09 18:02:48 -0400

    minor changes

--- catkin_ws/src/fsm/src/fsm_node.py
+++ catkin_ws/src/fsm/src/fsm_node.py
@@ -7,31 +7,36 @@ class FSMNode(object):
     def __init__(self):
         self.node_name = rospy.get_name()
 
-        # Build transition dictionray
+        # Build transition dictionary
         self.states_dict = rospy.get_param("~states")
+        # validate type, just in case
+        assert isinstance(self.states_dict, dict), type(self.states_dict)
 
         # Setup initial state
         self.state_msg = FSMState()
         self.state_msg.state = rospy.get_param("~initial_state")
         self.state_msg.header.stamp = rospy.Time.now()
         # Setup publisher and publish initial state
-        self.pub_state = rospy.Publisher("~mode",FSMState,queue_size=1,latch=True)
+        self.pub_state = rospy.Publisher("~mode", FSMState, queue_size=1, latch=True)
     
         # Provide service
-        self.srv_state = rospy.Service("~set_state",SetFSMState,self.cbSrvSetState)
+        self.srv_state = rospy.Service("~set_state", SetFSMState, self.cbSrvSetState)
 
         # Construct publishers
         self.pub_dict = dict()
         nodes = rospy.get_param("~nodes")
         for node_name, topic_name in nodes.items():
-            self.pub_dict[node_name] = rospy.Publisher(topic_name, BoolStamped, queue_size=1, latch=True)
+            p = rospy.Publisher(topic_name, BoolStamped, queue_size=1, latch=True)
+            self.pub_dict[node_name] = p
 
         # Construct subscribers
         param_events_dict = rospy.get_param("~events")
-        self.sub_list = list()
-        self.event_names = list()
+        self.sub_list = []
+        self.event_names = []
         for event_name, topic_name in param_events_dict.items():
-            self.sub_list.append(rospy.Subscriber("%s"%(topic_name), BoolStamped, self.cbEvent, callback_args=event_name))
+            s = rospy.Subscriber("%s" % (topic_name), BoolStamped, self.cbEvent,
+                                  callback_args=event_name)
+            self.sub_list.append(s)
 
         # Publish initial state
         self.publish()
@@ -39,7 +44,8 @@ class FSMNode(object):
     def _getNextState(self, state_name, event_name):
         state_dict = self.states_dict.get(state_name)
         if state_dict is None:
-            rospy.logwarn("[%s] %s not defined. Treat as terminal. "%(self.node_name,state_name))
+            rospy.logwarn("[%s] %s not defined. Treat as terminal. "
+                          % (self.node_name, state_name))
             return None
         else:
             if "transitions" in state_dict.keys():
@@ -52,12 +58,14 @@ class FSMNode(object):
     def _getActiveNodes(self,state_name):
         state_dict = self.states_dict.get(state_name)
         if state_dict is None:
-            rospy.logwarn("[%s] %s not defined. Treat as terminal. "%(self.node_name,state_name))
+            rospy.logwarn("[%s] %s not defined. Treat as terminal. " %
+                          (self.node_name, state_name))
             return None
         else:
             active_nodes = state_dict.get("active_nodes")
             if active_nodes is None:
-                rospy.logwarn("[%s] No active nodes defined for %s. Deactive all nodes."%(self.node_name,state_name))
+                rospy.logwarn("[%s] No active nodes defined for %s. Deactive all nodes."
+                              % (self.node_name, state_name))
                 active_nodes = []
             return active_nodes
 
@@ -73,7 +81,7 @@ class FSMNode(object):
 
     def publishState(self):
         self.pub_state.publish(self.state_msg)
-        rospy.loginfo("[%s] FSMState: %s" %(self.node_name, self.state_msg.state))
+        rospy.loginfo("[%s] FSMState: %s" % (self.node_name, self.state_msg.state))
 
     def publishBools(self):
         msg = BoolStamped()
@@ -89,12 +97,13 @@ class FSMNode(object):
                 msg.data = True
                 node_state = "ON"
             node_pub.publish(msg)
-            rospy.loginfo("[%s] Node %s set to %s." %(self.node_name, node_name, node_state))
+            rospy.loginfo("[%s] Node %s set to %s." %
+                          (self.node_name, node_name, node_state))
 
     def cbEvent(self,msg,event_name):
         if (msg.data):
             # Update timestamp
-            rospy.loginfo("[%s] Event: %s" %(self.node_name,event_name))
+            rospy.loginfo("[%s] Event: %s" % (self.node_name, event_name))
             self.state_msg.header.stamp = msg.header.stamp
             next_state = self._getNextState(self.state_msg.state,event_name)
             if next_state is not None:
@@ -103,7 +112,7 @@ class FSMNode(object):
                 self.publish()
 
     def on_shutdown(self):
-        rospy.loginfo("[%s] Shutting down." %(self.node_name))
+        rospy.loginfo("[%s] Shutting down." % self.node_name)
 
 if __name__ == '__main__':
     # Initialize the node with rospy

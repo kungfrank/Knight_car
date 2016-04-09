22a7b2bbf2aefd4a64475f29efed847d004c2d4a
Tristan Swedish <tswedish@mit.edu>
2016-04-09 17:20:02 -0400

    moved stub test to scripts and import using timeit

--- catkin_ws/src/f1/anti_instagram/scripts/antiinstagram_stub_test.py
+++ catkin_ws/src/f1/anti_instagram/scripts/antiinstagram_stub_test.py
@@ -1,8 +1,9 @@
-from AntiInstagram import *
 import numpy as np
+from anti_instagram.AntiInstagram import *
 import cv2
 import timeit
 
+from duckietown_utils.expand_variables import expand_environment
 
 # calculate transform for each image set
 def testImages(ai, imageset, gtimageset):
@@ -17,6 +18,13 @@ def testImages(ai, imageset, gtimageset):
 		if error > 500:
 			print("Correction seemed to fail for image # "+str(i))
 
+def read_file(filename):
+	filename = expand_environment(filename)
+	img = cv2.imread(filename)
+	if not img:
+		msg = 'Cannot read filename %r.' % filename
+		raise ValueError(msg)
+
 def genRandImg():
 	# determine size of image
 	size = 480,640,3
@@ -27,14 +35,14 @@ def genRandImg():
 
 def applyTransformOnRandomImg():
 	t = timeit.timeit(stmt='ai.applyTransform(img)', 
-		setup='import numpy as np; import AntiInstagram; img = np.array(np.random.random((480,640,3))*255, dtype= np.uint8); ai = AntiInstagram.AntiInstagram()', 
+		setup='import numpy as np; import anti_instagram.AntiInstagram as AntiInstagram; img = np.array(np.random.random((480,640,3))*255, dtype= np.uint8); ai = AntiInstagram.AntiInstagram()', 
 		number=10
 		)
 	print("Averge Apply Transform Took: " + str(t/10.0 * 1000) + " milliseconds")
 
 def calcuateTransformOnRandomImg():
 	t = timeit.timeit(stmt='ai.calculateTransform(img,True)', 
-		setup='import numpy as np; import AntiInstagram; img = np.array(np.random.random((480,640,3))*255, dtype= np.uint8); ai = AntiInstagram.AntiInstagram()', 
+		setup='import numpy as np; import anti_instagram.AntiInstagram as AntiInstagram; img = np.array(np.random.random((480,640,3))*255, dtype= np.uint8); ai = AntiInstagram.AntiInstagram()', 
 		number=10
 		)
 	print("Average Calculate Transform Took: " + str(t/10.0 * 1000) + " milliseconds")

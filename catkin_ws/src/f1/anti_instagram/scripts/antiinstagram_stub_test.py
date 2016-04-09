e04a5f0cd34540309ca964716baacf1aecc9c0a1
Tristan Swedish <tswedish@mit.edu>
2016-04-09 17:10:12 -0400

    updated and removed old stub test

--- catkin_ws/src/f1/anti_instagram/scripts/antiinstagram_stub_test.py
+++ catkin_ws/src/f1/anti_instagram/scripts/antiinstagram_stub_test.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
-from anti_instagram.AntiInstagram import *
+from AntiInstagram import *
+import numpy as np
 import cv2
-from duckietown_utils.expand_variables import expand_environment
+import timeit
 
 
 # calculate transform for each image set
 def testImages(ai, imageset, gtimageset):
 	for i,image in enumerate(imageset):
+		#print(image.shape)
 		ai.calculateTransform(image,True)
 		print ai.health
 		transimage = ai.applyTransform(image)
@@ -16,30 +17,57 @@ def testImages(ai, imageset, gtimageset):
 		if error > 500:
 			print("Correction seemed to fail for image # "+str(i))
 
-
-def read_file(filename):
-	filename = expand_environment(filename)
-	img = cv2.imread(filename)
-	if not img:
-		msg = 'Cannot read filename %r.' % filename
-		raise ValueError(msg)
+def genRandImg():
+	# determine size of image
+	size = 480,640,3
+	# add uniform noise
+	img = np.array(np.random.random(size)*255, dtype= np.uint8)
+	# return random image
 	return img
 
+def applyTransformOnRandomImg():
+	t = timeit.timeit(stmt='ai.applyTransform(img)', 
+		setup='import numpy as np; import AntiInstagram; img = np.array(np.random.random((480,640,3))*255, dtype= np.uint8); ai = AntiInstagram.AntiInstagram()', 
+		number=10
+		)
+	print("Averge Apply Transform Took: " + str(t/10.0 * 1000) + " milliseconds")
+
+def calcuateTransformOnRandomImg():
+	t = timeit.timeit(stmt='ai.calculateTransform(img,True)', 
+		setup='import numpy as np; import AntiInstagram; img = np.array(np.random.random((480,640,3))*255, dtype= np.uint8); ai = AntiInstagram.AntiInstagram()', 
+		number=10
+		)
+	print("Average Calculate Transform Took: " + str(t/10.0 * 1000) + " milliseconds")
+
+
+
+
+
 if __name__ == '__main__':
-	ai = AntiInstagram()
+	#ai = AntiInstagram()
 
+	#### TEST CALC TIMING ####
+	calcuateTransformOnRandomImg()
+	applyTransformOnRandomImg()
+
+	#### TEST SINGLE TRANSFORM ####
+	# For future, incorporate with yaml
 	# load test images
-	imagesetf = [
-		"${DUCKIETOWN_DATA}/phase_2/f1-illum-robust/tristan_manual_dataset1/tristan/frame0001.jpg",
-		"${DUCKIETOWN_DATA}/phase_2/f1-illum-robust/tristan_manual_dataset1/lapentab/frame0002.jpg"
-	]
-
-	gtimagesetf = [
-		"groundtruthimage0.jpg",
-		"groundtruthimage1.jpg"
-	]
-	
-	imageset = filter(read_file, imagesetf)
-	gtimageset = filter(read_file, gtimagesetf)
-
-	testImages(ai, imageset, gtimageset)
+	#imagesetf = [
+	#"/home/tristan/Dropbox/duckietown-data/phase_2/f1-illum-robust/tristan_manual_dataset1/tristan/frame0001.jpg",
+	#"/home/tristan/Dropbox/duckietown-data/phase_2/f1-illum-robust/tristan_manual_dataset1/lapentab/frame0002.jpg"
+	#]
+	#gtimagesetf = [
+	#"groundtruthimage0.jpg",
+	#"groundtruthimage1.jpg"
+	#]
+	#
+	#imageset = []
+	#for imgf in imagesetf:
+	#	img = cv2.imread(imgf)
+	#	imageset.append(img)
+	#gtimageset = []
+	#for imgf in gtimagesetf:
+	#	img = cv2.imread(imgf)
+	#	gtimageset.append(img)
+	#testImages(ai,gtimageset,gtimageset)

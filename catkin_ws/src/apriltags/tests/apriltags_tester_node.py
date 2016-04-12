f2f22e8cc4b3b93ca3f0a30bb0248b222044ba4f
Teddy Ort <teddy@mit.edu>
2016-04-11 21:39:34 -0400

    Refactors the apriltags test to an integration test and a unit test

--- catkin_ws/src/apriltags/tests/apriltags_tester_node.py
+++ catkin_ws/src/apriltags/tests/apriltags_tester_node.py
@@ -3,37 +3,58 @@ import rospy
 import unittest
 import rostest
 from duckietown_msgs.msg import AprilTags
+from sensor_msgs.msg import CameraInfo, Image
+from cv_bridge import CvBridge
+import cv2
+from tf import transformations as tr
 
 class ApriltagsTesterNode(unittest.TestCase):
-    def __init__(self, *args):
-        super(ApriltagsTesterNode, self).__init__(*args)
-        self.msg_tags = AprilTags()
-        self.msg_received = False
-
     def setup(self):
         # Setup the node
-        rospy.init_node('apriltags_tester_node', anonymous=False)
+        rospy.init_node('apriltags_preprocessing_tester_node', anonymous=False)
         self.msg_received = False
+        self.msg_tags = AprilTags()
 
         # Setup the publisher and subscriber
-        self.sub_tag = rospy.Subscriber("~apriltags", AprilTags, self.tagCallback)
+        self.pub_raw  = rospy.Publisher("~image_raw", Image, queue_size=1, latch=True)
+        self.pub_info  = rospy.Publisher("~camera_info", CameraInfo, queue_size=1, latch=True)
+        self.sub_tag = rospy.Subscriber( "~apriltags", AprilTags, self.tagCallback)
 
         # Wait for the node  to finish starting up
         timeout = rospy.Time.now() + rospy.Duration(5) # Wait at most 5 seconds for the node to come up
-        while (self.sub_tag.get_num_connections() < 1) and \
-                not rospy.is_shutdown() and rospy.Time.now() < timeout:
+        while (self.pub_raw.get_num_connections() < 1 or self.pub_info.get_num_connections() < 1 or
+                self.sub_tag.get_num_connections() < 1) and not rospy.is_shutdown() and rospy.Time.now() < timeout:
             rospy.sleep(0.1)
 
-    def tagCallback(self, msg_tag):
-        self.msg_tags = msg_tag
+    def tagCallback(self, msg_tags):
+        self.msg_tags = msg_tags
         self.msg_received = True
 
-    def test_subscriber(self):
+    def test_publisher_and_subscriber(self):
         self.setup()    # Setup the node
+        self.assertGreaterEqual(self.pub_raw.get_num_connections(), 1, "No connections found on image_raw topic")
+        self.assertGreaterEqual(self.pub_info.get_num_connections(), 1, "No connections found on camera_info topic")
         self.assertGreaterEqual(self.sub_tag.get_num_connections(), 1, "No connections found on apriltags topic")
 
-    def test_apriltag_detection(self):
-        self.setup()
+    def test_with_known_image(self):
+        filename = rospy.get_param("~filename")
+        self.setup()    # Setup the node
+
+        # Publish the camera info
+        msg_info = CameraInfo()
+        msg_info.height = 480
+        msg_info.width = 640
+        msg_info.K = [331.026328, 0.0, 319.035097, 0.0, 335.330339, 216.450133, 0.0, 0.0, 1.0]
+        self.pub_info.publish(msg_info)
+
+        # Pause to give the node time to receive the camera info
+        rospy.sleep(1)
+
+        # Publish the test image
+        img = cv2.imread(filename)
+        cvb = CvBridge()
+        msg_raw = cvb.cv2_to_imgmsg(img)
+        self.pub_raw.publish(msg_raw)
 
         # Wait for the message to be received
         timeout = rospy.Time.now() + rospy.Duration(5) # Wait at most 5 seconds for the node to reply
@@ -41,12 +62,23 @@ class ApriltagsTesterNode(unittest.TestCase):
             rospy.sleep(0.1)
         self.assertLess(rospy.Time.now(), timeout, "Waiting for apriltag detection timed out.")
 
-        # Check for the apriltag with id 60
+        # Check that an apriltag with id=60 was detected
         found = False
         for tag in self.msg_tags.detections:
-            found = True if tag.id == 60 else found
+            if tag.id == 60:
+                found = True
+                break
         self.assertTrue(found, "Expected apriltag with id=60 not found.")
 
+        self.assertEqual(tag.id, 60, "Unexpected tag id found.")
+        self.assertAlmostEqual(tag.transform.translation.x, 0.7, delta=0.1) # Allow 10 cm of error margin
+        self.assertAlmostEqual(tag.transform.translation.y, -0.185, delta=0.1) # Allow 10 cm of error margin
+        self.assertAlmostEqual(tag.transform.translation.z, 0.13, delta=0.1) # Allow 10 cm of error margin
+
+        # Convert the quat to an angle about z
+        rot = tag.transform.rotation
+        ang = tr.euler_from_quaternion((rot.x, rot.y, rot.z, rot.w))[2] #z axis angle only
+        self.assertAlmostEqual(ang, 0, delta=15) # Allow up to 15 degrees of error margin
+
 if __name__ == '__main__':
-    rospy.init_node('apriltags_tester_node', anonymous=False)
     rostest.rosrun('apriltags', 'apriltags_tester_node', ApriltagsTesterNode)

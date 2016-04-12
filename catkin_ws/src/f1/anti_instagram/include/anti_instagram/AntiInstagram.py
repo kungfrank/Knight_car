cae57e05228cdf6532c822602d253ef0b9c82cec
Teddy Ort <teddy@mit.edu>
2016-04-12 01:15:33 -0400

    Removes print which degrades performance

--- catkin_ws/src/f1/anti_instagram/include/anti_instagram/AntiInstagram.py
+++ catkin_ws/src/f1/anti_instagram/include/anti_instagram/AntiInstagram.py
@@ -26,7 +26,7 @@ class AntiInstagram():
 
 		# Estimates the scale and shift over multiple frame via an IIR filter with preference towards low-cost frames
 		IIR_weight=1000/(10000+cost)
-		logger.info("cost = %f, IIR_weight = %f" % (cost, IIR_weight))
+		#logger.info("cost = %f, IIR_weight = %f" % (cost, IIR_weight))
 		# self.scale = [r[0][0][0],g[0][0][0],b[0][0][0]]
 		# self.shift = [r[1][0], g[1][0],b[1][0]]
 		deltascale = np.array([r[0][0][0],g[0][0][0],b[0][0][0]])

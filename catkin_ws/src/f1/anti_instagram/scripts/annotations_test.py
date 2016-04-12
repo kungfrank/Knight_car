3c565f82dc709dda8bbded74957a18556f026586
Andrea Censi <censi@mit.edu>
2016-04-12 00:06:15 -0400

    cleanup

--- catkin_ws/src/f1/anti_instagram/scripts/annotations_test.py
+++ catkin_ws/src/f1/anti_instagram/scripts/annotations_test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-from anti_instagram import logger, wrap_test_main
+from anti_instagram import logger, wrap_test_main, AntiInstagram
 from duckietown_utils.expand_variables import expand_environment
 from duckietown_utils.locate_files_impl import locate_files
 from duckietown_utils.jpg import image_cv_from_jpg_fn
 import scipy.io
 import os
+from anti_instagram.AntiInstagram import calculate_transform, ScaleAndShift
 
-def examine_dataset(dirname):
+def examine_dataset(dirname, out):
     logger.info(dirname)
     dirname = expand_environment(dirname)
 
@@ -27,7 +28,14 @@ def examine_dataset(dirname):
     logger.debug('Using jpg %r to learn transformation.' % first_jpg)
 
     first_jpg_image = image_cv_from_jpg_fn(first_jpg)
-    transform = None
+
+
+    success, health, parameters = calculate_transform(first_jpg_image)
+    logger.info('success: %s' % str(success))
+    logger.info('health: %s' % str(health))
+    logger.info('parameters: %s' % str(parameters))
+
+    transform = ScaleAndShift(**parameters)
     
     for m in mats:
         logger.debug(m)
@@ -36,17 +44,26 @@ def examine_dataset(dirname):
             msg = 'JPG %r does not exist'
             raise ValueError(msg)
         
-        test_pair(transform, jpg, m)
+        test_pair(transform, jpg, m, out)
         
        
         
-def test_pair(transform, jpg, mat):
+def test_pair(transform, jpg, mat, out):
     """ 
         jpg = filename
         mat = filename
     """ 
     
     image_cv = image_cv_from_jpg_fn(jpg)
+    transformed = transform(image_cv)
+
+    if not os.path.exists(out):
+        os.makedirs(out)
+    import cv2
+    bn = os.path.splitext(os.path.basename(jpg))[0]
+    cv2.imwrite(os.path.join(out, '%s.orig.jpg' % bn), image_cv)
+    cv2.imwrite(os.path.join(out, '%s.transformed.jpg' % bn), transformed)
+
     data = scipy.io.loadmat(mat)
     regions = data['regions'].flatten()
     for r in regions:
@@ -61,13 +78,15 @@ def test_pair(transform, jpg, mat):
         print 'color', r['color']
 
 
+
+
 def anti_instagram_annotations_test():
     datasets = [
         "${DUCKIETOWN_DATA}/phase3-misc-files/so1/annotations_format_example/agirard/",
     ]
     
     for d in datasets:
-        examine_dataset(d)
+        examine_dataset(d, 'out')
 
 
 if __name__ == '__main__':

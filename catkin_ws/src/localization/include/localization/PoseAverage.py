77ae310a682083e7335521cdb09f87b4ff0fedb6
Teddy Ort <teddy@mit.edu>
2016-04-16 00:25:14 -0400

    Adds library stub

---
+++ catkin_ws/src/localization/include/localization/PoseAverage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from geometry_msgs.msg import Transform
+
+class PoseAverage(object):
+
+    def __init__(self):
+        pass
+
+    def add_pose(self, pose):
+        pass
+
+    def get_average(self):
+        return Transform()

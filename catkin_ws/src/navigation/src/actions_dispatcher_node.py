a2378044622b5a997fde406ce96cff170ef53cdc
joe-wl <jwleavit@mit.edu>
2016-04-16 11:38:42 -0400

    Converted actions dispatcher node to a class, enables mode persistence and starting at an intersection.

--- catkin_ws/src/navigation/src/actions_dispatcher_node.py
+++ catkin_ws/src/navigation/src/actions_dispatcher_node.py
@@ -6,74 +6,83 @@ from navigation.srv import *
 from duckietown_msgs.msg import FSMState, SourceTargetNodes
 from std_msgs.msg import Int16, String
 
-#adding logic because FSM publishes our state at a high rate
-#not just everytime the mode changes but multiple times in each mode
-firstUpdate = True
+class ActionsDispatcherNode():
+    def __init__(self):
 
-actions = []
-pub = 0
-#state_data = FSMState()
-#state_data.state = 'JOYSTICK_CONTROL'
+        #adding logic because FSM publishes our state at a high rate
+        #not just everytime the mode changes but multiple times in each mode
+        self.first_update = True
 
-def dispatcher(data):
-    global actions
-    global pub
-    global firstUpdate
-#    global state_data
-#    state_data = data
-    if firstUpdate == False and data.state != 'INTERSECTION_CONTROL': #state_data.INTERSECTION_CONTROL:
-        firstUpdate = True
+        self.actions = []
+        self.mode = 'JOYSTICK_CONTROL'
 
-    if firstUpdate == True and data.state == 'INTERSECTION_CONTROL' and actions: #data.INTERSECTION_CONTROL and actions:
-        rospy.sleep(2)
-        action = actions.pop(0)
-        print 'Dispatched:', action
-        if action == 's':
-            pub.publish(Int16(1))
-        elif action == 'r':
-            pub.publish(Int16(2))
-        elif action == 'l':
-            pub.publish(Int16(0))
-        elif action == 'w':
-            pub.publish(Int16(-1))    
+        # Subscribers:
+        self.sub_mode = rospy.Subscriber("~mode", FSMState, self.updateMode, queue_size = 1)
+        self.sub_plan_request = rospy.Subscriber("~plan_request", SourceTargetNodes, self.graph_search)
+
+        # Publishers:
+        self.pub = rospy.Publisher("~turn_type", Int16, queue_size=1, latch=True)
+        self.pubList = rospy.Publisher("~turn_plan", String, queue_size=1, latch=True)
+
+    def updateMode(self, data):
+        self.mode = data.state
+        self.dispatcher()
+
+    def dispatcher(self):
+        if self.first_update == False and self.mode != 'INTERSECTION_CONTROL':
+            self.first_update = True
+
+        if self.first_update == True and self.mode == 'INTERSECTION_CONTROL' and self.actions:
+            # Allow time for open loop controller to update state and allow duckiebot to stop at redline:
+            rospy.sleep(2)
+        
+            # Proceed with action dispatching:
+            action = self.actions.pop(0)
+            print 'Dispatched:', action
+            if action == 's':
+                self.pub.publish(Int16(1))
+            elif action == 'r':
+                self.pub.publish(Int16(2))
+            elif action == 'l':
+                self.pub.publish(Int16(0))
+            elif action == 'w':
+                self.pub.publish(Int16(-1))    
     
-        action_str = ''
-        for letter in actions:
-            action_str += letter
-
-        pubList.publish(action_str)
-        firstUpdate = False
-
-def graph_search(data):
-    print 'Requesting map for src: ', data.source_node, ' and target: ', data.target_node
-    global actions
-#    global state_data
-    rospy.wait_for_service('graph_search')
-    try:
-        graph_search = rospy.ServiceProxy('graph_search', GraphSearch)
-        resp = graph_search(data.source_node, data.target_node)
-        actions = resp.actions
-        if actions:
-            # remove 'f' (follow line) from actions and add wait action in the end of queue
-            actions = [x for x in actions if x != 'f']
-            actions.append('w')
-            print 'Actions to be executed:', actions
             action_str = ''
             for letter in actions:
                 action_str += letter
-            pubList.publish(action_str)
-#            dispatcher(state_data)
-        else:
-            print 'Actions to be executed:', actions
 
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+            self.pubList.publish(action_str)
+            self.firstUpdate = False
+
+    def graph_search(self, data):
+        print 'Requesting map for src: ', data.source_node, ' and target: ', data.target_node
+        rospy.wait_for_service('graph_search')
+        try:
+            graph_search = rospy.ServiceProxy('graph_search', GraphSearch)
+            resp = graph_search(data.source_node, data.target_node)
+            self.actions = resp.actions
+            if self.actions:
+                # remove 'f' (follow line) from actions and add wait action in the end of queue
+                self.actions = [x for x in self.actions if x != 'f']
+                self.actions.append('w')
+                print 'Actions to be executed:', self.actions
+                action_str = ''
+                for letter in self.actions:
+                    action_str += letter
+                self.pubList.publish(action_str)
+                self.dispatcher()
+            else:
+                print 'Actions to be executed:', self.actions
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+    def onShutdown(self):
+        rospy.loginfo("[ActionsDispatcherNode] Shutdown.")
 
 if __name__ == "__main__":
     rospy.init_node('actions_dispatcher_node')
-    rospy.Subscriber("~mode", FSMState, dispatcher, queue_size = 1)
-    rospy.Subscriber("~plan_request", SourceTargetNodes, graph_search)
-    pub = rospy.Publisher("~turn_type", Int16, queue_size=1, latch=True)
-    pubList = rospy.Publisher("~turn_plan", String, queue_size=1, latch=True)
-    
+    actions_dispatcher_node = ActionsDispatcherNode()
+    rospy.on_shutdown(actions_dispatcher_node.onShutdown)
     rospy.spin()

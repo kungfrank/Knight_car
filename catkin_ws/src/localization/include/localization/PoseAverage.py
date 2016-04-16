87a8337952a2017dce61679ee5c34515d07e98c0
Amado Antonini <amadoa@mit.edu>
2016-04-16 00:51:30 -0400

    adds PoseAverage library

--- catkin_ws/src/localization/include/localization/PoseAverage.py
+++ catkin_ws/src/localization/include/localization/PoseAverage.py
@@ -1,13 +1,35 @@
-#!/usr/bin/env python
 from geometry_msgs.msg import Transform
+import tf.transformations as tr
+import math
 
 class PoseAverage(object):
 
-    def __init__(self):
-        pass
 
-    def add_pose(self, pose):
-        pass
+    def __init__(self):
+        self.translation = [0.0, 0.0, 0.0]
+        self.n = 0
+        self.sum_sines = 0
+        self.sum_cosines = 0
+
+    def add_pose(self, transform_in):
+        self.n += 1
+        translation_in = transform_in.translation
+        self.translation = [(self.translation[0]*(self.n - 1) + translation_in.x)/self.n,\
+                            (self.translation[1]*(self.n - 1) + translation_in.y)/self.n,\
+                            (self.translation[2]*(self.n - 1) + translation_in.z)/self.n]
+        rotation_in = transform_in.rotation
+        quaternion = (rotation_in.x, rotation_in.y, rotation_in.z, rotation_in.w)
+        theta_in = tr.euler_from_quaternion(quaternion)[2]
+        self.sum_sines += math.sin(theta_in)
+        self.sum_cosines += math.cos(theta_in)
+
 
     def get_average(self):
-        return Transform()
+        if self.n == 0:
+            return None
+        transform_out = Transform()
+        transform_out.translation = self.translation
+        theta_out = math.atan2(self.sum_sines, self.sum_cosines)
+        transform_out.rotation = tr.quaternion_from_euler(0,0,theta_out)
+
+        return transform_out

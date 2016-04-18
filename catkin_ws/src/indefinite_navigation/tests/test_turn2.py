2202e4b408a1509a136bae56b2f3c6c9aee5ffc8
Catherine Liu <catherineliu0@gmail.com>
2016-04-18 22:54:09 +0000

    Convert turn test to rostest

--- catkin_ws/src/indefinite_navigation/tests/test_turn2.py
+++ catkin_ws/src/indefinite_navigation/tests/test_turn2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import rospy
+import rospy, unittest, rostest
 import math
 from intersection_control.util import HelloGoodbye #Imports module. Not limited to modules in this pkg. 
 from duckietown_msgs.msg import LanePose, StopLineReading
@@ -9,13 +9,19 @@ from std_msgs.msg import Bool #Imports msg
 #from duckietown_msgs.msg import messages to command the wheels
 from duckietown_msgs.msg import Twist2DStamped, BoolStamped, FSMState
 
-class IndefNavigationTurnNode(object):
-    def __init__(self):
+class IndefNavigationTurnNode(unittest.TestCase):
+    def __init__(self, *args):
+        super(IndefNavigationTurnNode, self).__init__(*args)
+        #self.setup()
+    def setup(self):
+        #give lane_filter some time to start before beginning
+        rospy.sleep(10)
+        rospy.init_node('indef_navigation_turn_node', anonymous=False)
         # Save the name of the node
         self.node_name = rospy.get_name()
         
         rospy.loginfo("[%s] Initialzing." %(self.node_name))
-        veh_name= rospy.get_param("veh")['duckiebot_visualizer']['veh_name']
+        veh_name= self.setupParam("~veh", "")
         self.type = self.setupParam("~type", 'right')
         lane_topic = "/" + veh_name + "/lane_filter_node/lane_pose"
         done_topic = "/" + veh_name + "/open_loop_intersection_control_node/intersection_done"
@@ -26,12 +32,12 @@ class IndefNavigationTurnNode(object):
         wheels_cmd = "/" + veh_name + "/open_loop_intersection_control_node/car_cmd"
         self.lane = None
         self.done = None
-
+        
         self.publish_mode = rospy.Publisher(mode_topic, FSMState, queue_size=1)
         self.pub_wheels = rospy.Publisher(wheels_cmd, Twist2DStamped, queue_size=1)
         self.sub_lane = rospy.Subscriber(lane_topic, LanePose, self.cbLane, queue_size=1)
         self.sub_done = rospy.Subscriber(done_topic, BoolStamped, self.cbDone, queue_size=1)
-        rospy.loginfo("[%s] Initialzed." %(self.node_name))
+        self.pubsubList = [self.publish_mode, self.pub_wheels, self.sub_lane, self.sub_done]
 
         rospy.wait_for_service(left_service)
         self.turn_left_serv = rospy.ServiceProxy(left_service, Empty)
@@ -43,10 +49,7 @@ class IndefNavigationTurnNode(object):
         self.turn_forward_serv = rospy.ServiceProxy(forward_service, Empty)
 
         self.rate = rospy.Rate(30) # 10hz
-        mode = FSMState()
-        mode.state = "INTERSECTION_CONTROL"
-        self.publish_mode.publish(mode)
-        rospy.sleep(0.5)
+        rospy.loginfo("[%s] Initialized." %(self.node_name))
 
     def setupParam(self,param_name,default_value):
         value = rospy.get_param(param_name,default_value)
@@ -60,7 +63,13 @@ class IndefNavigationTurnNode(object):
     def cbDone(self, data):
         self.done = data.data or self.done
 
-    def turn(self):
+    def test_turn(self):
+        self.setup()
+        mode = FSMState()
+        mode.state = "INTERSECTION_CONTROL"
+        self.publish_mode.publish(mode)
+        #need to wait 1/2 second before publishing new turn type
+        rospy.sleep(0.5)
         if self.type.lower() == 'left':
             self.turn_left_serv()
         elif self.type.lower() == 'right':
@@ -73,8 +82,8 @@ class IndefNavigationTurnNode(object):
             count += 1
             rospy.loginfo("Waiting for intersection_done")
             rospy.sleep(0.2)
-            if count > 25:
-                rospy.loginfo("TEST FAILED: TIMED OUT, WAITED MORE THAN 5s FOR INTERSECTION DONE")
+            if count > 20:
+                self.assertEquals(True, False, "Timed out waiting for intersection")
                 return
 
         stop = Twist2DStamped()
@@ -88,7 +97,6 @@ class IndefNavigationTurnNode(object):
             rospy.sleep(0.1)
         self.final = self.lane
         self.calculate()
-
     def calculate(self):
         init_d = 0.0
         init_phi = 0.0
@@ -99,7 +107,7 @@ class IndefNavigationTurnNode(object):
         off_d = abs(init_d - final_d)
         off_phi = abs(init_phi - final_phi)
         result_trim = "FAILED"
-
+        
         if abs(off_d) < 0.08:
             result_trim = "PASSED"
 
@@ -115,7 +123,7 @@ class IndefNavigationTurnNode(object):
         """ % ( init_d, init_phi, final_d, final_phi, \
                 off_d, off_phi, result_trim)
         print info
-
+        self.assertEqual(result_trim, "PASSED", info)
 
 
 
@@ -123,7 +131,5 @@ if __name__ == '__main__':
     # Initialize the node with rospy
     rospy.init_node('indef_navigation_turn_node', anonymous=False)
 
-    # Create the NodeName object
-    node = IndefNavigationTurnNode()
-    node.turn()
+    rostest.rosrun('rostest_turn_calibration', 'indef_navigation_turn_node', IndefNavigationTurnNode)
 

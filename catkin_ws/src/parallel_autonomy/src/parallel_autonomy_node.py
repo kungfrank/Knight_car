49bb6c8cfe954308f130fcda876ba72d9e18bdf6
Wyatt Ubellacker <wubella@mit.edu>
2016-04-18 17:56:36 -0400

    Parallel autonomy node testing leds

--- catkin_ws/src/parallel_autonomy/src/parallel_autonomy_node.py
+++ catkin_ws/src/parallel_autonomy/src/parallel_autonomy_node.py
@@ -2,20 +2,23 @@
 import rospy
 import numpy
 from duckietown_msgs.msg import FSMState, AprilTags, BoolStamped
-from std_msgs.msg import String, Int16, Joy #Imports msg
+from std_msgs.msg import String, Int16 #Imports msg
+from sensor_msgs.msg import Joy
+from rgb_led import RGB_LED
 
 class ParallelAutonomyNode(object):
     def __init__(self):
         # Save the name of the node
         self.node_name = rospy.get_name()
 
-        self.turn.LEFT = 0
-        self.turn.RIGHT = 1
-        self.turn.STRAIGHT = 2
-        self.turn.NONE = 3
+        self.led = RGB_LED()
+        self.turn_LEFT = 0
+        self.turn_RIGHT = 1
+        self.turn_STRAIGHT = 2
+        self.turn_NONE = 3
 
         self.availableTurns = []
-        self.turn_direction = self.turn.NONE
+        self.turn_direction = self.turn_NONE
 
         rospy.loginfo("[%s] Initialzing." %(self.node_name))
 
@@ -31,21 +34,27 @@ class ParallelAutonomyNode(object):
 
         self.rate = rospy.Rate(30) # 10hz
 
+        for i in range(10):
+                self.led.setRGB(2, [1, 0, 0])
+                sleep(.5)
+                self.led.setRGB(2, [1, 1, 0])
+                sleep(.5)
+
 
     def cbJoy(self,msg):
 
-        if msg.buttons[1] and self.turn.LEFT in self.availableTurns: # or self.joy.axes[3] > 0.2:
-            if self.turn_direction == self.turn.LEFT:
-                self.turn_direction = self.turn.STRAIGHT
+        if msg.buttons[1] and self.turn_LEFT in self.availableTurns: # or self.joy.axes[3] > 0.2:
+            if self.turn_direction == self.turn_LEFT:
+                self.turn_direction = self.turn_STRAIGHT
             else:
-                self.turn_direction = self.turn.LEFT
-        elif msg.buttons[2] and self.turn.RIGHT in self.availableTurns: #or self.joy.axes[3] < -0.2:
-            if self.turn_direction == self.turn.RIGHT:
-                self.turn_direction = self.turn.STRAIGHT
+                self.turn_direction = self.turn_LEFT
+        elif msg.buttons[2] and self.turn_RIGHT in self.availableTurns: #or self.joy.axes[3] < -0.2:
+            if self.turn_direction == self.turn_RIGHT:
+                self.turn_direction = self.turn_STRAIGHT
             else:
-                self.turn_direction = self.turn.RIGHT
-        if self.turn_direction == self.turn.STRAIGHT and self.turn.STRAIGHT not in self.availableTurns and if len(self.availableTurns)>0:
-            self.turn_direction = self.turn.NONE
+                self.turn_direction = self.turn_RIGHT
+        if self.turn_direction == self.turn_STRAIGHT and self.turn_STRAIGHT not in self.availableTurns and len(self.availableTurns)>0:
+            self.turn_direction = self.turn_NONE
         # blink leds via publish
 
     def cbTag(self, tag_msgs):
@@ -59,13 +68,13 @@ class ParallelAutonomyNode(object):
                     #go through possible intersection types
                     signType = taginfo.traffic_sign_type
                     if(signType == taginfo.NO_RIGHT_TURN or signType == taginfo.LEFT_T_INTERSECT):
-                        self.availableTurns = [self.turn.STRAIGHT,self.turn.LEFT]
+                        self.availableTurns = [self.turn_STRAIGHT,self.turn_LEFT]
                     elif (signType == taginfo.NO_LEFT_TURN or signType == taginfo.RIGHT_T_INTERSECT):
-                        self.availableTurns = [self.turn.RIGHT,self.turn.STRAIGHT]
+                        self.availableTurns = [self.turn_RIGHT,self.turn_STRAIGHT]
                     elif (signType== taginfo.FOUR_WAY):
-                        self.availableTurns = [self.turn.RIGHT,self.turn.STRAIGHT,self.turn.LEFT]
+                        self.availableTurns = [self.turn_RIGHT,self.turn_STRAIGHT,self.turn_LEFT]
                     elif (signType == taginfo.T_INTERSECTION):
-                        self.availableTurns = [self.turn.RIGHT,self.turn.LEFT]
+                        self.availableTurns = [self.turn_RIGHT,self.turn_LEFT]
 
                     #now randomly choose a possible direction
                     if(len(availableTurns)>0):
@@ -83,9 +92,9 @@ class ParallelAutonomyNode(object):
 
         if(self.fsm_mode == "WAITING_FOR_TURN_DIRECTION"):
             self.availableTurns = []
-            self.turn_direction = self.turn.STRAIGHT
+            self.turn_direction = self.turn_STRAIGHT
             rospy.sleep(2)
-            while self.turn_direction is self.turn.NONE:
+            while self.turn_direction is self.turn_NONE:
                 pass
             self.pub_turn_type.publish(self.turn_direction) # make sure mapping to turn_type is ok
             rospy.loginfo("[%s] Turn type: %i" %(self.node_name, self.turn_direction))

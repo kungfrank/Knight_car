f10ebc468e570eec1de3e78dc64548e98d52f7e5
Andrea Censi <censi@mit.edu>
2016-04-18 20:43:33 -0400

    fixes

--- catkin_ws/src/line_detector/src/line_detector_node.py
+++ catkin_ws/src/line_detector/src/line_detector_node.py
@@ -17,17 +17,15 @@ import threading
 import time
 
 
-
 class LineDetectorNode(object):
     def __init__(self):
-        self.node_name = "Line Detector"
+        self.node_name = "LineDetectorNode"
 
         # Thread lock 
         self.thread_lock = threading.Lock()
        
         # Constructor of line detector 
         self.bridge = CvBridge()
-
 
         self.active = True
 
@@ -37,20 +35,16 @@ class LineDetectorNode(object):
         self.intermittent_interval = 100
         self.intermittent_counter = 0
 
-
-        self.updateParams(None)
-
         # color correction
         self.ai = AntiInstagram()
 
-        # Verbose option 
-        if self.verbose:
-            self.pub_edge = rospy.Publisher("~edge", Image, queue_size=1)
-            self.pub_colorSegment = rospy.Publisher("~colorSegment", Image, queue_size=1)
-            
-        if False:
-            self.timer = rospy.Timer(rospy.Duration.from_sec(1.0), self.updateParams)
+        # these will be added if it becomes verbose
+        self.pub_edge = None
+        self.pub_colorSegment = None
 
+        self.detector_config = None
+        self.updateParams(None)
+            
         # Publishers
         self.pub_lines = rospy.Publisher("~segment_list", SegmentList, queue_size=1)
         self.pub_image = rospy.Publisher("~image_with_lines", Image, queue_size=1)
@@ -59,29 +53,38 @@ class LineDetectorNode(object):
         self.sub_image = rospy.Subscriber("~image", CompressedImage, self.cbImage, queue_size=1)
         self.sub_transform = rospy.Subscriber("~transform", AntiInstagramTransform, self.cbTransform, queue_size=1)
         self.sub_switch = rospy.Subscriber("~switch", BoolStamped, self.cbSwitch, queue_size=1)
+
         rospy.loginfo("[%s] Initialized (verbose = %s)." %(self.node_name, self.verbose))
 
+        self.timer = rospy.Timer(rospy.Duration.from_sec(2.0), self.updateParams)
+
+
     def updateParams(self, _event):
+        old_verbose = self.verbose
         self.verbose = rospy.get_param('~verbose',True)
+        if self.verbose != old_verbose:
+            self.loginfo('Verbose is now %r' % self.verbose)
 
         self.image_size = rospy.get_param('~img_size')
         self.top_cutoff = rospy.get_param('~top_cutoff')
 
         c = rospy.get_param('~detector')
-        assert isinstance(c, list) and len(c) == 2, c
         
-        klassname = c[0]
-        detector_params = c[1]
-        self.detector = instantiate(klassname, detector_params)
-        rospy.loginfo('detector_params: %r' % detector_params)
+        if self.detector_config != c:
+            self.loginfo('new detector config: %r' % c)
+            assert isinstance(c, list) and len(c) == 2, c
+            self.detector = instantiate(c[0], c[1])
+            self.detector_config = c
+
+        if self.verbose and self.pub_edge is None:
+            self.pub_edge = rospy.Publisher("~edge", Image, queue_size=1)
+            self.pub_colorSegment = rospy.Publisher("~colorSegment", Image, queue_size=1)
+
 
     def cbSwitch(self, switch_msg):
         self.active = switch_msg.data
 
     def cbImage(self, image_msg):
-        if self.stats.nreceived == 0:
-            rospy.loginfo('line_detector_node received first image.')
-
         self.stats.received()
 
         if not self.active:
@@ -96,7 +99,10 @@ class LineDetectorNode(object):
         self.ai.shift = transform_msg.s[0:3]
         self.ai.scale = transform_msg.s[3:6]
 
-        rospy.loginfo("AntiInstagram transform received")
+        self.loginfo("AntiInstagram transform received")
+
+    def loginfo(self, s):
+        rospy.loginfo('[%s] %s' % (self.node_name, s))
 
     def intermittent_log_now(self):
         return self.intermittent_counter % self.intermittent_interval == 1
@@ -104,8 +110,7 @@ class LineDetectorNode(object):
     def intermittent_log(self, s):
         if not self.intermittent_log_now():
             return
-        n = self.node_name
-        rospy.loginfo('[%s]%3d:%s' % (n, self.intermittent_counter, s))
+        self.loginfo('%3d:%s' % (self.intermittent_counter, s))
 
     def processImage(self, image_msg):
         if not self.thread_lock.acquire(False):
@@ -120,8 +125,6 @@ class LineDetectorNode(object):
             self.thread_lock.release()
 
     def processImage_(self, image_msg):
-        if self.stats.nprocessed == 0:
-            rospy.loginfo('line_detector_node processing first image.')
 
         self.stats.processed()
 
@@ -137,7 +140,7 @@ class LineDetectorNode(object):
         try:
             image_cv = image_cv_from_jpg(image_msg.data)
         except ValueError as e:
-            rospy.loginfo('line detector could not decode image: %s' % e)
+            self.loginfo('Could not decode image: %s' % e)
             return
 
         tk.completed('decoded')
@@ -227,7 +230,7 @@ class LineDetectorNode(object):
 
 
     def onShutdown(self):
-        rospy.loginfo("[LineDetectorNode] Shutdown.")
+        self.loginfo("Shutdown.")
             
     def toSegmentMsg(self,  lines, normals, color):
         
@@ -247,28 +250,39 @@ class LineDetectorNode(object):
 
 class Stats():
     def __init__(self):
+        self.nrsesets = 0
         self.reset()
 
     def reset(self):
+        self.nrsesets += 1
         self.t0 = time.time()
         self.nreceived = 0
         self.nskipped = 0
         self.nprocessed = 0
 
     def received(self):
+        if self.nreceived == 0 and self.nresets == 1:
+            rospy.loginfo('line_detector_node received first image.')
         self.nreceived += 1
+
     def skipped(self):
         self.nskipped += 1
+
     def processed(self):
+        if self.nprocessed == 0 and self.nresets == 1:
+            rospy.loginfo('line_detector_node processing first image.')
+
         self.nprocessed += 1
 
     def info(self):
         delta = time.time() - self.t0
-	if self.nreceived:
-        	skipped_perc = (100.0 * self.nskipped / self.nreceived)
+
+        if self.nreceived:
+            skipped_perc = (100.0 * self.nskipped / self.nreceived)
         else:
-		skipped_perc = 0
-	def fps(x):
+            skipped_perc = 0
+
+        def fps(x):
             return '%.1f fps' % (x / delta)
 
         m = ('In the last %.1f s: received %d (%s) processed %d (%s) skipped %d (%s) (%1.f%%)' %

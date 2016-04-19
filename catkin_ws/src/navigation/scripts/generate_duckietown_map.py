fa1ed1694fd08e0450ae226738dfac31538d7aae
franzoni <franzoni315@gmail.com>
2016-04-18 22:29:22 -0400

    map conversion is working

--- catkin_ws/src/navigation/scripts/generate_duckietown_map.py
+++ catkin_ws/src/navigation/scripts/generate_duckietown_map.py
@@ -155,6 +155,72 @@ class Tile():
 
 		return node_loc,edges
 
+	def create_edges(self,tile_map):
+		if self.type == 'turn':
+			return self.create_turn_edges(tile_map)
+		elif self.type == '3way':
+			return self.create_3way_edges(tile_map)
+		elif self.type == '4way':	
+			return self.create_4way_edges(tile_map)
+		else:
+			return []
+
+	def create_turn_edges(self,tile_map):
+		edges = []
+		edges.append(self.connect_node(self.node1,tile_map))
+		edges.append(self.connect_node(self.node2,tile_map))
+		return edges
+
+	def create_3way_edges(self,tile_map):
+		edges = []
+		edges.append(self.connect_node(self.node2,tile_map))
+		edges.append(self.connect_node(self.node4,tile_map))
+		edges.append(self.connect_node(self.node6,tile_map))
+		return edges
+	def create_4way_edges(self,tile_map):
+		edges = []
+		edges.append(self.connect_node(self.node2,tile_map))
+		edges.append(self.connect_node(self.node4,tile_map))
+		edges.append(self.connect_node(self.node6,tile_map))
+		edges.append(self.connect_node(self.node8,tile_map))
+		return edges
+	def connect_node(self, node,tile_map):
+		next_tile_pos_x = self.x + node.direction[0]
+		next_tile_pos_y = self.y + node.direction[1]
+		t = self.get_tile(next_tile_pos_x,next_tile_pos_y,tile_map)
+		while t.type == 'straight':
+			next_tile_pos_x = next_tile_pos_x + node.direction[0]
+			next_tile_pos_y = next_tile_pos_y + node.direction[1]
+			t = self.get_tile(next_tile_pos_x,next_tile_pos_y,tile_map)
+
+		if t.type == 'turn':
+			if t.node1.flow(node) == 2:
+				return [node.name, t.node1.name, 'f']
+			elif t.node2.flow(node) == 2:
+				return [node.name, t.node2.name, 'f']
+		elif t.type == '3way':
+			if t.node1.flow(node) == 4:
+				return [node.name, t.node1.name, 'f']
+			elif t.node3.flow(node) == 4:
+				return [node.name, t.node3.name, 'f']
+			elif t.node5.flow(node) == 4:
+				return [node.name, t.node5.name, 'f']
+		elif t.type == '4way':
+			if t.node1.flow(node) == 4:
+				return [node.name, t.node1.name, 'f']
+			elif t.node3.flow(node) == 4:
+				return [node.name, t.node3.name, 'f']
+			elif t.node5.flow(node) == 4:
+				return [node.name, t.node5.name, 'f']
+			elif t.node7.flow(node) == 4:
+				return [node.name, t.node7.name, 'f']
+
+	def get_tile(self, x, y,tile_map):
+		for tile in tile_map:
+			if tile.x == x and tile.y == y:
+				return tile
+
+
 	def rotate(self,theta,vec):
 		theta = np.deg2rad(theta)
 		vec = np.matrix(vec).transpose()
@@ -162,7 +228,7 @@ class Tile():
 			[np.sin(theta), np.cos(theta)]])
 		res = np.dot(rotMatrix,vec)
 		res = res.tolist()
-		return (res[0][0],res[1][0])
+		return (int(res[0][0]),int(res[1][0]))
 
 	def rotateAndTranslate(self,theta,x,y,vec):
 		theta = np.deg2rad(theta)
@@ -187,56 +253,59 @@ class Node():
 		self.direction = direction
 		Node.n = Node.n + 1
 
+	def flow(self, node):
+		return (self.direction[0]+node.direction[0])**2 + (self.direction[1]+node.direction[1])**2
+
 
 class graph_creator():
-    def __init__(self):
-        self.node_locations = {}
-        self.edges = []
-        self.tile_map = []
-    def add_node_locations(self,node_loc):
-        self.node_locations.update(node_loc)
-       
-    def add_edges(self, ed):
-    	for edge in ed:
-    		source = edge[0]
-    		target = edge[1]
-    		action = edge[2]
-	        manhattan_dist = abs(self.node_locations[source][0] - self.node_locations[target][0]) + abs(self.node_locations[source][1] - self.node_locations[target][1])
-	        self.edges.append([source, target, manhattan_dist, action])
-    def save(self, name='duckietown_map.pkl'):
-        afile = open(r'maps/duckietown_map.pkl', 'w+')
-        pickle.dump([self.edges, self.node_locations], afile)
-        afile.close()
-    def draw(self):
-        # Create graph
-        duckietown_graph = Graph()
-        for edge in self.edges:
-	        duckietown_graph.add_edge(edge[0], edge[1], edge[2], edge[3])
-	
-        duckietown_graph.set_node_positions(self.node_locations)
-        duckietown_graph.draw(map_name='duckietown_map')
-    
-    def build_tile_map_from_csv(self):
-    	with open('maps/tiles_226.csv', 'rb') as f:
-		    spamreader = csv.reader(f,skipinitialspace=True)
-		    for i,row in enumerate(spamreader):
-		    	if i != 0:
-		    		row_ = [element.strip() for element in row] # remove white spaces
-		        	self.tile_map.append(Tile(row_))
-        self.generate_node_locations()
-    
-    def generate_node_locations(self):
+	def __init__(self):
+		self.node_locations = {}
+		self.edges = []
+		self.tile_map = []
+	def add_node_locations(self,node_loc):
+		self.node_locations.update(node_loc)
+	   
+	def add_edges(self, ed):
+		for edge in ed:
+			source = edge[0]
+			target = edge[1]
+			action = edge[2]
+			manhattan_dist = abs(self.node_locations[source][0] - self.node_locations[target][0]) + abs(self.node_locations[source][1] - self.node_locations[target][1])
+			self.edges.append([source, target, manhattan_dist, action])
+	def save(self, name='duckietown_map.pkl'):
+		afile = open(r'maps/duckietown_map.pkl', 'w+')
+		pickle.dump([self.edges, self.node_locations], afile)
+		afile.close()
+	def draw(self):
+	    # Create graph
+		duckietown_graph = Graph()
+		for edge in self.edges:
+			duckietown_graph.add_edge(edge[0], edge[1], edge[2], edge[3])
+
+		duckietown_graph.set_node_positions(self.node_locations)
+		duckietown_graph.draw(map_name='duckietown_map')
+
+	def build_tile_map_from_csv(self):
+		with open('maps/tiles_226.csv', 'rb') as f:
+			spamreader = csv.reader(f,skipinitialspace=True)
+			for i,row in enumerate(spamreader):
+				if i != 0:
+					row_ = [element.strip() for element in row] # remove white spaces
+					self.tile_map.append(Tile(row_))
+		self.generate_node_locations()
+		self.generate_edges()
+	def generate_node_locations(self):
 		for tile in self.tile_map:
 			node_loc,edges = tile.create_nodes()
-			print tile.x, tile.y, edges
 			self.add_node_locations(node_loc)
 			self.add_edges(edges)
-
-
-
-    def get_map_226(self):
-            # Node locations (for visual representation) and heuristics calculation
-        node_locations = {"11":(4,0.75),"12":(3.75,1),"13":(3.25,1),"14":(3,0.75),"15":(3,0.25),"18":(4,0.25),
+	def generate_edges(self):
+		for tile in self.tile_map:
+			edges = tile.create_edges(self.tile_map)
+			self.add_edges(edges)
+	def get_map_226(self):
+		# Node locations (for visual representation) and heuristics calculation
+		node_locations = {"11":(4,0.75),"12":(3.75,1),"13":(3.25,1),"14":(3,0.75),"15":(3,0.25),"18":(4,0.25),
                           "21":(1,2.75),"22":(0.75,3),"23":(0.25,3),"26":(0.25,2),"27":(0.75,2),"28":(1,2.25),
                           "31":(1,5.75),"32":(0.75,6),"33":(0.25,6),"36":(0.25,5),"37":(0.75,5),"38":(1,5.25),
                           "41":(3,5.75),"42":(2.75,6),"43":(2.25,6),"44":(2,5.75),"45":(2,5.25),"46":(2.25,5),
@@ -257,7 +326,7 @@ class graph_creator():
 
         # Create edges for the map.
         # Edges is a list of lists, in which each edge is in the format: [source, target, action]
-        edges = [["11", "12", 'r'],
+		edges = [["11", "12", 'r'],
         ["11", "14",  's'],
         ["12", "121", 'f'], ["121", "122", 'f'], ["122", "123", 'f'], ["123", "11", 'f'] , 
         ["13", "14",  'r'],
@@ -314,7 +383,7 @@ class graph_creator():
         ["57", "54",  'l'],
         ["58", "581",  'f'], ["581", "582",  'f'], ["582", "583",  'f'], ["583", "584",  'f'], ["584", "41",  'f']]
 
-        return node_locations, edges
+		return node_locations, edges
  
 if __name__ == "__main__":
     gc = graph_creator()

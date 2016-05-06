5b1bbd5c7e622232be05e6f3ed2a6949b124eb4d
liampaull <lpaull@mit.edu>
2016-05-06 11:20:22 -0400

    trying to integrate apriltags - needs testing

--- catkin_ws/src/apriltags_ros/apriltags_ros/src/apriltags_postprocessing_node.py
+++ catkin_ws/src/apriltags_ros/apriltags_ros/src/apriltags_postprocessing_node.py
@@ -3,6 +3,7 @@ import rospkg
 import rospy
 import yaml
 from apriltags_ros.msg import AprilTagDetectionArray
+from duckietown_msgs.msg import AprilTagsWithInfos
 import numpy as np
 import tf.transformations as tr
 from geometry_msgs.msg import PoseStamped
@@ -24,8 +25,43 @@ class AprilPostPros(object):
         self.bandaid     = self.setupParam("~bandaid", False)
         self.snap_angle = self.setupParam("~snap_angle", 35.0)
 
+# -------- Start adding back the tag info stuff
+
+        rospack = rospkg.RosPack()
+        self.pkg_path = rospack.get_path('apriltags')
+        tags_filepath = self.setupParam("~tags_file", self.pkg_path+"/apriltagsDB/apriltagsDB.yaml") # No tags_file input atm., so default value is used
+        self.loc = self.setupParam("~loc", -1) # -1 if no location is given
+        tags_file = open(tags_filepath, 'r')
+        self.tags_dict = yaml.load(tags_file)
+        tags_file.close()
+        self.info = TagInfo()
+ 
+        self.sign_types = {"StreetName": self.info.S_NAME,
+            "TrafficSign": self.info.SIGN,
+            "Light": self.info.LIGHT,
+            "Localization": self.info.LOCALIZE,
+            "Vehicle": self.info.VEHICLE}
+        self.traffic_sign_types = {"stop": self.info.STOP, 
+            "yield": self.info.YIELD, 
+            "no-right-turn": self.info.NO_RIGHT_TURN, 
+            "no-left-turn": self.info.NO_LEFT_TURN, 
+            "oneway-right": self.info.ONEWAY_RIGHT, 
+            "oneway-left": self.info.ONEWAY_LEFT, 
+            "4-way-intersect": self.info.FOUR_WAY, 
+            "right-T-intersect": self.info.RIGHT_T_INTERSECT, 
+            "left-T-intersect": self.info.LEFT_T_INTERSECT,
+            "T-intersection": self.info.T_INTERSECTION,
+            "do-not-enter": self.info.DO_NOT_ENTER,
+            "pedestrian": self.info.PEDESTRIAN,
+            "t-light-ahead": self.info.T_LIGHT_AHEAD,
+            "duck-crossing": self.info.DUCK_CROSSING}
+
+# ---- end tag info stuff 
+
+
+
         self.sub_prePros        = rospy.Subscriber("~apriltags_in", AprilTagDetectionArray, self.callback, queue_size=1)
-        self.pub_postPros       = rospy.Publisher("~apriltags_out", AprilTagDetectionArray, queue_size=1)
+        self.pub_postPros       = rospy.Publisher("~apriltags_out", AprilTagsWithInfos, queue_size=1)
         self.pub_visualize = rospy.Publisher("~tag_pose", PoseStamped, queue_size=1)
 
         rospy.loginfo("[%s] has started", self.node_name)
@@ -37,8 +73,42 @@ class AprilPostPros(object):
         return value
 
     def callback(self, msg):
+
+        tag_infos = []
+
         # Load tag detections message
         for detection in msg.detections:
+
+            # ------ start tag info processing
+
+            new_info = TagInfo()
+            #new_location_info = Vector2D()
+            new_info.id = int(detection.id)
+            id_info = self.tags_dict[new_info.id]
+            
+            # Check yaml file to fill in ID-specific information
+            new_info.tag_type = self.sign_types[id_info['tag_type']]
+            if new_info.tag_type == self.info.S_NAME:
+                new_info.street_name = id_info['street_name']
+            elif new_info.tag_type == self.info.SIGN:
+                new_info.traffic_sign_type = self.traffic_sign_types[id_info['traffic_sign_type']]
+            elif new_info.tag_type == self.info.VEHICLE:
+                new_info.vehicle_name = id_info['vehicle_name']
+            
+            # TODO: Implement location more than just a float like it is now.
+            # location is now 0.0 if no location is set which is probably not that smart
+            if self.loc == 226:
+                l = (id_info['location_226'])
+                if l is not None:
+                    new_info.location = l
+            elif self.loc == 316:
+                l = (id_info['location_316'])
+                if l is not None:
+                    new_info.location = l
+            
+
+            # --- end tag info processing
+
 
             # Define the transforms
             veh_t_camxout = tr.translation_matrix((self.camera_x, self.camera_y, self.camera_z))
@@ -94,8 +164,11 @@ class AprilPostPros(object):
             # self.pub_visualize.publish(pose)
             #############################################################################
 
+        new_tag_data = AprilTagsWithInfos()
+        new_tag_data.detections = msg.detections
+        new_tag_data.infos = tag_infos
         # Publish Message
-        self.pub_postPros.publish(msg)
+        self.pub_postPros.publish(new_tag_data)
 
 if __name__ == '__main__': 
     rospy.init_node('AprilPostPros',anonymous=False)

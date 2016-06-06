b1602f54f1d9d4c71a4b2dc91408f448c060418b
brianfire <fire594594594@gmail.com>
2016-06-06 15:55:28 +0000

    update mocap settings

--- catkin_ws/src/mocap/upstart.sh
+++ catkin_ws/src/mocap/upstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Open LCM Tunnel"
-bot-lcm-tunnel 10.0.1.5 &
-echo "...done."
+#echo "Open LCM Tunnel"
+#bot-lcm-tunnel 10.0.1.5 &
+#echo "...done."
 
 echo "PiCamera ROS to LCM"
 roslaunch mocap camera.launch &&

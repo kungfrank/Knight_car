cd5193cd1af4357afee81859f59f2c91f8c4760b
tsaichiawen <amytsai17@hotmail.com>
2016-06-07 11:06:37 +0800

    plate recognition

--- catkin_ws/src/spring2016_nctu/kfc/plate_recognition/src/plate_recognition_node.py
+++ catkin_ws/src/spring2016_nctu/kfc/plate_recognition/src/plate_recognition_node.py
@@ -93,34 +93,33 @@ class lane_controller(object):
 	
 	blnKNNTrainingSuccessful = DetectChars.loadKNNDataAndTrainKNN()         # attempt KNN training
 
-	    if blnKNNTrainingSuccessful == False:                               # if KNN training was not successful
+	if blnKNNTrainingSuccessful == False:                               # if KNN training was not successful
 		print "\nerror: KNN traning was not successful\n"               # show error message
 		return                                                          # and exit program
 	    # end if
 
-	    imgOriginalScene  = image  #cv2.imread("1.png")               # open image
+	imgOriginalScene  = image  #cv2.imread("1.png")               # open image
 
-	    #if imgOriginalScene is None:                            # if image was not read successfully
+	#if imgOriginalScene is None:                            # if image was not read successfully
 		#print "\nerror: image not read from file \n\n"      # print error message to std out
 		#os.system("pause")                                  # pause so user can see error message
 		#return                                              # and exit program
-	    # end if
+		# end if
 
-	    listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)           # detect plates
+	listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)           # detect plates
 
-	    listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)        # detect chars in plates
+	listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)        # detect chars in plates
 
-	    cv2.imshow("imgOriginalScene", imgOriginalScene)            # show scene image
+	cv2.imshow("imgOriginalScene", imgOriginalScene)            # show scene image
 
-	    if len(listOfPossiblePlates) == 0:                          # if no plates were found
+	if len(listOfPossiblePlates) == 0:                          # if no plates were found
 		print "\nno license plates were detected\n"             # inform user no plates were found
-	    else:                                                       # else
-		        # if we get in here list of possible plates has at leat one plate
-
-		        # sort the list of possible plates in DESCENDING order (most number of chars to least number of chars)
+	else:                                                       # else
+		# if we get in here list of possible plates has at leat one plate
+		# sort the list of possible plates in DESCENDING order (most number of chars to least number of chars)
 		listOfPossiblePlates.sort(key = lambda possiblePlate: len(possiblePlate.strChars), reverse = True)
 
-		        # suppose the plate with the most recognized chars (the first plate in sorted by string length descending order) is the actual plate
+		# suppose the plate with the most recognized chars (the first plate in sorted by string length descending order) is the actual plate
 		licPlate = listOfPossiblePlates[0]
 
 		cv2.imshow("imgPlate", licPlate.imgPlate)           # show crop of plate and threshold of plate

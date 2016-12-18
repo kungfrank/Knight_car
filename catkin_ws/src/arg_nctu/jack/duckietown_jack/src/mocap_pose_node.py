e7ab3912988480a39c7cf548b9d8d5d9cee727cb
brianfire <fire594594594@gmail.com>
2016-12-18 15:53:18 +0000

    add mocap motion planning

--- catkin_ws/src/arg_nctu/jack/duckietown_jack/src/mocap_pose_node.py
+++ catkin_ws/src/arg_nctu/jack/duckietown_jack/src/mocap_pose_node.py
@@ -14,13 +14,17 @@ class mocap_pose(object):
 		self.node_name = rospy.get_name()
 
 		self.active = True
-		self.out_of_boundary = False
+		self.motion_to_target = False
+		self.pos = Point();
+		self.target = Point();
+		self.yaw = 0; 
 
 		# Publicaiton
 		self.pub_car_cmd = rospy.Publisher("~car_cmd",Twist2DStamped,queue_size=1)
-		self.sub_joystick_car_cmd = rospy.Subscriber("~joystick_car_cmd",Twist2DStamped, self.cbJoystick,queue_size=1)
+		#self.sub_joystick_car_cmd = rospy.Subscriber("~joystick_car_cmd",Twist2DStamped, self.cbJoystick,queue_size=1)
 		self.sub_vehicle_position = rospy.Subscriber("~vehicle_position", Point, self.cbPosition, queue_size=1)
 		self.sun_vehicle_orientation = rospy.Subscriber("~vehicle_orientation", Quaternion, self.cbOrientation, queue_size=1)
+		self.sub_target_position = rospy.Subscriber("~target_position", Point, self.cbTargetn, queue_size=1)
 
 		# safe shutdown
 		rospy.on_shutdown(self.custom_shutdown)
@@ -58,27 +62,49 @@ class mocap_pose(object):
 
 	def cbPosition(self, pose_msg):
 
-		xl = 0;
-		xh = 1.5;
-		yl = 0;
-		yh = 1.5;
+		self.pos.x = pose_msg.x
+		self.pos.y = pose_msg.y
+		self.pos.z = pose_msg.z
 
-		if pose_msg.x < xl or pose_msg.x > xh or pose_msg.y < yl or pose_msg.y > yh:
-			self.out_of_boundary == True
-			rospy.loginfo("Duckiebot out of tiles bundaries")
-		else:
-			self.out_of_boundary == False
-			rospy.loginfo("Joystick available")
-	def cbOrientation(self , pose_o_msg) :
+	def cbOrientation(self , pose_o_msg):
 
 		quaternion = ( pose_o_msg.x, pose_o_msg.y, pose_o_msg.z, pose_o_msg.w)
 		euler = tf.transformations.euler_from_quaternion(quaternion)
 		roll = euler[0]
 		pitch = euler[1]
 		yaw = euler[2]
-		
+		self.yaw = yaw
 		print (roll, pitch, yaw)
 
+	def cbTarget(self, pose_msg):
+
+		rospy.loginfo("targe x : [%d] " %pose_msg.x)
+		self.Forward_x(pose_msg.x)
+
+	def Spinning(self, direction):
+
+		while(abs(self.yaw - direction) >= 5):
+			car_control_msg = Twist2DStamped()
+			car_control_msg.v = 1
+			car_control_msg.omega = 0.5
+			self.publishCmd(car_control_msg)
+			rospy.sleep(0.5)
+			car_control_msg.v = 0
+			car_control_msg.omega = 0
+			self.publishCmd(car_control_msg)
+
+	def Forward_x(self, target_x):
+
+		if(self.pos.x >= target_x):
+			self.Spinning(0)
+			while(self.pos.x > target_x):
+				car_control_msg.v = 1
+				car_control_msg.omega = 0
+				self.publishCmd(car_control_msg)
+				rospy.sleep(0.5)
+				car_control_msg.v = 0
+				car_control_msg.omega = 0
+				self.publishCmd(car_control_msg)
 
 if __name__ == "__main__":
 	rospy.init_node("mocap_pose",anonymous=False)

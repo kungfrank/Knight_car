648f9a9881d53662e08f594b04dbb61b1fb14bb7
ken19960315 <ken19960315@gmail.com>
2017-01-08 06:21:54 +0000

    For final project

--- catkin_ws/src/fall2016_nctu/buyme/duckietown_buyme/src/project.py
+++ catkin_ws/src/fall2016_nctu/buyme/duckietown_buyme/src/project.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from duckietown_msgs.msg import BoolStamped, FSMState
+from duckietown_msgs.msg import BoolStamped, FSMState, Twist2DStamped
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Point
 import math
+import time
 
 class Project(object):
     def __init__(self):
@@ -13,30 +14,30 @@ class Project(object):
 
         self.stack = ['B']
         
-	## publishers and subscribers
-        self.sub_mode      = rospy.Subscriber("/buyme/fsm_node/mode",FSMState, self.processStateChange)
-	self.sub_in_lane      = rospy.Subscriber("/buyme/lane_filter_node/in_lane", BoolStamped, self.process)
-	self.sub_switch = rospy.Subscriber("/buyme/project/switch", BoolStamped, self.cbSwitch, queue_size=1)
+        ## publishers and subscribers
+        self.sub_mode = rospy.Subscriber("/buyme/fsm_node/mode",FSMState, self.processStateChange)
+        self.sub_in_lane = rospy.Subscriber("/buyme/lane_filter_node/in_lane", BoolStamped, self.process)
+        self.sub_switch = rospy.Subscriber("/buyme/project/switch", BoolStamped, self.cbSwitch, queue_size=1)
         
         self.pub_last_is_forward = rospy.Publisher("~last_is_forward", BoolStamped, queue_size=1, latch=True)
         self.pub_last_is_turn = rospy.Publisher("~last_is_turn", BoolStamped, queue_size=1, latch=True)
+        self.pub_time_is_up = rospy.Publisher("~time_is_up", BoolStamped, queue_size=1, latch=True)
         self.pub_car_cmd = rospy.Publisher("/buyme/motion_planning/car_cmd",Twist2DStamped,queue_size=1)
 
     def processStateChange(self, msg):
+        self.state=msg.state
 
-	self.state=msg.state
-
-    def car_command(v, omega, duration):
+    def car_command(self, v, omega, duration):
         # Send stop command
         car_control_msg = Twist2DStamped()
         car_control_msg.v = v 
         car_control_msg.omega = omega 
-        pub_car_cmd.publish(car_control_msg)
+        self.pub_car_cmd.publish(car_control_msg)
         rospy.sleep(duration)
         #rospy.loginfo("Shutdown")
         car_control_msg.v = 0.0 
         car_control_msg.omega = 0.0 
-        pub_car_cmd.publish(car_control_msg)
+        self.pub_car_cmd.publish(car_control_msg)
 
     class switch(object):
         def __init__(self, value):
@@ -56,58 +57,73 @@ class Project(object):
             else:
                 return False
 
-    def motion_planning(concat):
+    def motion_planning(self, concat):
         for i in range(len(concat)):
             primitives = concat[i]
-            for case in switch(primitives):
+            for case in self.switch(primitives):
                 if case('F'):
-                    car_command(0.5, 0, 0.25)
+                    self.car_command(0.6, 0, 0.78)
                     break
                 if case('L'):
-                    car_command(0.2, 4, 0.82)
+                    self.car_command(0.2, 4, 0.82)
                     break
                 if case('R'):
-                    car_command(0.2, -4, 0.78)
+                    self.car_command(0, -3.5, 1.0)
                     break
                 if case('B'):
-                    car_command(-0.4, 0, 0.5)
+                    self.car_command(-0.6, 0, 0.78)
                     break
 
     def process(self, msgg):
         if self.active == True:
-    	    print "##################fsm state is " + `self.state`
-
-	    if self.state == "FORWARD":
-                motion_planning("F")
+            print "##################fsm state is " + `self.state`
+     
+            if self.state == "RESET":
+                self.stack = ['B']
+                time.sleep(2)
+            elif self.state == "FORWARD":
+                self.motion_planning("F")
                 self.stack.append('F')
-	    
+                time.sleep(2)	    
             elif self.state == "TURN_RIGHT":
-                motion_planning("R")
-                self.stack.append('R')
-
-	    elif self.state == "TURN_BACK":
-                motion_planning("RR")
-                self.stack.append('L')
-    
+                if self.stack.pop() == 'R':
+                    self.stack.append('R')
+                    msg = BoolStamped()
+                    msg.data = True
+                    self.pub_time_is_up.publish(msg)
+                else:
+                    self.motion_planning("R")
+                    self.stack.append('R')
+                time.sleep(2)	    
+            elif self.state == "TURN_BACK":
+                if self.stack.pop() == 'L':
+                    self.stack.append('L')
+                    msg = BoolStamped()
+                    msg.data = True
+                    self.pub_time_is_up.publish(msg)
+                else:
+                    self.motion_planning("RR")
+                    self.stack.append('L')
+                time.sleep(2)	    
             elif self.state == "TRACE_BACK":
-                motion_planning("RB")
+                self.motion_planning("RB")
                 self.stack.pop()
                 c = self.stack.pop()
                 if c == 'F':
                     self.stack.append('F')
-	            msg = BoolStamped()
+                    msg = BoolStamped()
                     msg.data = True
                     self.pub_last_is_forward.publish(msg)
                     msg.data = False
                     self.pub_last_is_turn.publish(msg)
                 elif c == 'R':
-	            msg = BoolStamped()
+                    msg = BoolStamped()
                     msg.data = False
                     self.pub_last_is_forward.publish(msg)
                     msg.data = True
                     self.pub_last_is_turn.publish(msg)
                 elif c == 'L':
-	            msg = BoolStamped()
+                    msg = BoolStamped()
                     msg.data = False
                     self.pub_last_is_forward.publish(msg)
                     msg.data = False
@@ -115,20 +131,18 @@ class Project(object):
                 else:
                     self.stack.append('B')
                     print "Bottom of stack"
-
+                    time.sleep(2)	    
             else:
                 print "Wrong State"
 
-    	    printroute()
+            self.printroute()
 
         if not self.active:
-	    
-	    msg = BoolStamped()
+            msg = BoolStamped()
             msg.data = False
             self.pub_last_is_forward.publish(msg)
             self.pub_last_is_turn.publish(msg)
-
-	    print "#####################Project not active######################"
+            print "#####################Project not active######################"
 
     def printroute(self):
         route = ""
@@ -146,6 +160,5 @@ class Project(object):
 if __name__ == '__main__': 
     rospy.init_node('Project',anonymous=False)
     lane_filter_node = Project()
-    rospy.on_shutdown(lane_filter_node.onShutdown)
     rospy.spin()
 

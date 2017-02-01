06bcf531f367c29d4773800de17ff9c47d3f9cf6
brianfire <fire594594594@gmail.com>
2017-02-01 11:15:15 +0000

    repair timer and left-right-turn lane_filer

--- catkin_ws/src/arg_nctu/kaku/duckietown_kaku/src/timer.py
+++ catkin_ws/src/arg_nctu/kaku/duckietown_kaku/src/timer.py
@@ -8,63 +8,53 @@ import time
 import math
 
 class Timer(object):
-    def __init__(self):
-        self.node_name = "timer"
-        self.active = True
-	self.i = 0
+	def __init__(self):
+		self.node_name = "timer"
+		# fsm switch
+		self.active = True
+		# timer state
+		self.start = False
+		self.state = ""
+		## publishers and subscribers
+		self.sub_mode = rospy.Subscriber("~fsm_node/mode",FSMState, self.processStateChange)
+		self.sub_in_lane = rospy.Subscriber("~lane_filter_node/in_lane", BoolStamped, self.processTimer)
+		self.sub_switch = rospy.Subscriber("~switch", BoolStamped, self.cbSwitch, queue_size=1)
+		self.pub_time_is_up = rospy.Publisher("~time_is_up", BoolStamped, queue_size=1, latch=True)
 
-        
-	## publishers and subscribers
-        #self.sub_mode      = rospy.Subscriber("/kaku/fsm_node/mode",FSMState, self.processStateChange)
-        self.sub_mode      = rospy.Subscriber("~fsm_node/mode",FSMState, self.processStateChange)
-	#self.sub_in_lane      = rospy.Subscriber("/kaku/lane_filter_node/in_lane", BoolStamped, self.processTimer)
-	self.sub_in_lane      = rospy.Subscriber("~lane_filter_node/in_lane", BoolStamped, self.processTimer)
+	def processStateChange(self, msg):
+		self.state=msg.state #
 
-	#self.sub_switch = rospy.Subscriber("/kaku/timer/switch", BoolStamped, self.cbSwitch, queue_size=1)
-	self.sub_switch = rospy.Subscriber("~switch", BoolStamped, self.cbSwitch, queue_size=1)
-        self.pub_time_is_up = rospy.Publisher("~time_is_up", BoolStamped, queue_size=1, latch=True)
+	def processTimer(self, msgg):
+		if self.active: # fsm switch on
+			if (self.state == "LANE_FOLLOWING_TURN_RIGHT") or (self.state == "LANE_FOLLOWING_TURN_LEFT"): # if in fsm state "left turn" or "right turn""
+				if not self.start: # if timer not start yet
+					self.timer_start = time.time() # record start time
+					self.start = True # change timer state to start
+					print "start time: ", self.timer_start
+				self.timer_end = time.time() # record time now
+				print "end time: ", self.timer_end
+				if (self.timer_end - self.timer_start) > 2: #if time duration between start time and time now bigger than 2 seconsds
+					# publish time is up
+					msg = BoolStamped()
+					msg.data = True
+					self.pub_time_is_up.publish(msg)
+					print "time is up"
+		if not self.active: # fsm switch off
+			# publish timer is off and reset timer state to not starting
+			msg = BoolStamped()
+			msg.data = False
+			self.pub_time_is_up.publish(msg)
+			self.start = False
 
-    def processStateChange(self, msg):
+	def cbSwitch(self, switch_msg):
+		self.active = switch_msg.data
 
-	self.state=msg.state
-
-
-    def processTimer(self, msgg):
-        if self.active == True:
-    	    #print "##################1. fsm state is " + `self.state`
-
-	    if self.i ==0:
-	        if self.state == "LANE_FOLLOWING_TURN_RIGHT" or "LANE_FOLLOWING_TURN_LEFT":
-	            self.timer_start = time.time()
-		    self.i = 1
-	            #print "####################Timer start######################"
-    	    self.timer_end = time.time()
-
-	    if (self.timer_end - self.timer_start) > 2: 
-	        #print "####################Timer End##########################"
-		msg = BoolStamped()
-		msg.data = True
-		self.pub_time_is_up.publish(msg)
-
-	if not self.active:
-	    self.timer_start = time.time()
-	    
-	    msg = BoolStamped()
-            msg.data = False
-            self.pub_time_is_up.publish(msg)
-
-	    self.i = 0
-	    #print "#####################Timer not active######################"
-
-    def cbSwitch(self, switch_msg):
-        self.active = switch_msg.data
- 
-    def onShutdown(self):
-        rospy.loginfo("[Timer] Shutdown.")
+	def onShutdown(self):
+		rospy.loginfo("[Timer] Shutdown.")
 
 if __name__ == '__main__': 
-    rospy.init_node('Timer',anonymous=False)
-    lane_filter_node = Timer()
-    rospy.on_shutdown(lane_filter_node.onShutdown)
-    rospy.spin()
+	rospy.init_node('Timer',anonymous=False)
+	timer_node = Timer()
+	rospy.on_shutdown(timer_node.onShutdown)
+	rospy.spin()
 

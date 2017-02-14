93ca9a6280d8d6a116bf61f01279f5bf41115bff
jihshi <k573261014@gmail.com>
2017-02-14 07:53:28 +0000

    yellow blue line

--- catkin_ws/src/line_detector/src/line_detector_node_gc.py
+++ catkin_ws/src/line_detector/src/line_detector_node_gc.py
@@ -21,9 +21,10 @@ class LineDetectorNode(object):
     def __init__(self):
         self.node_name = "LineDetectorNode"
 
-	self.bw_1 = 0
-	self.bw_2 = 0
-	
+        self.bw_1 = 0
+        self.bw_2 = 0
+
+    
         # Thread lock 
         self.thread_lock = threading.Lock()
        
@@ -175,107 +176,102 @@ class LineDetectorNode(object):
         # Detect lines and normals
 
         gray = cv2.cvtColor(image_cv_corr,cv2.COLOR_BGR2GRAY)
-	edges = cv2.Canny(gray, 100, 200, apertureSize = 3)
-	lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, np.empty(1), 3, 1)
+        edges = cv2.Canny(gray, 100, 200, apertureSize = 3)
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, np.empty(1), 3, 1)
 
-	hsv_green1 = (38,25,100)
-	hsv_green2 = (50,60,200)
+        hsv_yellow1 = (25,50,50)
+        hsv_yellow2 = (45,255,255)
 
-	hsv_white1 = (0,20,0)
-	hsv_white2 = (60,60,255)
+        hsv_blue1 = (100,90,80)
+        hsv_blue2 = (150,255,155)
 
-	#change color space to HSV
-	hsv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2HSV)
+        #change color space to HSV
+        hsv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2HSV)
 
-	#find the color
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3))
+        #find the color
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3))
 
-	green = cv2.inRange(hsv, hsv_green1, hsv_green2)
-	green = cv2.dilate(green, kernel)
-	self.bw1=green
+        yellow = cv2.inRange(hsv, hsv_yellow1, hsv_yellow2)
+        yellow = cv2.dilate(yellow, kernel)
+        self.bw_1=yellow
 
-	white = cv2.inRange(hsv, hsv_white1, hsv_white2)
-	white = cv2.dilate(white, kernel) 
-	self.bw2=white
-
-
-	edge_color_green = cv2.bitwise_and(green, edges)
-	edge_color_white   = cv2.bitwise_and(white, edges)
-
-	lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, np.empty(1), 3, 1)
-	lines_green = cv2.HoughLinesP(edge_color_green, 1, np.pi/180, 10, np.empty(1), 3, 1)
-	lines_white = cv2.HoughLinesP(edge_color_white, 1, np.pi/180, 10, np.empty(1), 3, 1)
+        blue = cv2.inRange(hsv, hsv_blue1, hsv_blue2)
+        blue = cv2.dilate(blue, kernel) 
+        self.bw_2=blue
 
 
-	if lines_green is not None:
-    		lines_green = np.array(lines_green[0])
-    		print "found green lines"
+        edge_color_yellow = cv2.bitwise_and(yellow, edges)
+        edge_color_blue   = cv2.bitwise_and(blue, edges)
+
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, np.empty(1), 3, 1)
+        lines_yellow = cv2.HoughLinesP(edge_color_yellow, 1, np.pi/180, 10, np.empty(1), 3, 1)
+        lines_blue = cv2.HoughLinesP(edge_color_blue, 1, np.pi/180, 10, np.empty(1), 3, 1)
+
+
+        if lines_yellow is not None:
+            lines_yellow = np.array(lines_yellow[0])
+            print "found yellow lines"
     
-	else:
-    		lines_green = []
-    		print "no green lines"
+        else:
+            lines_yellow = []
+            print "no yellow lines"
     
-	if lines_white is not None:
-    		lines_white = np.array(lines_white[0])
-   		print "found white lines"
+        if lines_blue is not None:
+            lines_blue = np.array(lines_blue[0])
+            print "found blue lines"
 
-	else:
-    		lines_white= []
-    		print "no white lines"
+        else:
+            lines_blue= []
+            print "no blue lines"
 
 
-	arr_cutoff = np.array((0, 40, 0, 40))
-	arr_ratio = np.array((1./120, 1./160, 1./120, 1./160))
+        arr_cutoff = np.array((0, 40, 0, 40))
+        arr_ratio = np.array((1./120, 1./160, 1./120, 1./160))
 
-	lines_1 = lines_green
-	lines_2 = lines_white
+        lines_1 = lines_yellow
+        lines_2 = lines_blue
 
-	normals = []
-	centers = []
-	if len(lines_2)>0:
-    		#find the normalized coordinates
-    		lines_normalized = ((lines_1 + arr_cutoff) * arr_ratio)
+        normals = []
+        centers = []
+        if len(lines_2)>0:
+            #find the normalized coordinates
+            lines_normalized = ((lines_1 + arr_cutoff) * arr_ratio)
 
-    		#find the unit vector
-    		length = np.sum((lines_1[:, 0:2] -lines_1[:, 2:4])**2, axis=1, keepdims=True)**0.5
-    		dx = 1.* (lines_1[:,3:4]-lines_1[:,1:2])/length
-    		dy = 1.* (lines_1[:,0:1]-lines_1[:,2:3])/length
+            #find the unit vector
+            length = np.sum((lines_1[:, 0:2] -lines_1[:, 2:4])**2, axis=1, keepdims=True)**0.5
+            dx = 1.* (lines_1[:,3:4]-lines_1[:,1:2])/length
+            dy = 1.* (lines_1[:,0:1]-lines_1[:,2:3])/length
 
-    		#find the center point
-    		centers = np.hstack([(lines_1[:,0:1]+lines_1[:,2:3])/2, (lines_1[:,1:2]+lines_1[:,3:4])/2])
+            #find the center point
+            centers = np.hstack([(lines_1[:,0:1]+lines_1[:,2:3])/2, (lines_1[:,1:2]+lines_1[:,3:4])/2])
 
        
-    		#find the vectors' direction
-    		x3 = (centers[:,0:1] - 4.*dx).astype('int')
-    		x3[x3<0]=0
-    		x3[x3>=160]=160-1
+            #find the vectors' direction
+            x3 = (centers[:,0:1] - 4.*dx).astype('int')
+            x3[x3<0]=0
+            x3[x3>=160]=160-1
 
-    		y3 = (centers[:,1:2] - 4.*dy).astype('int')
-    		y3[y3<0]=0
-    		y3[y3>=120]=120-1
+            y3 = (centers[:,1:2] - 4.*dy).astype('int')
+            y3[y3<0]=0
+            y3[y3>=120]=120-1
 
-    		x4 = (centers[:,0:1] + 4.*dx).astype('int')
-    		x4[x4<0]=0
-    		x4[x4>=160]=160-1
+            x4 = (centers[:,0:1] + 4.*dx).astype('int')
+            x4[x4<0]=0
+            x4[x4>=160]=160-1
 
-    		y4 = (centers[:,1:2] + 4.*dy).astype('int')
-    		y4[y4<0]=0
-    		y4[y4>=120]=120-1
+            y4 = (centers[:,1:2] + 4.*dy).astype('int')
+            y4[y4<0]=0
+            y4[y4>=120]=120-1
     
-    		flag_signs = (np.logical_and(bw_2[y3,x3]>0, bw_2[y4,x4]>0)).astype('int')*2-1
-    		normals = np.hstack([dx, dy]) * flag_signs
+            flag_signs = (np.logical_and(self.bw_2[y3,x3]>0, self.bw_2[y4,x4]>0)).astype('int')*2-1
+            normals = np.hstack([dx, dy]) * flag_signs
     
     
-    		flag = ((lines_1[:,2]-lines_1[:,0])*normals[:,1] - (lines_1[:,3]-lines_1[:,1])*normals[:,0])>0
-    		for i in range(len(lines_1)):
-        		if flag[i]:
-            			x1,y1,x2,y2 = lines_1[i, :]
-            			lines_1[i, :] = [x2,y2,x1,y1]
-
-
-
-
-
+            flag = ((lines_1[:,2]-lines_1[:,0])*normals[:,1] - (lines_1[:,3]-lines_1[:,1])*normals[:,0])>0
+            for i in range(len(lines_1)):
+                if flag[i]:
+                    x1,y1,x2,y2 = lines_1[i, :]
+                    lines_1[i, :] = [x2,y2,x1,y1]
 
 
         tk.completed('detected')
@@ -288,10 +284,9 @@ class LineDetectorNode(object):
         arr_cutoff = np.array((0, self.top_cutoff, 0, self.top_cutoff))
         arr_ratio = np.array((1./self.image_size[1], 1./self.image_size[0], 1./self.image_size[1], 1./self.image_size[0]))
         if len(lines_2) > 0:
-            lines_normalized_white = ((lines_2 + arr_cutoff) * arr_ratio)
-            segmentList.segments.extend(self.toSegmentMsg(lines_normalized_white, normals, 0))
+            lines_normalized_blue = ((lines_2 + arr_cutoff) * arr_ratio)
+            segmentList.segments.extend(self.toSegmentMsg(lines_normalized, normals,centers, 0))
 
-        
         self.intermittent_log('# segments: white %3d ' % (len(lines_2)))
         
         tk.completed('prepared')
@@ -306,7 +301,27 @@ class LineDetectorNode(object):
 
             # Draw lines and normals
             image_with_lines = np.copy(image_cv_corr)
-            drawLines(image_with_lines, lines_2, (0, 0, 0))
+            if len(lines_1)>0:
+                for x1,y1,x2,y2 in lines_1:    
+                    cx = int(x1+x2)/2
+                    cy = int(y1+y2)/2
+                    if cx >160:
+                        cx = 160-1
+                    elif cx<0:
+                        cx=0
+                    if cy >120:
+                        cy = 120-1
+                    elif cy<0:
+                        cy=0
+                    if(self.bw_2[cy,cx-1]==255 and self.bw_1[cy,cx+1]==255):
+                        cv2.line(image_with_lines, (x1,y1), (x2,y2), (255,255,255))
+                        cv2.circle(image_with_lines, (x1,y1), 1, (0,255,0)) #green circle
+                        cv2.circle(image_with_lines, (x2,y2), 1, (255,0,0)) #red circle
+                    if(self.bw_2[cy,cx+1]==255 and self.bw_1[cy,cx-1]==255):
+                        cv2.line(image_with_lines, (x1,y1), (x2,y2), (255,255,255))
+                        cv2.circle(image_with_lines, (x1,y1), 1, (0,255,0)) #green circle
+                        cv2.circle(image_with_lines, (x2,y2), 1, (255,0,0)) #red circle
+            #drawLines(image_with_lines, (lines_2), (255, 255, 255))
             #drawLines(image_with_lines, yellow.lines, (255, 0, 0))
             #drawLines(image_with_lines, red.lines, (0, 255, 0))
 
@@ -321,11 +336,11 @@ class LineDetectorNode(object):
 
 
 #         if self.verbose:
-            colorSegment = color_segment(white.area, red.area, yellow.area) 
-            edge_msg_out = self.bridge.cv2_to_imgmsg(self.detector.edges, "mono8")
-            colorSegment_msg_out = self.bridge.cv2_to_imgmsg(colorSegment, "bgr8")
-            self.pub_edge.publish(edge_msg_out)
-            self.pub_colorSegment.publish(colorSegment_msg_out)
+            #colorSegment = color_segment(self.bw_1, self.bw_2) 
+            #edge_msg_out = self.bridge.cv2_to_imgmsg(self.detector.edges, "mono8")
+            #colorSegment_msg_out = self.bridge.cv2_to_imgmsg(colorSegment, "bgr8")
+            #self.pub_edge.publish(edge_msg_out)
+            #self.pub_colorSegment.publish(colorSegment_msg_out)
 
             tk.completed('pub_edge/pub_segment')
 
@@ -335,35 +350,27 @@ class LineDetectorNode(object):
     def onShutdown(self):
         self.loginfo("Shutdown.")
             
-    def toSegmentMsg(self,  lines, normals, color):
-        
+    def toSegmentMsg(self,  lines, normals,centers, color):
         segmentMsgList = []
-        for x1,y1,x2,y2,norm_x,norm_y in np.hstack((lines,normals)):\
-			x=(x1+x2)/2
-			y=(y1+y2)/2
-            		x3 = int(x - 1.*norm_x)
-            		y3 = int(y - 1.*norm_y)
-            		x4 = int(x + 1.*norm_x)
-            		y4 = int(y + 1.*norm_y)
-			if  bw_2[y3,x3]>0 :
-            			segment = Segment()
-            			segment.color = color
-            			segment.pixels_normalized[0].x = x1
-            			segment.pixels_normalized[0].y = y1
-            			segment.pixels_normalized[1].x = x2
-            			segment.pixels_normalized[1].y = y2
-            			segment.normal.x = norm_x
-            			segment.normal.y = norm_y
+        for x1,y1,x2,y2,norm_x,norm_y in np.hstack((lines,normals)):
+            segment = Segment()
+            segment.color = color
+            segment.pixels_normalized[0].x = x1
+            segment.pixels_normalized[0].y = y1
+            segment.pixels_normalized[1].x = x2
+            segment.pixels_normalized[1].y = y2
+            segment.normal.x = norm_x
+            segment.normal.y = norm_y
              
-			if  bw_2[y4,x4]>0 :
-            			segment = Segment()
-            			segment.color = color
-            			segment.pixels_normalized[0].x = x1
-            			segment.pixels_normalized[0].y = y1
-            			segment.pixels_normalized[1].x = x2
-            			segment.pixels_normalized[1].y = y2
-            			segment.normal.x = norm_x
-            			segment.normal.y = norm_y
+                #if  self.bw_2[y4,x4]>0 :
+                 #   segment = Segment()
+                  #  segment.color = color
+                   # segment.pixels_normalized[0].x = x1
+                    #segment.pixels_normalized[0].y = y1
+                    #segment.pixels_normalized[1].x = x2
+                    #segment.pixels_normalized[1].y = y2
+                    #segment.normal.x = norm_x
+                    #segment.normal.y = norm_y
             segmentMsgList.append(segment)
         return segmentMsgList
 
